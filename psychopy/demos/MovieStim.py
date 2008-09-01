49d3c70cd1ba05c802f5656bcf3ce425322b4a6d
Jon Peirce <jon@peirce.org.uk>
2008-09-01 14:47:35 +0000

    added demo for moviestim
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@109 1c617f74-e3c1-11dd-95e1-755cbddbdd66

---
+++ psychopy/demos/MovieStim.py
@@ -0,0 +1,10 @@
+from psychopy import visual, core, event
+
+win = visual.Window([800,600])
+mov = visual.MovieStim(win, 'testmovie.mpg', flipVert=False, flipHoriz=False)
+
+for frameN in range (2000):
+    mov.draw()
+    win.update()
+    
+core.quit()
