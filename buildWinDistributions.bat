7198b1ac7598e4f4220dd282de15084c56f6909e
Jon Peirce <jon@peirce.org.uk>
2008-09-01 15:04:24 +0000

    move standard files (readme.txt etc to root)
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@111 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- buildWinDistributions.bat
+++ buildWinDistributions.bat
@@ -3,6 +3,6 @@ rem c:/python24/python setup.py bdist_wininst --install-script=psychopy_post_ins
 
 rem python25
 rem python setupEgg.py bdist_egg
-python setup.py sdist --formats=zip
+python setupEgg.py sdist --formats=zip
 python setup.py bdist_wininst --install-script=psychopy_post_inst.py
 rem dist\PsychoPy-0.93.0.win32-py2.5.exe
