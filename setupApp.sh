0881f6ab89effbca8429d023275c3e3f53c53471
Jon Peirce <jon@peirce.org.uk>
2008-12-17 16:21:49 +0000

    minor correction to path in setupApp to fit with new git-svn idea
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@223 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- setupApp.sh
+++ setupApp.sh
@@ -1,8 +1,7 @@
-
 rm psychopy/demos/*.pyc
 cd PsychoPyIDE
 sudo rm -r build
 sudo rm -r dist
 python setupApp.py py2app
 cd ..
-sudo mv PsychoPyIDE/dist/PsychoPyIDE.app/ ../dist/PsychoPyIDE.app
+sudo mv PsychoPyIDE/dist/PsychoPyIDE.app/ dist/PsychoPyIDE.app
