0bcecf264304847075b11dc8de30738701a885f1
Jon Peirce <jon@peirce.org.uk>
2008-12-17 16:34:45 +0000

    updated changelog and improved setupApp.py to create PKG_INFO
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@224 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- setupApp.sh
+++ setupApp.sh
@@ -1,3 +1,8 @@
+echo 'DID YOU UPDATE THE CHANGELOG?'
+python setup.py sdist #create the tar.gz version
+python setup.py egg_info #to upload info to pypi
+python setup.py bdist_egg #an egg file for distribution
+#then handle the mac app bundle
 rm psychopy/demos/*.pyc
 cd PsychoPyIDE
 sudo rm -r build
