9ea67ac7eace53efff5d26230bf7a560fddef3f4
Jon Peirce <jon@peirce.org.uk>
2009-06-15 08:19:41 +0000

    minor
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@378 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- setupApp.sh
+++ setupApp.sh
@@ -7,6 +7,7 @@ rm psychopy/demos/*.pyc
 cd PsychoPyIDE
 sudo rm -r build
 sudo rm -r dist
-python setupApp.py py2app #don't run sudo
+python setupApp.py py2app #don't run this as sudo
 cd ..
-sudo mv PsychoPyIDE/dist/PsychoPyIDE.app/ dist/PsychoPyIDE.app
+if [-e dist/PsychoPyIDE.app/ ]; then sudo rm dist/PsychoPyIDE.app/; fi
+sudo mv PsychoPyIDE/dist/PsychoPyIDE.app/ dist/PsychoPyIDE.app/
