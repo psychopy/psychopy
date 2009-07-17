e0c28125b2d5ad5e6176580f0205f01c4baa5229
Jon Peirce <jon@peirce.org.uk>
2009-07-17 17:27:53 +0000

    added ext.linux and the first attempt at raising thread priority
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@410 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- psychopy/ext/__init__.py
+++ psychopy/ext/__init__.py
@@ -3,8 +3,10 @@ import sys
 
 if sys.platform=='win32':
 	from win32 import *
-if sys.platform=='darwin':
+elif sys.platform=='darwin':
 	from darwin import *
-if sys.platform=='posix':
+elif sys.platform.startswith('linux'):#normally 'linux2'
+	from linux import *
+elif sys.platform=='posix':#ever?!
 	from posix import *
 	
--- psychopy/ext/posix.py
+++ psychopy/ext/posix.py
@@ -1,9 +1,9 @@
-"""
+import """
 placeholder for adding c (or ctypes) extensions to the linux PsychoPy
 """
 def rush(value=True):    
     """Raise the priority of the current thread/process 
-    NOT YET IMPLEMENTED ON linux (but you can use 
+    NOT YET IMPLEMENTED ON posix (but you can use 
     > nice python ......
     
     """
