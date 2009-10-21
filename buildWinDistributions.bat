5a736309c3a83a06e37bbee4e2125ab7e50197aa
Jon Peirce <jon@peirce.org.uk>
2009-10-21 09:22:29 +0000

    minor
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@670 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- buildWinDistributions.bat
+++ buildWinDistributions.bat
@@ -1,11 +1,12 @@
 rem build simple distributions
-rem python setup.py bdist_egg
+python setup.py bdist_egg
 python setup.py sdist --formats=zip
 python setup.py bdist_wininst --install-script=psychopy_post_inst.py
 
 rem install the current version to site-packages
 python setup.py install
 
+xcopy /I /Y PsychoPy.egg-info\PKG-INFO dist\
 xcopy /I /Y psychopy\*.txt C:\Python25
 rem build the installer
 makensis.exe /v3 buildCompleteInstaller.nsi
