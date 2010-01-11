6b3354c4c030fdde01b76691c2ab28045e69fe7c
Jon Peirce <jon@peirce.org.uk>
2010-01-11 10:56:34 +0000

    app: added urls.py
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@740 1c617f74-e3c1-11dd-95e1-755cbddbdd66

---
+++ docs/source/subs.rst
@@ -0,0 +1,1 @@
+.. |PP| replace:: /PsychoPy/
---
+++ psychopy/app/urls.py
@@ -0,0 +1,26 @@
+"""A central location to store information about urls  
+"""
+
+import wxIDs
+
+urls={}
+
+#links based on string names
+urls['builder']="http://www.psychopy.org/builder/builder.html"
+urls['builder.loops']="http://www.psychopy.org/builder/flow.html#loops"
+#NB. builder components get their urls defined by the component code (so a custom component can have a url)
+
+urls['downloads']="http://code.google.com/p/psychopy/"
+urls['changelog']="http://www.psychopy.org/changelog.html"
+urls['prefs']="http://www.psychopy.org/general/prefs.html"
+urls['prefs.general']="http://www.psychopy.org/general/prefs.html#general-settings"
+urls['prefs.app']="http://www.psychopy.org/general/prefs.html#application-settings"
+urls['prefs.coder']="http://www.psychopy.org/general/prefs.html#coder-settings"
+urls['prefs.builder']="http://www.psychopy.org/general/prefs.html#builder-settings"
+urls['prefs.connections']="http://www.psychopy.org/general/prefs.html#connection-settings"
+
+#links keyed by wxIDs (e.g. menu item IDs)
+urls[wxIDs.psychopyHome]="http://www.psychopy.org/"
+urls[wxIDs.psychopyReference]="http://www.psychopy.org/api/"
+urls[wxIDs.coderTutorial]="http://www.psychopy.org/coder/tutorial1.html"
+urls[wxIDs.builderHelp]=urls['builder']
