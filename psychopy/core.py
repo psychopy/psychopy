81af2314ae610c23c3973bb2963a12b685960e46
Jeremy Gray <jrgray@gmail.com>
2010-04-19 14:16:16 +0000

    core: class RuntimeInfo(dict); svnversion(); shellCall()
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@847 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- psychopy/core.py
+++ psychopy/core.py
@@ -1,12 +1,13 @@
-"""Basic timing functions
+"""Basic functions, including timing and run-time configuration profile
 """
 # Part of the PsychoPy library
-# Copyright (C) 2009 Jonathan Peirce
+# Copyright (C) 2010 Jonathan Peirce
 # Distributed under the terms of the GNU General Public License (GPL).
 
 import sys, os, time, threading
+import subprocess, shlex
 
-try:from ext import rush
+try: from ext import rush
 except: pass
 
 runningThreads=[]
@@ -82,3 +83,134 @@ def wait(secs, hogCPUperiod=0.2):
         pyglet.media.dispatch_events()
     except:
         pass #maybe pyglet 
+
+
+def shellCall(shellCmd, stderr=False):
+    """Calls a system command via subprocess, returns the stdout from the command.
+    
+    returns (stdout,stderr) if kwarg stderr==True
+    """  
+    stdoutData, stderrData = subprocess.Popen(shlex.split(shellCmd),stdout=subprocess.PIPE).communicate()
+    if stderr:
+        return stdoutData, stderrData
+    else:
+        return stdoutData
+
+def svnversion(dir='.'):
+    """Tries to discover the svn version (revision #) for a directory.
+    """
+    svnrev = ''
+    if core.sys.platform in ['darwin', 'linux2', 'freebsd']:
+        svnrev,stderr = shellCall('svnversion -n "'+dir+'"',stderr=True) 
+        if stderr: svnrev = ''
+    else: # this hack worked for me on Win XP sp2 with TortoiseSVN (SubWCRev.exe)
+        svnrev = ''
+        tmpin = os.path.join(dir,'tmp.in')
+        f = open(tmpin,'w')
+        f.write('$WCREV$ [$WCDATE$]')
+        f.close()
+        tmph = os.path.join(dir,'tmp.h')
+        stdout,stderr = shellCall('subwcrev "'+dir+'" "'+tmpin+'" "'+tmph+'"',stderr=True)
+        os.unlink(tmpin)
+        if stderr == None:
+            f = open(tmph,'r')
+            svnrev = f.readline()
+            f.close()
+            os.unlink(tmph)
+    if len(svnrev):
+        if svnrev != 'exported': 
+            svnrev = 'r'+svnrev
+    else: svnrev = None
+
+    return svnrev
+
+
+class RuntimeInfo(dict):
+    """Return a dict-like object holding run-time info about PsychoPy, the experiment script,
+    the system & OS, python & packages, and openGL.
+    
+    The experiment script is sys.argv[0], but you have to feed in your author and version info.
+    verbose=True reports more detail, including OpenGL info
+    """
+    def __init__(self, author='', version='', verbose=False):
+        dict.__init__(self) #this will cause an object to be created with all the same methods as a dict
+        
+        # the approach here is backwards to control item order and appearance in the __str__ method
+        # so first build up a string that is formatted nicely for a log file (including comments)
+        # then convert to dict
+        import psychopy
+        profileInfo = '  #PsychoPy:  see http://www.psychopy.org\n'
+        profileInfo += '    "psychopy_version": "'+psychopy.__version__+'",\n'
+        
+        profileInfo += '  #Experiment script:\n'
+        profileInfo += '    "experiment_scriptName": "'+os.path.basename(sys.argv[0]).replace('"','')+'",\n'
+        profileInfo += '    "experiment_scriptAuthor": "'+author.replace('"','')+'",\n'
+        profileInfo += '    "experiment_scriptVersion": "'+version.replace('"','')+'",\n'
+        profileInfo += '    "experiment_runDate": "'+time.ctime()+'",\n'
+        scriptDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        profileInfo += '    "experiment_fullPath": "'+scriptDir+'",\n'
+        svnrev = svnversion(dir=scriptDir)
+        if svnrev: 
+            profileInfo += '    "experiment_svnRev": "'+svnrev+'",\n'
+        
+        profileInfo += '  #System:\n'
+        profileInfo += '    "hostname": "'+platform.node().replace('"','')+'",\n'
+        profileInfo += '    "platform": "'+core.sys.platform
+        if core.sys.platform=='darwin':
+            OSXver, junk, architecture = platform.mac_ver()
+            profileInfo += ' '+OSXver+' '+architecture
+        elif core.sys.platform == 'linux2':
+            profileInfo += ' '+platform.release()
+        elif core.sys.platform in ['win32']:
+            profileInfo += ' windowsversion='+repr(sys.getwindowsversion())
+        profileInfo += '",\n'
+        profileInfo += '    "monitor": "(not implemented)",\n'
+        profileInfo += '    "fps": "(not implemented)",\n'
+        
+        profileInfo += '  #Python:\n'
+        profileInfo += '    "python_version": "'+core.sys.version.split()[0]+'",\n'
+        
+        if verbose:
+            # External python packages:
+            import numpy; profileInfo += '    "numpy_version": "'+numpy.__version__.replace('"','')+'",\n'
+            import scipy; profileInfo += '    "scipy_version": "'+scipy.__version__.replace('"','')+'",\n'
+            import matplotlib; profileInfo += '    "matplotlib_version": "'+matplotlib.__version__.replace('"','')+'",\n'
+            import pyglet; profileInfo += '    "pyglet_version": "'+pyglet.__version__.replace('"','')+'",\n'
+            try: import pygame; profileInfo += '    "pygame_version": "'+pygame.__version__.replace('"','')+'",\n'
+            except: pass
+            
+            # Python gory details:
+            profileInfo += '    "python_fullVersion": "'+sys.version.replace('\n',' ').replace('"','')+'",\n'
+            profileInfo += '    "python_executable": "'+sys.executable.replace('"','')+'",\n'
+            
+            # OpenGL info:
+            profileInfo += '  #OpenGL:\n'
+            tmpwin = visual.Window([10,10]) # some drivers want a window open first
+            from pyglet.gl import gl_info
+            profileInfo += '    "openGL_vendor": "'+gl_info.get_vendor().replace('"','')+'",\n'
+            profileInfo += '    "openGL_renderingEngine": "'+gl_info.get_renderer().replace('"','')+'",\n'
+            profileInfo += '    "openGL_version": "'+gl_info.get_version().replace('"','')+'",\n'
+            
+            extensionsOfInterest=['GL_ARB_multitexture', 
+                'GL_EXT_framebuffer_object','GL_ARB_fragment_program',
+                'GL_ARB_shader_objects','GL_ARB_vertex_shader',
+                'GL_ARB_texture_non_power_of_two','GL_ARB_texture_float']
+            for ext in extensionsOfInterest:
+                profileInfo += '    "'+ext+'": '+str(bool(gl_info.have_extension(ext)))+',\n'
+            tmpwin.close()
+        
+        # store the string version for later use, eg, printing
+        self.stringRepr = profileInfo
+        
+        # convert string to dict
+        d = eval('{'+profileInfo+'}')
+        for k in d.keys():
+            self[k] = d[k]
+        
+    def __repr__(self):    
+        # returns nicer format string, still legal python dict
+        return '{'+self.stringRepr+'}'
+    
+    def __str__(self):
+        # for easier human reading (e.g., in a log file)
+        return self.stringRepr.replace('"','').replace(',\n','\n')
