e0cd337cf76e18a8b64a43d3d17a9092317a43b8
Jeremy Gray <jrgray@gmail.com>
2010-05-24 19:11:10 +0000

    better searchCopyrightYear.py
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@991 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- searchCopyrightYear.py
+++ searchCopyrightYear.py
@@ -2,22 +2,31 @@
 
 """looks for lines containing 'Copyright|(C)', <last-year>, and 'Peirce' in all files in or below the current directory
   ignore .svn, .hg, .git, .pyc, .mp4, ... and some others
-and writes out a file with commands to update previous year to the current year
-  eg in 2008: 'Copyright (C) 2007 Jonathan Peirce'  --> 'Copyright (C) 2008 Jonathan Peirce'
-this program by itself never makes any changes, just generates a file that will do so if you source it.
+and writes out an executable file, replaceCopyright<year>, with commands that could be used to update last-year to the current year.
 
-eg, finds:
+usage steps:
+- 'svn update'
+- 'svn commit any pending changes'
+- 'python psychopy/tests/run.py'
+- './searchCopyrightYear.py'
+- review the new file, replaceCopyright<Year>.sh, edit as needed
+- './replaceCopyrightYear.py' -- this does the replacing
+- 'python psychopy/tests/run.py' -- make sure we didn't break anything
+- 'svn commit -m "updated copyright year"'
+
+currently will handle:
 Copyright (C) 2009 Jonathan Peirce
 (C) 2002-2009 Jonathan Peirce
 copyright = u'2009, Jonathan Peirce'
 PsychoPy2, version %s (c)Jonathan Peirce, 2009, GNU GPL
 Copyright (c) 2002-2009, JW Peirce.
 
-uses perl -pi -e ...; I couldn't figure out a pythonic way to do in-place changes to files
-import fileinput
+relies on: perl -pi -e 's/\Qold\E/new/' <file>
+I couldn't figure out a pythonic way to do in-place changes to files
+import fileinput # looked promising but a) don't really want to copy every line of every file, and b) doesn't actually work easily...
 for line in fileinput.input(file, inplace = 1):
-  print line.replace(...).strip() --> loses initial whitespace
-  line.replace(....) --> adds quote marks around line
+  print line.replace(...).strip() #--> loses initial whitespace
+  line.replace(....) #--> adds quote marks around line
 """
 
 __author__ = 'Jeremy Gray'
@@ -34,44 +43,50 @@ oldYear = str(int(newYear)-1) # last year; will need to set manually if you miss
 print "Checking all lines of all files for copyright <year> info..."
 
 #get paths and names of eligible files (in or below current directory)
-files = os.popen('find . | egrep -v ".pyc|/.hg|/.svn|/.git|.pdf|.dll|.wav|.mp4|.mpg|.ico|.jpg|.png|.DS_Store"').readlines()
-files = [f.strip() for f in files]
-try: del files[files.index(sys.argv[0])]
-except: pass # not in the list
+files = os.popen('find . | egrep -v ".pyc|/.hg|/.svn|/.git|.pdf|.dll|.wav|.mp4|.mpg|.ico|.jpg|.gif|.png|.DS_Store"').readlines()
+files = [f.strip() for f in files if f.find(os.path.basename(sys.argv[0])) == -1] # strip(), and remove self from the list
 
-someFiles = False # found any at all to be updated?
-badLines = 0 # '$' will mess with perl search-replace
-tmpFile = './replaceCopyrightYear.tmp'
+badLines = 0 #  ['$/] will mess with perl search-replace; other characters might too
+targetFiles = 0 # count of files to be updated
+tmpFile = './replaceCopyright'+oldYear+'_'+newYear+'.sh'
 try: del files[files.index(tmpFile)]
 except: pass
 tmp = open(tmpFile, 'w')
+tmp.write('#!/bin/sh \n echo Updating...\n')
 
 # check each line of each file:
 for file in files: 
     lines = os.popen('grep '+oldYear+' "'+file+'" | grep Peirce | egrep -i "\(c\)|copyright"').readlines()
-    for line in [i.strip() for i in lines]: #allow multiple lines per file, ignore directories etc
-        anchor = '^' # start-of-line anchor makes search-replace much more efficient
-        if line.find('$') > -1:
-            badLines += 1
-            print file+": cannot handle '$' in line: ", os.popen("grep -n '"+line+"' '"+file+"'").read()
-            continue
-        if line.find("'") > -1: # capture stuff in between single-quotes
+    for line in lines: #allow multiple lines per file, each gets its own replace command; directories -> ignored
+        line = line.strip()
+        anchor = '^' # start-of-line anchor makes search-replace more efficient
+        if line.find("'") > -1: # capture stuff in between single-quotes, hopefully including the year
             line = line[line.find("'")+1:]
             line = line[:line.find("'")]
+            if line.find(oldYear) == -1:
+                badLines += 1
+                print file+": expected <last-year> somewhere between single-quotes:", line
+                continue
             anchor = '' # will not match at the start of the line anymore
-        newLine = line.replace(oldYear, newYear)
-        someFiles = True
-        #print file+': '+line+' --> '+newLine
-        cmd = "perl -pi -e 's/"+anchor+"\Q"+line+"\E/"+newLine+"/' '"+file+"'\n" # 'source' fails if there are single quotes in the line...
-        tmp.write(cmd) # save the command into a tmp file
+        if line.find('$') > -1 or line.find('/') > -1:
+            badLines += 1
+            print file+": cannot handle '$' or '/' in line:", line
+            continue
+        newLine = line.replace(oldYear, newYear) # should not contain characters that will mess with perl 's/oldLine/newLine/'
+        cmd = "echo "+file+"\n" # helps with debugging, if the perl s/// flails due to a bad character -> you know what file to look at
+        cmd += "perl -pi -e 's/"+anchor+"\Q"+line+"\E/"+newLine+"/' '"+file+"'\n" # only match one line, avoid s///g
+        tmp.write(cmd) 
+        targetFiles += 1
+tmp.write('echo Updated %d files.\n' % targetFiles)
 tmp.close()
 
-print os.popen('cat '+tmpFile).read() # merely display, do not actually do, all of the replacement commands
-if someFiles:
-    print '\nIf all of the above changes look correct, to make all changes type:\n  source '+tmpFile
-    print 'If something looks amiss, you can edit '+tmpFile+', and then source it.'
+print os.popen('cat '+tmpFile+" | grep 'perl -pi -e'").read() # merely display, do not actually do, all of the replacement commands
+os.popen('chmod u+x '+tmpFile) # make executable
+if targetFiles:
+    print 'If all of the above changes look correct, to make all changes type:\n  '+tmpFile
+    print 'If something looks amiss, you can manually edit '+tmpFile+', and then run it.'
     if badLines:
-        print "Warning: %d lines were skipped because they had a '$' character, see output about" % badLines
+        print "Warning: %d lines were skipped because they had a special character, see output" % badLines
 else:
     print 'No matching files found for', oldYear
     os.unlink(tmpFile)
