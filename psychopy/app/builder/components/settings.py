a887fa0b1ab4ae5722db05a9f16793b664a59aa2
Jon Peirce <jon@peirce.org.uk>
2010-08-30 21:12:34 +0000

    builder: force closing of window after experiment has run
    git-svn-id: https://psychopy.googlecode.com/svn/trunk@1122 1c617f74-e3c1-11dd-95e1-755cbddbdd66

--- psychopy/app/builder/components/settings.py
+++ psychopy/app/builder/components/settings.py
@@ -82,4 +82,6 @@ class SettingsComponent:
     def writeEndCode(self,buff):
         """write code for end of experiment (e.g. close log file)
         """
-        buff.writeIndented("logFile.close()")
+        buff.writeIndented("logFile.close()\n")
+        buff.writeIndented("win.close()\n")
+        buff.writeIndented("core.quit()\n")
