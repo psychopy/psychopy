5fc1da7abbff9ab208db5a61e3466c076fe3219d
Jon Peirce <jon@peirce.org.uk>
2010-09-08 12:57:03 +0100

    build scripts: added script to save build info (git id, platform)
    the idea is for the script to be run by setup.py
    This will allow us to track the exact git version being used
    (rather than just the PsychoPy versi nnumber) and the platform that
    the build was made on (e.g. vista, leopard, snow leopard...)

---
+++ psychopy/version.py
@@ -0,0 +1,7 @@
+__version__ = '1.62.01'
+__license__ = 'GNU GPLv3 (or more recent equivalent)'
+__author__ = 'Jonathan Peirce'
+__author_email__='jon@peirce.org.uk'
+__maintainer_email__='psychopy-users@googlegroups.com'
+__url__='http://www.psychopy.org'
+__downloadUrl__="http://code.google.com/p/psychopy/"
