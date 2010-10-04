21f09e9ec008d54a36362f0a6d40c0bae8ef8802
Jon Peirce <jon@peirce.org.uk>
2010-10-04 15:23:29 +0100

    builder: pasted routines weren't fully updating their name

--- psychopy/app/builder/builder.py
+++ psychopy/app/builder/builder.py
@@ -2232,6 +2232,7 @@ class BuilderFrame(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             routineName=dlg.GetValue()
             newRoutine = copy.deepcopy(self.app.copiedRoutine)
+            newRoutine.name = routineName
             self.exp.addRoutine(routineName, newRoutine)#add to the experiment
             self.routinePanel.addRoutinePage(routineName, newRoutine)#could do redrawRoutines but would be slower?
             self.addToUndoStack("paste Routine %s" %routineName)
