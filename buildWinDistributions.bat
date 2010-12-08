ec0d19818665ea559bb5c37e27f91e9e5b6d6916
Jon Peirce <jon@peirce.org.uk>
2010-12-08 11:41:19 +0000

    build: updates to build scripts on win32

--- buildWinDistributions.bat
+++ buildWinDistributions.bat
@@ -6,8 +6,8 @@ python setup.py bdist_wininst --install-script=psychopy_post_inst.py
 rem install the current version to site-packages
 python setup.py install
 
-del C:\Python25\Lib\site-packages\psychopy.pth
-xcopy /I /Y psychopy\*.txt C:\Python25
+del C:\Python26\Lib\site-packages\psychopy.pth
+xcopy /I /Y psychopy\*.txt C:\Python26
 rem build the installer
 makensis.exe /v3 buildCompleteInstaller.nsi
 
@@ -16,4 +16,4 @@ move /Y "StandalonePsychoPy*.exe" ..\dist\
 move /Y dist\* ..\dist\
 
 rem reinsert my dev .pth file
-xcopy /I /Y ..\psychopy.pth C:\Python25\Lib\site-packages\psychopy.pth
+xcopy /I /Y ..\psychopy.pth C:\Python26\Lib\site-packages\psychopy.pth
