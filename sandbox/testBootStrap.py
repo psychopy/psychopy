b38b891cf91d00b17f25243700915f4a58a4590d
Jon Peirce <jon@peirce.org.uk>
2010-12-11 11:37:06 +0000

    fixed EOLs

--- sandbox/testBootStrap.py
+++ sandbox/testBootStrap.py
@@ -1,9 +1,9 @@
-#test boot
-
-from psychopy import data, core
-import scipy
-
-xx= scipy.array([[0, 0, 0, 0], [0.0,0.0,1.0,1.0],[1,1,1,1]])
-timer=core.Clock()
-boots = data.bootrsp(xx,1000)
+#test boot
+
+from psychopy import data, core
+import scipy
+
+xx= scipy.array([[0, 0, 0, 0], [0.0,0.0,1.0,1.0],[1,1,1,1]])
+timer=core.Clock()
+boots = data.bootrsp(xx,1000)
 print timer.getTime()
