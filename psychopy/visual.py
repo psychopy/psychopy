95f9e7a056cb3b52c4d76be288f2f1bc69c48d36
jeremygray <jrgray@gmail.com>
2011-03-05 22:32:28 -0500

    trying out my first git push

--- psychopy/visual.py
+++ psychopy/visual.py
@@ -5303,11 +5303,10 @@ class RatingScale:
         # only resets things that are likely to have changed when the ratingScale instance is used by a subject
         self.noResponse = True
         self.markerPlaced = False
-        #NB in the following, "is not" is needed instead of "!=" because markerStart could be 0
-        # "0==False" but "0 is not False"
-        if self.markerStart!=None and (self.markerStart is not False):
+        #NB markerStart could be 0; during __init__, its forced to be numeric and valid, or None (not boolean)
+        if self.markerStart != None:
             self.markerPlaced = True
-            self.markerPlacedAt = self.markerStart - self.low
+            self.markerPlacedAt = self.markerStart - self.low # __init__ assures this is valid 
         self.firstDraw = True # triggers self.myClock.reset() at start of draw()
         self.decisionTime = 0
         self.markerPosFixed = False
@@ -5341,6 +5340,7 @@ class RatingScale:
             return None
         
         return self.decisionTime
+    
 class Aperture:
     """Used to create a shape (circular for now) to restrict a stimulus
     visibility area.
