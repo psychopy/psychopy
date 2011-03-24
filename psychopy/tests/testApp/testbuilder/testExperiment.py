3b1e102c8f64e12eda0eb2c6c6f72fdf37e8afe3
jeremygray <jrgray@gmail.com>
2011-03-24 01:42:25 -0400

    bugfixes, path fixes for test load compile
    get "Exception TypeError: TypeError("'NoneType' object is not callable",) in <bound method Popen3.__del__ of <popen2.Popen3 instance at 0x2013ad00>> ignored"
    when calling from tests. odd.

--- psychopy/tests/testApp/testbuilder/testExperiment.py
+++ psychopy/tests/testApp/testbuilder/testExperiment.py
@@ -7,13 +7,14 @@ exp = psychopy.app.builder.experiment.Experiment()
 here = path.abspath(path.dirname(__file__))
 
 #def shellCall(shellCmd):
-#    # this had build problems on mac when in psychopy.core but is preferred way to do things
+#    # this is the preferred way to do things but had build problems on mac when it was in psychopy.core 
 #    import subprocess, shlex
 #    stdoutData, stderrData = subprocess.Popen(shlex.split(shellCmd),
 #            stdout=subprocess.PIPE, stderr=subprocess.PIPE ).communicate()
 #    return stdoutData.strip(), stderrData.strip()
 
 def shellCall(shellCmd):
+    # do a shell call, capture stdout, stderr
     stdO,stdI,stdE = popen2.popen3(shellCmd)
     stdOData = stdO.read().strip()
     stdEData = stdE.read().strip()
@@ -22,39 +23,37 @@ def shellCall(shellCmd):
     stdE.close()
     return stdOData, stdEData
 
-def testExperiment_AddRoutine():
+def testExp_AddRoutine():
     exp.addRoutine('instructions')
 #    exp.routines['instructions'].AddComponent(
 #    exp.Add
 
 
-def testExp_LoadCompileSave6psyexpFiles():
-    target_file = 'tmp_lastrun.py'
-    #demos = here.replace('/tests/testApp/testbuilder', '/demos/builder/*/*.psyexp')
-    test_psyexp = glob.glob('*psyexp') 
+def testExp_LoadCompileSavePsyexpFiles():
+    tmp_file = path.join(here, 'tmp_lastrun.py')
+    test_psyexp = glob.glob(path.join(here, '*psyexp'))
     assert len(test_psyexp) >= 2 # want 2+ demo psyexp files to test; bart.psyexp had a unicode char -> error
     for file in test_psyexp:
-        if file.find('bart.psyexp'): continue
+        if file.find('bart.psyexp') > -1: continue
         # go from psyexp file on disk to internal builder representation:
         exp.loadFromXML(path.join(here, file))
         assert len(exp.namespace.user) # should automatically populate the namespace
         assert not exp.namespace.get_collisions() # ... without duplicates
         # from there generate a script:
-        buff = exp.writeScript() # as StringIO object
+        buff = exp.writeScript() # is a StringIO object
         script = buff.getvalue()
         assert len(str(script))
-        # save the script to a tmp file:
-        f = open(target_file, 'wb+')
+        # save the script:
+        f = open(tmp_file, 'wb+')
         f.write(script)
         f.close()
-        # compile the temp file, catching error msgs:
-        #unlink(target_file) # ==> no file -> raise error, good
-        stdout_contents, syntax_error = shellCall("python -m py_compile "+target_file)
-        assert not stdout_contents   # from: "python -m py_compile tmp_lastrun.p"
-        assert not syntax_error  # from: "python -m py_compile tmp_lastrun.py"
+        # compile the temp file, catching error msgs (including no file at all):
+        stdout_contents, syntax_error = shellCall("python -m py_compile "+tmp_file)
+        assert not stdout_contents   # from: `python -m py_compile tmp_lastrun.py`
+        assert not syntax_error  # from: `python -m py_compile tmp_lastrun.py`
         #py_compile.compile(target_file) # fails to catch errors
-    #unlink(target_file)
-    unlink(target_file+'c')
+    unlink(tmp_file)
+    unlink(tmp_file+'c')
         
 def testExp_NameSpace():
     assert exp.namespace.exists('psychopy') == "Psychopy module"
@@ -63,6 +62,10 @@ def testExp_NameSpace():
     assert exp.namespace.exists('foo') == "script variable"
     exp.namespace.add('foo')
     assert exp.namespace.get_collisions() == ['foo']
+    
+    assert not exp.namespace.is_valid('123')
+    assert not exp.namespace.is_valid('a1 23')
+    assert not exp.namespace.is_valid('a123$')
     
     assert exp.namespace.make_valid('123') == 'var_123'
     assert exp.namespace.make_valid('a a a') == 'a_a_a'
