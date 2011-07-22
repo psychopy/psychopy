869daa6bc32c95655d84707e46919052d1a9f7b9
Yaroslav Halchenko <debian@onerussian.com>
2011-07-22 14:58:49 -0400

    cosmetic -- more of trailing spaces + few spell checks

--- psychopy/tests/utils.py
+++ psychopy/tests/utils.py
@@ -5,12 +5,12 @@ import numpy as np
 import OpenGL
 
 def compareScreenshot(fileName, win, crit=5.0):
-    """Compare the current back buffer of the given window with the file 
+    """Compare the current back buffer of the given window with the file
     """
-    #if we start this from a folder below run.py the data folder won't be found 
+    #if we start this from a folder below run.py the data folder won't be found
     if not os.path.isdir(os.path.split(fileName)[0]):
         fileName = os.path.join('..',fileName)
-    #if the file exists run a test, if not save the file 
+    #if the file exists run a test, if not save the file
     if not os.path.isfile(fileName):
         saveImage=True
     else:saveImage=False
@@ -32,4 +32,3 @@ def compareScreenshot(fileName, win, crit=5.0):
             frame.save(filenameLocal, optimize=1)
             print "** Saved copy of actual frame to %s **" %filenameLocal
         assert rms<crit
-        
