04911f502c568a5836f360401ab30df112814c1b
Jeremy Gray <jrgray@gmail.com>
2011-07-24 19:37:37 -0400

    bugfix: restore remove routine from flow via contextual menu

--- psychopy/app/builder/builder.py
+++ psychopy/app/builder/builder.py
@@ -310,8 +310,9 @@ class FlowPanel(wx.ScrolledWindow):
         component=self.componentFromID[self._menuComponentID]
         flow = self.frame.exp.flow
         if op=='remove':
-            # remove name from namespace only for loops, not routines
-            if component.type in ['TrialHandler', 'StairHandler']:
+            # remove name from namespace only if its a loop (exists only in the flow)
+            # routines do not have a type attribute
+            if hasattr(component, 'type') and component.type in ['TrialHandler', 'StairHandler']:
                 trialListFile = component.params['trialListFile'].val
                 if trialListFile:
                     self.trialList, fieldNames = data.importTrialList(trialListFile, returnFieldNames=True)
