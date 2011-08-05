2226f7148e7cdce00f10a76caf93802d536556bd
Jonathan Peirce <jon@peirce.org.uk>
2011-08-05 11:30:14 +0100

    builder: Experiment was not loading xlsx files properly from relative path

--- psychopy/app/builder/experiment.py
+++ psychopy/app/builder/experiment.py
@@ -247,6 +247,8 @@ class Experiment:
         #open the file using a parser that ignores prettyprint blank text
         parser = etree.XMLParser(remove_blank_text=True)
         f=open(filename)
+        folder = os.path.split(filename)[0]
+        os.chdir(folder)
         self._doc=etree.XML(f.read(),parser)
         f.close()
         root=self._doc#.getroot()
@@ -360,7 +362,7 @@ class Param:
     '[3,4]'
     >>> print Param(val=[3,4], valType='code')
     [3, 4]
-    
+
     >>> #### auto str -> code:  at least one non-escaped '$' triggers str -> code: ####
     >>> print Param('[x,y]','str') # str normally returns string
     '[x,y]'
