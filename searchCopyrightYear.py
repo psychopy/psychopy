4e187838613db7bdad4d1ebd658f0da607d4fcff
Jeremy Gray <jrgray@gmail.com>
2012-02-25 09:11:05 -0500

    update copyright-year search/replace

--- searchCopyrightYear.py
+++ searchCopyrightYear.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-"""looks for lines containing 'Copyright|(C)', <last-year>, and 'Peirce' in all files in or below the current directory
-  ignore .svn, .hg, .git, .pyc, .mp4, ... and some others
-and writes out an executable file, replaceCopyright<year>, with commands that could be used to update last-year to the current year.
+"""looks for lines containing 'Copyright|(C)', <last-year>, and 'Peirce'
+in all files in or below the current directory
+and writes out an executable file, replaceCopyright<year>, with commands that
+could be used to update last-year to the current year.
 
 usage steps:
 - run tests
@@ -12,16 +13,10 @@ usage steps:
 - run tests again -- make sure we didn't break anything
 - commit
 
-currently will handle:
-Copyright (C) 2009 Jonathan Peirce
-(C) 2002-2009 Jonathan Peirce
-copyright = u'2009, Jonathan Peirce'
-PsychoPy2, version %s (c)Jonathan Peirce, 2009, GNU GPL
-Copyright (c) 2002-2009, JW Peirce.
-
 relies on: perl -pi -e 's/\Qold\E/new/' <file>
 I couldn't figure out a pythonic way to do in-place changes to files
-import fileinput # looked promising but a) don't really want to copy every line of every file, and b) doesn't actually work easily...
+import fileinput # looked promising but a) don't really want to copy every line
+of every file, and b) doesn't actually work easily...
 for line in fileinput.input(file, inplace = 1):
   print line.replace(...).strip() #--> loses initial whitespace
   line.replace(....) #--> adds quote marks around line
@@ -29,27 +24,36 @@ for line in fileinput.input(file, inplace = 1):
 
 __author__ = 'Jeremy Gray'
 
-import os,sys,time
-
-print "\n\nsearch for copyright year is BROKEN. please fix.\n\n"
-sys.exit()
+import os, sys, time, glob
 
 from psychopy import core
 
 assert sys.platform == 'darwin' or sys.platform.startswith('linux')
-#perl = os.popen('perl -V | head -1').read()
 perlVersion = core.shellCall('perl -V').splitlines()[0]
 assert perlVersion.find('perl5') > -1 # not completely sure what will happen with other perl versions...
 
 newYear = str(time.localtime()[0]) # current year
 oldYear = str(int(newYear)-1) # last year; will need to set manually if you miss a year
 
-print "Checking all lines of all files for copyright <year> info..."
+print "copyright %s -> %s: searching for files" % (oldYear, newYear)
 
-#get paths and names of eligible files (in or below current directory)
-#files = os.popen(r"find . | sed -e 's/ /\\ /g'" + ' | egrep -v ".pyc|/.hg|/.svn|/.git|.pdf|.dll|.wav|.mp4|.mpg|.ico|.jpg|.gif|.png|.DS_Store"').readlines()
-files = core.shellCall('sh ./find_pp.sh')
-files = [f.strip() for f in files.splitlines() if not os.path.isdir(f)]
+#find relevant files:
+files = []
+for root, dirs, tmpfiles in os.walk('.'):
+    for f in tmpfiles:
+        file = root+'/'+f
+        try: tail = f.rsplit('.',2)[1]
+        except: tail = 'NONE'
+        if tail in ['html','orig','pickle','doctree','pyc','pdf','dll','pyw', 'mov',
+                'wav','mp4','mpg','ico','jpg','gif','png','DS_Store','xlsx', 'icns','svg']:
+            continue
+        if not file.find('build/pygame/Contents/Packages') > -1 \
+            and not file.find('build/pyobjc') > -1 \
+            and not file.find('/sandbox/') > -1 \
+            and not file.find('/docs/build/') > -1 \
+            and not file.startswith('./.git'):
+                #print tail, file
+                files.append(file)
 print len(files), 'files found, screening each'
 
 badLines = 0 #  ['$/] will mess with perl search-replace; other characters might too
@@ -60,42 +64,45 @@ except: pass
 tmp = open(tmpFile, 'w')
 tmp.write('#!/bin/sh \necho Updating...\n')
 
-# check each line of each file:
+# check each line of each relevant file:
 for file in files:
+    if os.path.isdir(file) or file.endswith(sys.argv[0]):
+        continue
     contents = open(r''+file+'', 'r').readlines()
-    lines = [line for line in contents if line.find("Peirce") > -1 and \
-             (line.lower().find("(c)") or line.lower().find("copyright"))]
-    for line in lines: #allow multiple lines per file, each gets its own replace command; directories -> ignored
+    lines = [line for line in contents if \
+             line.find("Peirce") > -1 and \
+             line.find(oldYear) > -1 and \
+             (line.lower().find("(c)") > -1 or line.lower().find("copyright") > -1) 
+             ]
+    for i,line in enumerate(lines): #allow multiple lines per file, each gets its own replace command
+        #print i+1, file
         line = line.strip()
-        anchor = '^' # start-of-line anchor makes search-replace more efficient
+        #print line
         if line.find("'") > -1: # capture stuff in between single-quotes, hopefully including the year
             line = line[line.find("'")+1:]
             line = line[:line.find("'")]
             if line.find(oldYear) == -1:
-            #    badLines += 1
-            #    print file+": expected <last-year> somewhere between single-quotes:", line
+                badLines += 1
+                print file+": expected <last-year> somewhere between single-quotes:", line
                 continue # skip the line
-            anchor = '' # will not match at the start of the line anymore
         if line.find('$') > -1 or line.find('/') > -1:
-            #badLines += 1
-            #print file+": cannot handle '$' or '/' in line:", line
+            badLines += 1
+            print file+": cannot handle '$' or '/' in line:", line
             continue
         newLine = line.replace(oldYear, newYear) # should not contain characters that will mess with perl 's/oldLine/newLine/'
-        cmd = "echo "+file+"\n" # helps with debugging, if the perl s/// flails due to a bad character -> you know what file to look at
-        cmd += "perl -pi -e 's/"+anchor+"\Q"+line+"\E/"+newLine+"/' '"+file+"'\n" # only match one line, avoid s///g
+        cmd = "echo "+file+"\n  " # helps with debugging, if the perl s/// flails due to a bad character -> you know what file to look at
+        cmd += "perl -pi -e 's/\Q"+line+"\E/"+newLine+"/' '"+file+"'\n" # only match one line, avoid s///g
         tmp.write(cmd) 
         targetFiles += 1
 tmp.write('echo Updated %d files.\n' % targetFiles)
 tmp.close()
-#os.unlink(tmpFile)
 
-core.shellCall('cat '+tmpFile)
 core.shellCall('chmod u+x '+tmpFile) # make executable
 if targetFiles:
-    print 'If all of the above changes look correct, to make all changes type:\n  '+tmpFile
-    print 'If something looks amiss, you can manually edit '+tmpFile+', and then run it.'
+    print 'To make %d changes, inspect then run:\n  '%targetFiles, tmpFile
+    print 'If something looks amiss, you can manually edit then run it.'
     if badLines:
-        print "Warning: %d lines were skipped because they had a special character, see output" % badLines
+        print "Warning: %d lines were skipped" % badLines
 else:
     print 'No matching files found for year', oldYear
     os.unlink(tmpFile)
