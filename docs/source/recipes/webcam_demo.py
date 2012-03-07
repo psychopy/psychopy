6a4d6e29c128cda890058c25a57a8a15d81d1495
Jeremy Gray <jrgray@gmail.com>
2012-03-07 16:15:38 -0500

    DOC: recipe for adding a web-cam
    just copy & pasted from mailing list, not tried (easy_install opencv did not work for me)

---
+++ docs/source/recipes/webcam_demo.py
@@ -0,0 +1,22 @@
+
+from psychopy import visual, event, core
+import Image, time, pylab, cv, numpy
+
+mywin = visual.Window(allowGUI=False, monitor='testMonitor', units='norm',colorSpace='rgb',color=[-1,-1,-1], fullscr=True)
+mywin.setMouseVisible(False)
+
+capture = cv.CaptureFromCAM(0)
+img = cv.QueryFrame(capture)
+pi = Image.fromstring("RGB", cv.GetSize(img), img.tostring(), "raw", "BGR", 0, 1)
+print pi.size
+myStim = visual.PatchStim(win=mywin, tex=pi, pos=[0,0.5], size = [0.6,0.6], opacity = 1.0, units = 'norm')
+myStim.setAutoDraw(True)
+
+while True:
+    img = cv.QueryFrame(capture)
+    pi = Image.fromstring("RGB", cv.GetSize(img), img.tostring(), "raw", "BGR", 0, 1)
+    myStim.setTex(pi)
+    mywin.flip()
+    theKey = event.getKeys()
+    if len(theKey) != 0:
+        break
