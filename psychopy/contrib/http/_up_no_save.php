bc573a885b817e91f6a14bfd39f1710885b82740
Jeremy Gray <jrgray@gmail.com>
2012-03-08 08:08:37 -0500

    RF: http upload demo

--- psychopy/contrib/http/_up_no_save.php
+++ psychopy/contrib/http/_up_no_save.php
@@ -3,7 +3,7 @@
 // this is a version of up.php to allow anyone anywhere to test against my server
 // using psychopy coder/demo/misc/http_upload.py
 // it works the same as up.php except that no file is saved on my machine
-// JRG note to self: rename this file up.php and put in webroot/upload_test/up.php
+// JRG note to self: put in webroot/upload_test/up_no_save.php
 
 require('../psychopy_org/up.php'); # run up.php
 unlink($target); # remove the saved file
--- psychopy/demos/coder/misc/http_upload.py
+++ psychopy/demos/coder/misc/http_upload.py
@@ -8,13 +8,14 @@ from psychopy.contrib.http import post
 # edit info to reflect your set-up:
 info = {
     'host': 'scanlab.psych.yale.edu', # your server; IP address is fine
-    'selector': 'http://scanlab.psych.yale.edu/upload_test/up.php',
+    'selector': 'http://scanlab.psych.yale.edu/upload_test/up_no_save.php',
     #'selector': 'http://your_server_here/your_path_here/up.php',
     'filename': __file__} # path to file to upload (__file__ is this script--it will upload itself)
 
+print 'trying', info['selector'], ':'
 status = post.upload(**info) # do the upload, get return value
 print status
 
-if not status.startswith('success'):
-    print '''\n# a good upload will return something like this:
-            success good_upload c5df3bf286b8e3cc9bfccaf1218adf43342b6725c901987bda7989e29c136b45 892'''
+if not status.startswith('success'): # handle your error conditions, e.g., warn that upload failed
+    print '''\nFAILED.\na good upload will return something like this (different sha256 and byte count):
+success good_upload c5df3bf286b8e3cc9bfccaf1218adf43342b6725c901987bda7989e29c136b45 854'''
