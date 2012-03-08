a9e2a58f71292257d9714689052c98f788bbd481
Jonathan Peirce <jon@peirce.org.uk>
2012-03-08 17:02:47 +0000

    BF: removed unecessary print statements (from debugging) in visual

--- psychopy/visual.py
+++ psychopy/visual.py
@@ -453,7 +453,6 @@ class Window:
 
         if self.winType == "glut": GLUT.glutSwapBuffers()
         elif self.winType =="pyglet":
-            #print "updating pyglet"
             #make sure this is current context
             self.winHandle.switch_to()
 
@@ -1080,7 +1079,7 @@ class Window:
 
         status = FB.glCheckFramebufferStatusEXT (FB.GL_FRAMEBUFFER_EXT);
         if status != FB.GL_FRAMEBUFFER_COMPLETE_EXT:
-            print "Error in framebuffer activation"
+            logging.warning("Error in framebuffer activation")
             return
         GL.glDisable(GL.GL_TEXTURE_2D)
 
@@ -2333,7 +2332,6 @@ class PatchStim(_BaseVisualStim):
         stimulus changes. Call it if you change a property manually
         rather than using the .set() command
         """
-        #print 'updating Shaders list'
         self.needUpdate=0
         GL.glNewList(self._listID,GL.GL_COMPILE)
         #setup the shaderprogram
@@ -2402,7 +2400,6 @@ class PatchStim(_BaseVisualStim):
         stimulus changes. Call it if you change a property manually
         rather than using the .set() command
         """
-        #print 'updating No Shaders list'
         self.needUpdate=0
 
         GL.glNewList(self._listID,GL.GL_COMPILE)
@@ -4989,7 +4986,6 @@ class BufferImageStim(PatchStim):
             texture = data.tostring()#serialise
         else:#pyglet on linux needs ctypes instead of string object!?
             texture = data.ctypes#serialise
-        # print data.shape, self.origSize # they are swapped, relative to each other
 
         #bind the texture in openGL
         GL.glEnable(GL.GL_TEXTURE_2D)
@@ -5777,7 +5773,6 @@ class RatingScale:
         Accounts for non-zero low end, autoRescale, and precision.
         The return value is assured to be on the scale.
         """
-        print 'set tick'
         # on the line:
         value = max(min(self.high, value), self.low)
         # with requested precision:
@@ -6603,7 +6598,6 @@ def _setColor(self, color, colorSpace=None, operation='',
         if hasattr(self,'dkl_rgb'): win=self #self is probably a Window
         elif hasattr(self, 'win'): win=self.win #self is probably a Stimulus
         else:
-            print hasattr(self,'dkl_rgb'), dir(self)
             win=None
             logging.error("_setColor() is being applied to something that has no known Window object")
     #convert new self.color to rgb space
@@ -6703,7 +6697,6 @@ def getMsPerFrame(myWin, nFrames=60, showVisual=False, msg='', msDelay=0.):
     msdrawAvg = 1000. * float(numpy.average(drawTimes))
     msdrawSD = 1000. * float(numpy.std(drawTimes))
     msfree = 1000. * float(numpy.average(freeTimes))
-    #print "draw=%.1fms free=%.1fms pad=%.1fms" % (msdrawAvg,msfree,msDelay)
 
     return msPFavg, msPFstd, msPFmed #, msdrawAvg, msdrawSD, msfree
 
