c7c29bf10842dacf1a76546fc44dbff30671c686
William Högman <william@hogman.me>
2012-03-13 01:13:19 +0100

    BF: DataHandler.addDataType no longer tries loop over unicode strings

--- psychopy/data.py
+++ psychopy/data.py
@@ -2375,7 +2375,7 @@ class DataHandler(dict):
         during initialisation and as each xtra type is needed.
         """
         if not shape: shape = self.dataShape
-        if type(names) != str:
+        if not isinstance(names,basestring):
             #recursively call this function until we have a string
             for thisName in names: self.addDataType(thisName)
         else:
