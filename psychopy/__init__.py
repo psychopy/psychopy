15253c39d97d6b7f0ae1bb5922a9a32e04395175
Jon Peirce <jon@peirce.org.uk>
2012-04-22 21:14:25 +0100

    DOC: changelog and version number for new release

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -7,7 +7,7 @@
 #--------------------------------------------------------------------------
 
 #version info for PsychoPy
-__version__='1.73.04'
+__version__='1.73.06'
 __license__='GNU GPLv3 (or more recent equivalent)'
 __author__='Jonathan Peirce'
 __author_email__='jon@peirce.org.uk'
