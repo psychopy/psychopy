6b678bc137f09b1fae338b2d1753e5f3b070f7e7
Jonathan Peirce <jon@peirce.org.uk>
2012-06-21 15:40:04 +0100

    tests: the extra imports now only run if the username indicates that you are Jon!
    The extra imports are only important when building the standalone distributions
    (which nobody else does).

--- psychopy/tests/test_Installation.py
+++ psychopy/tests/test_Installation.py
@@ -1,4 +1,5 @@
 import sys
+from psychopy.tests import utils
 
 def test_essential_imports():
     import wx
@@ -9,17 +10,23 @@ def test_essential_imports():
     import pyglet
     import OpenGL
     import openpyxl
+    import lxml
 
 def test_extra_imports():
+    import os, pwd
+    user = pwd.getpwuid(os.getuid()).pw_name
+    if user not in ['jwp','lpzjwp']:
+        utils.skip('Testing extra imports is only needed for building Standalone distributions')
     import serial
     import pp
     import egi
-    try: import labjack
-    except: import u3, u6, ue9, LabJackPython
+    try:
+        import labjack
+    except:
+        import u3, u6, ue9, LabJackPython
     import pylink
     import ioLabs
     import hid
-    import lxml
     #avbin
     import pyglet
     assert pyglet.media.have_avbin
