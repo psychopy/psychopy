a86ef96ed51b0733d7d1886631feb4aa5d672018
Piotr Iwaniuk <piwaniuk@poczta.onet.pl>
2012-08-13 14:00:53 +0200

    Fixed style. Added stub for sending tags from experiment code.

--- psychopy/contrib/obci.py
+++ psychopy/contrib/obci.py
@@ -4,15 +4,15 @@ class OBCIWindow(visual.Window):
     def __init__(self, *args, **kwargs):
         self._toTag = []
         try:
-          self.obciContext = kwargs["obciContext"]
-        except KeyError, e:
-          raise Excpetion("Missing obciContext argument")
+            self.obciContext = kwargs["obciContext"]
+        except KeyError:
+            raise Exception("Missing obciContext argument")
         
         super(OBCIWindow, self).__init__(*args, **kwargs)
         # TODO check whether obciContext is instance of ExpsHelper?
     
     def tagOnFlip(self, name):
-      self._toTag.append(str(name))
+        self._toTag.append(str(name))
     
     def doFlipLogging(self, now):
         super(OBCIWindow, self).doFlipLogging(now)
@@ -21,3 +21,15 @@ class OBCIWindow(visual.Window):
         for tagEntry in self._toTag:
             self.obciContext.send_tag(now, now, tagEntry)
         self._toTag = []
+
+class TagOnFlip(object):
+    def __init__(self, window, doTag = True, tagName = "tag",
+            doSignal = False, signalByte = 'A'):
+        self.window = window
+        self.status = None
+        self.doTag = doTag
+        self.tagName = tagName
+        self.doSignal = doSignal
+        self.signalByte = signalByte
+    def schedule(self):
+        pass
