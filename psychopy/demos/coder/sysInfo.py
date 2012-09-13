93ad5caf7a6c56222823cdf5a277df5c4cc72891
Jeremy Gray <jrgray@gmail.com>
2012-09-13 10:47:41 -0400

    ENH: sysinfo.py reports pyo version

--- psychopy/demos/coder/sysInfo.py
+++ psychopy/demos/coder/sysInfo.py
@@ -15,6 +15,7 @@ import numpy; print "numpy", numpy.__version__
 import scipy; print "scipy", scipy.__version__
 import matplotlib; print "matplotlib", matplotlib.__version__
 import pyglet; print "pyglet", pyglet.version
+import pyo; print "pyo", '.'.join(map(str, pyo.getVersion()))
 import psychopy; print "PsychoPy", psychopy.__version__
 
 win = visual.Window([100,100])#some drivers want a window open first
