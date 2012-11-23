e8e95da63022217d1744308848fd4ec3103ed83f
Henrik Singmann <henrik.singmann (at) psychologie.uni-freiburg.de>
2012-11-23 11:19:46 +0100

    RatingScale choices (builder) keep old behavior

--- psychopy/app/builder/experiment.py
+++ psychopy/app/builder/experiment.py
@@ -340,6 +340,10 @@ class Experiment:
                 component=getAllComponents()[componentType](\
                     name=componentNode.get('name'),
                     parentName=routineNode.get('name'), exp=self)
+                # check for components that were absent in later versions of the builder and change the default behavior (currently only the new behavior of choices for RatingScale, HS, November 2012)
+                if componentType=='RatingScaleComponent':
+                    if not componentNode.get('choiceLabelsAboveLine'): #this rating scale was created using older version of psychopy
+                        component.params['choiceLabelsAboveLine'].val=True  #important to have .val here
                 #populate the component with its various params
                 for paramNode in componentNode:
                     self._getXMLparam(params=component.params, paramNode=paramNode)
