1b6ad7d282fa7b058ad2418def8531db57f163f3
Piotr Iwaniuk <piotr.iwaniuk@titanis.pl>
2012-12-14 14:58:54 +0100

    Fixed error when saving exported file. refs #1147

--- psychopy/app/builder/resource_pool.py
+++ psychopy/app/builder/resource_pool.py
@@ -92,7 +92,7 @@ class ResourcePoolDialog(wx.Frame):
         if name:
             file_name = wx.FileSelector("Export file", flags=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT, parent=self)
             exported_file = open(file_name, "wb")
-            exported_file.write(self.pool.get_content(name))
+            exported_file.write(self.pool.get_resource(name).get_content())
             exported_file.close()
             print file_name
 
