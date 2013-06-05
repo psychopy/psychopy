e7ad944f8a21484605a0a753a18207e8b007c23a
Yaroslav Halchenko <debian@onerussian.com>
2013-06-04 21:25:33 -0400

    ENH: use conftest.py and pytest_configure to setup a fixture to start the app in non-interactive mode
    this is py.test specific solution but should be more robust.  I have removed various hanging around setup*s:
    setup_module seems to be not invoked whenever testing only submodule etc
    
    also avoiding use of globals here by assigning to the actual app class ;)

---
+++ psychopy/tests/test_app/conftest.py
@@ -0,0 +1,18 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+
+"""
+py.test fixtures to create an instance of PsychoPyApp for testing
+"""
+from psychopy.app.psychopyApp import PsychoPyApp
+
+def pytest_configure(config):
+    # print "D: CREATING APP"
+    PsychoPyApp._called_from_test = True
+    PsychoPyApp._app = PsychoPyApp(interactive=False)
+    print "D: CREATED APP"
+    #del PsychoPyApp._called_from_test
+
+def pytest_unconfigure(config):
+    # print "D: KILLING THE APP"
+    PsychoPyApp._app.quit() # this currently uses sys.exit() which ends testing :-(
