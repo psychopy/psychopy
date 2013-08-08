097dec1a2327b028269de11a55d3aacda06080cb
Jeremy R. Gray <jrgray@gmail.com>
2013-08-07 20:10:51 -0400

    BF: RatingScale component: set low/high + anchorText
    - low/high could be wrong if anchorText was also given
    - fixed some code/str issues; $ and \$ were not working right

--- psychopy/app/builder/components/ratingscale.py
+++ psychopy/app/builder/components/ratingscale.py
@@ -109,10 +109,10 @@ class RatingScaleComponent(BaseComponent):
             hint="Should accepting a rating cause the end of the routine (e.g. trial)?")
         self.params['lowAnchorText'] = Param(lowAnchorText, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=[],
-            hint="Description of the low end of the scale")
+            hint="Description of the low end of the scale; to hide it, set it to a space.")
         self.params['highAnchorText'] = Param(highAnchorText, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=[],
-            hint="Description of the high end of the scale")
+            hint="Description of the high end of the scale; to hide it, set it to a space.")
         self.params['customize_everything'] = Param(customize_everything, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=[],
             hint="Use this text to create the rating scale as you would in a code component; overrides all"+
@@ -159,26 +159,23 @@ class RatingScaleComponent(BaseComponent):
                 if self.params['choiceLabelsAboveLine'].val:
                     init_str += ', labels=False'
             else:
-                # low/lowAnchorText
+                # try to add low as int; but might be a var instead
+                try:
+                    init_str += ', low=%d' % int(self.params['low'].val)
+                except ValueError:
+                    if self.params['low'].val:
+                        init_str += ", low=%s" % self.params['low']
                 if len(self.params['lowAnchorText'].val):
-                    init_str += ", lowAnchorText='"+self.params['lowAnchorText'].val+"'"
-                else:
-                    try:
-                        int(self.params['low'].val)
-                        init_str += ', low='+self.params['low'].val
-                    except ValueError:
-                        if len(self.params['low'].val):
-                            init_str += ", lowAnchorText='"+self.params['low'].val+"'"
+                    init_str += ", lowAnchorText=%s" % self.params['lowAnchorText']
+
                 # high/highAnchorText
-                if len(self.params['highAnchorText'].val):
-                    init_str += ", highAnchorText='"+self.params['highAnchorText'].val+"'"
-                else:
-                    try:
-                        int(self.params['high'].val)
-                        init_str += ', high='+self.params['high'].val
-                    except ValueError:
-                        if len(self.params['high'].val):
-                            init_str += ", highAnchorText='"+self.params['high'].val+"'"
+                try:
+                    init_str += ', high=%d' % int(self.params['high'].val)
+                except ValueError:
+                    if len(self.params['high'].val):
+                        init_str += ", high=%s" % self.params['high']
+                if self.params['highAnchorText'].val:
+                    init_str += ", highAnchorText=%s" % self.params['highAnchorText']
 
             if not len(choices) and len(unicode(self.params['scaleDescription'])):
                 init_str += ", scale=%s" % self.params['scaleDescription']
