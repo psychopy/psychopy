1b31f95d3b9750debc510e5a570036dade9266b4
Sol Simpson <sds-git@isolver-software.com>
2013-10-11 07:39:08 -0400

    ENH: Added ioHub Touch device
    - Currently support for Elo Touch Screens using the serial interface.
    - Serial -> USB converter cable can be used.
    - Allows independent monitoring of touch screen events from that of the
    Mouse device; so both devices can be used in parallel and generate
    different event streams.

--- psychopy/iohub/constants.py
+++ psychopy/iohub/constants.py
@@ -109,6 +109,11 @@ try:
         MOUSE_MOVE=36
         MOUSE_DRAG=37
         
+        TOUCH=40
+        TOUCH_MOVE=41
+        TOUCH_PRESS=42
+        TOUCH_RELEASE=43
+        
         EYETRACKER=50
         MONOCULAR_EYE_SAMPLE=51
         BINOCULAR_EYE_SAMPLE=52
@@ -160,6 +165,15 @@ try:
 
             #: Constant for a Mouse Drag Event.
             MOUSE_DRAG=37
+
+            #: Constant for a Touch motion Event.
+            TOUCH_MOVE=41
+
+            #: Constant for an initial Touch press Event.
+            TOUCH_PRESS=42
+
+            #: Constant for a Touch release Event.
+            TOUCH_RELEASE=43
             
             #: Constant for an Eye Tracker Monocular Sample Event.
             MONOCULAR_EYE_SAMPLE=51
@@ -249,6 +263,7 @@ try:
         OTHER = 1
         KEYBOARD = 20
         MOUSE = 30
+        TOUCH=40
         EYETRACKER = 50
         NETWORK=60
         EVENTPUBLISHER=61
@@ -272,6 +287,9 @@ try:
 
             #: Constant for a Mouse Device.
             MOUSE = 30
+            
+            #: Constant for a Touch Device.
+            TOUCH = 40
 
             #: Constant for an EyeTracker Device.
             EYETRACKER = 50
