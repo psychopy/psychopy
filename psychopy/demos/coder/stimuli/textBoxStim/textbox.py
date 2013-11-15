ec78505231b03195137971b2862ceb9402b2e74f
Sol Simpson <sds-git@isolver-software.com>
2013-11-15 10:08:19 -0500

    updated textbox.py demo
    this example no longer provides the font_name to use, so the textbox
    uses  one of the fonts found on the system.

--- psychopy/demos/coder/stimuli/textBoxStim/textbox.py
+++ psychopy/demos/coder/stimuli/textBoxStim/textbox.py
@@ -14,36 +14,16 @@ window=visual.Window(display_resolution,
                         screen=0
                         )
 
-fm=visual.textbox.getFontManager()
-available_font_names=fm.getFontFamilyStyles()
-prefered_fonts=[fn for fn,fs in available_font_names if fn in [
-                                                            'Courier New',
-                                                            'Consolas',
-                                                            'Lucida Sans Typewriter',
-                                                            'Ubuntu Mono',
-                                                            'DejaVu Sans Mono',
-                                                            'Bitstream Vera Sans Mono',
-                                                            'Luxi Mono']]
-if len(prefered_fonts)==1:
-    font_name1=prefered_fonts[0]
-    font_name2=available_font_names[1][0] 
-elif len(prefered_fonts)>1:
-    font_name1=prefered_fonts[0]
-    font_name2=prefered_fonts[1] 
-else:
-    font_name1=available_font_names[0][0]   
-    font_name2=available_font_names[1][0]   
-                                                           
+# Create two textBox stim, each using different parameters supported by
+# Textbox. Note that since no font_name is provided when creating the
+# textbox stim, a default font is selected by TextBox stim automatically.  
+#                                                         
 textbox=visual.TextBox(window=window, 
                          text='This is the plain TextBox UX.', 
-                         font_name=font_name1,
                          bold=False,
                          italic=False,
-                         font_size=32,
+                         font_size=18,
                          font_color=[-1,-1,1], 
-                         #grid_color=[-1,-1,-1,1],
-                         #grid_stroke_width=1,
-                         #background_color=[0,.25,.9,1],
                          size=(1.9,.5),
                          pos=(0.0,0.5), 
                          units='norm',
@@ -53,10 +33,9 @@ textbox=visual.TextBox(window=window,
 
 textbox2=visual.TextBox(window=window,
                          text='This TextBox is using the different UX features.', 
-                         font_name=font_name2,
                          bold=False,
                          italic=False,
-                         font_size=36,
+                         font_size=32,
                          font_color=[1,-1,-1], 
                          background_color=[-1,-1,-1,1],
                          border_color=[-1,-1,1,1],
