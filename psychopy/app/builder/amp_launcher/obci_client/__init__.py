5c2fb06237e77cdfc7a25934cf0475782c2c749a
Joanna Tustanowska <joanna.tustanowska@titanis.pl>
2014-01-22 16:28:25 +0100

    missing default param in peer config for obci experiment

--- psychopy/app/builder/amp_launcher/obci_client/__init__.py
+++ psychopy/app/builder/amp_launcher/obci_client/__init__.py
@@ -132,7 +132,8 @@ class ExperimentSettings(object):
                     "sentry_log_level": "info",
                     "file_log_level": "info",
                     "mx_log_level": "info",
-                    "log_dir": "~/.obci/logs"
+                    "log_dir": "~/.obci/logs",
+                    "experiment_uuid": ""
                 }
             },
             'path': self.amp_config["exec_file"]
