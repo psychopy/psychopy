f3a70ca238f54b9d2b45379f0644ff3bb34abc9c
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2014-03-10 23:58:13 +0100

    DOC: Fix two typos in documentation

--- psychopy/app/_psychopyApp.py
+++ psychopy/app/_psychopyApp.py
@@ -152,9 +152,9 @@ class PsychoPyApp(wx.App):
         #then override the prev files by command options and passed files
         if len(sys.argv)>1:
             if sys.argv[1]==__name__:
-                args = sys.argv[2:] # program was excecuted as "python.exe PsychoPyIDE.py %1'
+                args = sys.argv[2:] # program was executed as "python.exe PsychoPyIDE.py %1'
             else:
-                args = sys.argv[1:] # program was excecuted as "PsychoPyIDE.py %1'
+                args = sys.argv[1:] # program was executed as "PsychoPyIDE.py %1'
             #choose which frame to start with
             if args[0] in ['builder', '--builder', '-b']:
                     mainFrame='builder'
