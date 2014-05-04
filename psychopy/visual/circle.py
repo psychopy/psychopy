4b553323cc03e8eb2b04e5a718cd90290dfece67
Jonas Lindeløv <jonas@cnru.dk>
2014-05-04 15:59:58 +0100

    RF: finished attributeSetters for Polygon and Circle. radius and edges are now solely defined in Polygon.

--- psychopy/visual/circle.py
+++ psychopy/visual/circle.py
@@ -8,12 +8,8 @@ as a special case of a :class:`~psychopy.visual.Polygon`'''
 # Distributed under the terms of the GNU General Public License (GPL).
 
 import psychopy  # so we can get the __path__
-from psychopy import logging
 
 from psychopy.visual.polygon import Polygon
-from psychopy.tools.attributetools import logAttrib, attributeSetter
-
-import numpy
 
 
 class Circle(Polygon):
@@ -24,12 +20,6 @@ class Circle(Polygon):
     def __init__(self, win, radius=.5, edges=32, **kwargs):
         """
         Circle accepts all input parameters that `~psychopy.visual.ShapeStim` accept, except for vertices and closeShape.
-
-        :Parameters:
-
-            edges : float or int (default=32)
-                Specifies the resolution of the polygon that is approximating the
-                circle.
         """
         #what local vars are defined (these are the init params) for use by __repr__
         self._initParams = dir()
@@ -37,28 +27,8 @@ class Circle(Polygon):
         #kwargs isn't a parameter, but a list of params
         self._initParams.remove('kwargs')
         self._initParams.extend(kwargs)
-        self.edges = edges
 
         #initialise parent class
         kwargs['edges'] = edges
         kwargs['radius'] = radius
         super(Circle, self).__init__(win, **kwargs)
-    
-    @attributeSetter
-    def radius(self, radius):
-        """float, int, tuple, list or 2x1 array
-        Radius of the Circle (distance from the center to the corners).
-        If radius is a 2-tuple or list, the values will be interpreted as semi-major and
-        semi-minor radii of an ellipse.
-
-        :ref:`Operations <attrib-operations>` supported.
-        
-        Usually there's a setAttribute(value, log=False) method for each attribute. Use this if you want to disable logging."""
-        self.__dict__['radius'] = numpy.asarray(radius)
-        self._calcVertices()
-        self.setVertices(self.vertices, log=False)
-    def setRadius(self, radius, log=True):
-        """Usually you can use 'stim.attribute = value' syntax instead,
-        but use this method if you need to suppress the log message"""
-        self.radius = radius
-        logAttrib(self, log, 'radius', radius)
