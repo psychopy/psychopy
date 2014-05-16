1e79f6ac5a750bfbd9e44810ac2b2b677eefef3d
Jonas Lindeløv <jonas@cnru.dk>
2014-05-16 21:49:52 +0200

    RF: created setAttribute() function which will eventually replace setWithOperation() and callAttributeSetter() with a few bugfixes/enhancements.

--- psychopy/tools/attributetools.py
+++ psychopy/tools/attributetools.py
@@ -32,6 +32,83 @@ class attributeSetter(object):
     def __repr__(self):
         return repr(self.__getattribute__)
 
+def setAttribute(self, attrib, value, log, operation=False, stealth=False):
+    """
+    This function is useful to direct the old set* functions to the
+    @attributeSetter. It has the same functionality but supports logging control 
+    as well, making it useful for cross-@attributeSetter calls as well.
+    
+    Typical usage: e.g. in setSize(self, value, operation, log=None) do::
+    
+        def setSize(self, value, operation, log=None):
+            setAttribute(self, 'size', value, log, operation)  # call attributeSetter
+    
+    Sets an object property (scalar or numpy array), optionally with an operation
+    given in a string. If operation is None or '', value is multiplied with old 
+    to keep shape.
+    
+    If stealth is True, then use self.__dict[key] = value and avoid calling attributeSetters. 
+    If stealth is False, use setattr(). autoLog controls the value of autoLog during this setattr().    
+    
+    History: introduced in version 1.79 to avoid exec-calls. Even though it looks
+    complex, it is very fast :-)"""
+    
+    # Change the value of "value" if there is an operation. Even if it is '',
+    # that indicates that this value could potentially be subjected to an operation.
+    if operation is not False:
+        try:
+            oldValue = getattr(self, attrib)
+            oldValue = numpy.asarray(oldValue, float)
+    
+            # Calculate new value using operation
+            if operation in ('', None):
+                value = oldValue * 0 + value  # Preserves dimensions, if array
+            elif operation == '+':
+                value = oldValue + value
+            elif operation == '*':
+                value = oldValue * value
+            elif operation == '-':
+                value = oldValue - value
+            elif operation == '/':
+                value = oldValue / value
+            elif operation == '**':
+                value = oldValue ** value
+            elif operation == '%':
+                value = oldValue % value
+            else:
+                raise ValueError('Unsupported value "', operation, '" for operation when setting', attrib, 'in', self.__class__.__name__)
+    
+        except AttributeError:
+            # attribute is not set yet. Do it now in a non-updating manner
+            value = numpy.asarray(value, float)
+        except TypeError:
+            # Attribute is "None" or an unset attributeSetter. This is a sign that we are just initing
+            if oldValue is None or isinstance(oldValue, attributeSetter) and operation in ('', None):
+                value = numpy.asarray(value, float)
+            elif value is None:
+                # Not an operation, but let's be friendly...
+                value = value
+            else:
+                raise TypeError
+        except ValueError:
+            # The old value is a string, typical of a color change from named to e.g. rgb.
+            if type(oldValue) is str and operation in ('', None):
+                value = numpy.asarray(value, float)
+            else:
+                raise ValueError
+    # Ok, operation or not, change the attribute in self without callback to attributeSetters
+    if stealth:
+        self.__dict__[attrib] = value  # without logging as well
+    else:
+        # Trick to control logging of attributeSetter. Set logging in self.autoLog
+        autoLogOrig = self.autoLog  # save original value
+        self.__dict__['autoLog'] = log or autoLogOrig and log is None  # set to desired logging. log=None dafaults to autoLog
+        setattr(self, attrib, value)  # set attribute, calling attributeSetter if it exists
+        print attrib, value
+        if attrib != 'autoLog':  # hack: if attrib was 'autoLog', do not set it back to original value!
+            self.__dict__['autoLog'] = autoLogOrig  # return autoLog to original
+        
+
 def callAttributeSetter(self, attrib, value, log=None):
     """Often, the get*() functions just add the ability to log compared to attributeSetter.
     As attributeSetter respects self.autoLog, we can do the following to control logging"""
