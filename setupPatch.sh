e67acec3378f7f0376808a346e0506fce66655cc
Jon Peirce <jon.peirce@gmail.com>
2014-06-05 13:31:08 +0100

    DOCS: bumped version number for release 1.80.06

--- setupPatch.sh
+++ setupPatch.sh
@@ -5,7 +5,8 @@ python setup.py sdist --format=zip
 python setup.py egg_info
 python setup.py bdist_egg
 
-echo register with;
-echo python setup.py register
+echo upload to pypi with;
+echo python setup.py sdist --format==zip upload
+echo python setup.py bdist_egg upload
 
 mv dist/* ../dist
