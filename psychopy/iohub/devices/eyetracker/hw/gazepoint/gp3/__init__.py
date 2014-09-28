12b21ba6057b7a4005b8813b57d0d7a51383e9d7
Sol Simpson <sds-git@isolver-software.com>
2014-09-28 00:34:29 -0400

    ENH: Updated GP3 iohub interface
    eye tracker interface class cleanup.

--- psychopy/iohub/devices/eyetracker/hw/gazepoint/gp3/__init__.py
+++ psychopy/iohub/devices/eyetracker/hw/gazepoint/gp3/__init__.py
@@ -1,13 +1,1 @@
-"""
-ioHub
-Common Eye Tracker Interface for TheEyeTribe system.
-.. file: ioHub/devices/eyetracker/hw/theeyetribe/__init__.py
-
-Copyright (C) 2012-2014 iSolver Software Solutions
-Distributed under the terms of the GNU General Public License (GPL version 3 or any later version).
-
-.. moduleauthor:: ??
-.. fileauthor:: ??
-"""
-
 from eyetracker import *
