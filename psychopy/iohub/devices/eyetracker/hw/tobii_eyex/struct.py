51fa791ff77788f174b4c2506d0f62ed86f5deaa
Dan Fitch <dgfitch@gmail.com>
2014-12-12 13:22:53 -0600

    Refactor structs in preparation for more

--- psychopy/iohub/devices/eyetracker/hw/tobii_eyex/struct.py
+++ psychopy/iohub/devices/eyetracker/hw/tobii_eyex/struct.py
@@ -48,40 +48,53 @@ class UsbDeviceInfo(Structure):
         ("firmware_version", c_char * 128)
     ]
                
-class TobiigazePoint3d(Structure):
+class TobiiGazePoint3d(Structure):
     
-    _fields_ = [("x", c_double), ("y", c_double), ("z", c_double)]
+    _fields_ = [
+        ("x", c_double),
+        ("y", c_double),
+        ("z", c_double)
+    ]
 
-class TobiigazePoint2d(Structure):
+class TobiiGazePoint2d(Structure):
     
-    _fields_ = [("x", c_double), ("y", c_double)]
+    _fields_ = [
+        ("x", c_double),
+        ("y", c_double)
+    ]
 
 class TobiiGazeDataEye(Structure):
     
-    _fields_ = [("eye_position_from_eye_tracker_mm", TobiigazePoint3d),
-                ("eye_position_in_track_box_normalized", TobiigazePoint3d),
-                ("gaze_point_from_eye_tracker_mm", TobiigazePoint3d),
-                ("gaze_point_on_display_normalized", TobiigazePoint3d)
-                ]
+    _fields_ = [
+        ("eye_position_from_eye_tracker_mm", TobiiGazePoint3d),
+        ("eye_position_in_track_box_normalized", TobiiGazePoint3d),
+        ("gaze_point_from_eye_tracker_mm", TobiiGazePoint3d),
+        ("gaze_point_on_display_normalized", TobiiGazePoint3d)
+    ]
     
 class TobiiGazeData(Structure):
     
-    _fields_ = [("timestamp", c_uint64), ("tracking_status", c_uint),
-                ("left", TobiiGazeDataEye), 
-                ("right", TobiiGazeDataEye)
-                ]
+    _fields_ = [
+        ("timestamp", c_uint64),
+        ("tracking_status", c_uint),
+        ("left", TobiiGazeDataEye), 
+        ("right", TobiiGazeDataEye)
+    ]
 
 class TobiiGazeDataExtension(Structure):
     
-    _fields_ = [("column_id", c_uint32), ("data", c_uint8 * 256), 
-                ("actual_size", c_uint32)
-                ]
+    _fields_ = [
+        ("column_id", c_uint32), 
+        ("data", c_uint8 * 256), 
+        ("actual_size", c_uint32)
+        ]
 
 
 class TobiiGazeDataExtensions(Structure):
-    _fields_ = [("extensions", 
-                 TobiiGazeDataExtension * TOBIIGAZE_MAX_GAZE_DATA_EXTENSIONS), 
-                 ("actual_size", c_uint32)
-                 ]
+    _fields_ = [
+        ("extensions", 
+        TobiiGazeDataExtension * TOBIIGAZE_MAX_GAZE_DATA_EXTENSIONS), 
+        ("actual_size", c_uint32)
+    ]
 
 
