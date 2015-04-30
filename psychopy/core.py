cb228101951973afdc921178cf82fb4b26d6f86f
Sol Simpson <sol@isolver-software.com>
2015-04-30 12:55:19 -0400

    BF: non existent import when reverted a change

--- psychopy/core.py
+++ psychopy/core.py
@@ -5,7 +5,7 @@
 # Distributed under the terms of the GNU General Public License (GPL).
 
 import sys, threading
-from clock import MonotonicClock, Clock, CountdownTimer, EllapsedTimeClock, wait, monotonicClock, getAbsTime
+from clock import MonotonicClock, Clock, CountdownTimer, wait, monotonicClock, getAbsTime
 # always safe to call rush, even if its not going to do anything for a particular OS
 from psychopy.platform_specific import rush
 from . import logging
