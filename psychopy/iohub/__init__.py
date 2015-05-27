dca2fda17d8375b4a09aa0231867032f7dfe0bb0
Jon Peirce <jon.peirce@gmail.com>
2015-05-27 13:12:24 +0100

    DOCS: added description for iohub __init__
    fixes #906

--- psychopy/iohub/__init__.py
+++ psychopy/iohub/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""iohub provides an integrated framework to interact with input and output
+devices (from keyboards and mice to eyetrackers and custom hardware) in a 
+precise synchronised manner that will not interfere with stimulus presentation
+"""
+
 # ioHub Python Module
 # .. file: psychopy/iohub/__init__.py
 #
