ca82fd4def14a91f821eacd45e6d6978105286a3
Jeremy R. Gray <jrgray@gmail.com>
2016-01-25 11:48:27 -0500

    WIP: figuring out test failure
    weirdly unable to commit a new version of componsTemplate.txt

--- psychopy/tests/test_app/test_builder/test_components.py
+++ psychopy/tests/test_app/test_builder/test_components.py
@@ -11,6 +11,7 @@ from psychopy.app.builder.components import getAllComponents
 
 # what reference to use?
 profile = 'componsTemplate.txt'
+profile = 'c.txt'
 
 # always ignore hints, labels, and categories. other options:
 # should it be ok or an error if the param[field] order differs from the profile?
