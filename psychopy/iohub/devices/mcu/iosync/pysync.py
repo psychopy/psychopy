5d58176cadc31067e82bfa734bf11af144ce36dc
Sol Simpson <sol@isolver-software.com>
2016-04-02 20:15:57 -0400

    Adding missed file updates for iosync keybaord

--- psychopy/iohub/devices/mcu/iosync/pysync.py
+++ psychopy/iohub/devices/mcu/iosync/pysync.py
@@ -2,7 +2,7 @@
 # Part of the psychopy.iohub library.
 # Copyright (C) 2012-2016 iSolver Software Solutions
 # Distributed under the terms of the GNU General Public License (GPL).
-
+from __future__ import division, absolute_import
 """
 Python Interface for ioSync. ioSync uses a Teensy 3 / 3.1 microcontroller
 to provide a low cost solution for digital and analog inputs, as well as digital
@@ -105,15 +105,25 @@ PWM Outputs:
     PMW_3: 23   // digital pin 23 on T3
 
 """
+from collections import OrderedDict
+import struct
 
 import serial
 import numpy as np
-from collections import OrderedDict
+
 from ... import Computer
 from ....errors import print2err
+from ....util import NumPyRingBuffer as RingBuffer
+
+from .t3keymap import char2t3code
 
 getTime = Computer.getTime
 
+
+_t3key_format = struct.Struct('!HB')
+_keycode2bytes = dict()
+for ctckey, ctcshort in char2t3code.items():
+    _keycode2bytes[ctckey] = bytearray(_t3key_format.pack(ctcshort, 0))
 
 class T3Event(object):
     DIGITAL_INPUT_EVENT = 1
@@ -363,14 +373,22 @@ class GetT3AnalogInputStateRequest(T3Request):
 class GenerateKeyboardEventRequest(T3Request):
     """
     Requests the Teensy to generate a key press event and then a release event
-    press_duration*100 msec later.use_char is not actually used currently.
-    The
+    press_duration*100 msec later. key_symbol must be one of the str keys found
+    in the iohub.devices.mcu.iosync.t3keymap.char2t3code dict.
     """
 
-    def __init__(self, use_char='v', press_duration=15):
-        T3Request.__init__(
-            self, T3Request.GENERATE_KEYBOARD_EVENT, [
-                use_char, press_duration])
+    def __init__(self, key_symbol, press_duration=15):
+        ba3 = _keycode2bytes.get(key_symbol, None)
+        if ba3 is None:
+            estr = "GenerateKeyboardEventRequest: {} is not a valid key_symbol.".format(key_symbol)
+            raise ValueError(estr)
+        if press_duration < 0 or press_duration > 255:
+            estr = "GenerateKeyboardEventRequest: {} is not a valid press_duration.".format(press_duration)
+            raise ValueError(estr)
+        # First 2 bytes of ba3 are the teensy 3 key code mapping.
+        # Update 3rd byte to hold the press_duration.
+        ba3[2]=press_duration
+        T3Request.__init__(self, T3Request.GENERATE_KEYBOARD_EVENT, ba3)
 
 
 class SetT3DigitalOutputStateRequest(T3Request):
@@ -626,210 +644,6 @@ class T3MC(object):
 
     def __del__(self):
         self.close()
-
-
-# RingBuffer
-
-class RingBuffer(object):
-    """NumPyRingBuffer is a circular buffer implemented using a one dimensional
-    numpy array on the backend. The algorithm used to implement the ring buffer
-    behavour does not require any array copies to occur while the ring buffer
-    is maintained, while at the same time allowing sequential element access
-    into the numpy array using a subset of standard slice notation.
-
-    When the circular buffer is created, a maximum size , or maximum
-    number of elements,  that the buffer can hold *must* be specified. When
-    the buffer becomes full, each element added to the buffer removes the oldest
-    element from the buffer so that max_size is never exceeded.
-
-    Items are added to the ring buffer using the classes append method.
-
-    The current number of elements in the buffer can be retrieved using the
-    getLength() method of the class.
-
-    The isFull() method can be used to determine if
-    the ring buffer has reached its maximum size, at which point each new element
-    added will disregard the oldest element in the array.
-
-    The getElements() method is used to retrieve the actual numpy array containing
-    the elements in the ring buffer. The element in index 0 is the oldest remaining
-    element added to the buffer, and index n (which can be up to max_size-1)
-    is the the most recent element added to the buffer.
-
-    Methods that can be called from a standard numpy array can also be called using the
-    NumPyRingBuffer instance created. However Numpy module level functions will not accept
-    a NumPyRingBuffer as a valid arguement.
-
-    To clear the ring buffer and start with no data in the buffer, without
-    needing to create a new NumPyRingBuffer object, call the clear() method
-    of the class.
-
-    Example::
-
-        ring_buffer=RingBuffer(10)
-
-        for i in xrange(25):
-            ring_buffer.append(i)
-            print '-------'
-            print 'Ring Buffer Stats:'
-            print '\tWindow size: ',len(ring_buffer)
-            print '\tMin Value: ',ring_buffer.min()
-            print '\tMax Value: ',ring_buffer.max()
-            print '\tMean Value: ',ring_buffer.mean()
-            print '\tStandard Deviation: ',ring_buffer.std()
-            print '\tFirst 3 Elements: ',ring_buffer[:3]
-            print '\tLast 3 Elements: ',ring_buffer[-3:]
-
-    """
-
-    def __init__(self, max_size, dtype=np.float32):
-        self._dtype = dtype
-        self._npa = np.empty(max_size * 2, dtype=dtype)
-        self.max_size = max_size
-        self._index = 0
-
-    def append(self, element):
-        """Add element e to the end of the RingBuffer. The element must match
-        the numpy data type specified when the NumPyRingBuffer was created. By
-        default, the RingBuffer uses float32 values.
-
-        If the Ring Buffer is full, adding the element to the end of the array
-        removes the currently oldest element from the start of the array.
-
-        :param numpy.dtype element: An element to add to the RingBuffer.
-        :returns None:
-
-        """
-        i = self._index
-        self._npa[i % self.max_size] = element
-        self._npa[(i % self.max_size) + self.max_size] = element
-        self._index += 1
-
-    def getElements(self):
-        """Return the numpy array being used by the RingBuffer, the length of
-        which will be equal to the number of elements added to the list, or the
-        last max_size elements added to the list. Elements are in order of
-        addition to the ring buffer.
-
-        :param None:
-        :returns numpy.array: The array of data elements that make up the Ring Buffer.
-
-        """
-        return self._npa[
-            self._index %
-            self.max_size:(
-                self._index %
-                self.max_size) + self.max_size]
-
-    def isFull(self):
-        """Indicates if the RingBuffer is at it's max_size yet.
-
-        :param None:
-        :returns bool: True if max_size or more elements have been added to the RingBuffer; False otherwise.
-
-        """
-        return self._index >= self.max_size
-
-    def clear(self):
-        """Clears the RingBuffer. The next time an element is added to the
-        buffer, it will have a size of one.
-
-        :param None:
-        :returns None:
-
-        """
-        self._index = 0
-
-    def __setitem__(self, indexs, v):
-        if isinstance(indexs, (list, tuple)):
-            for i in indexs:
-                if isinstance(i, (int, long)):
-                    i = i + self._index
-                    self._npa[i % self.max_size] = v
-                    self._npa[(i % self.max_size) + self.max_size] = v
-                elif isinstance(i, slice):
-                    istart = indexs.start
-                    if istart is None:
-                        istart = 0
-                    istop = indexs.stop
-                    if indexs.stop is None:
-                        istop = 0
-                    start = istart + self._index
-                    stop = istop + self._index
-                    self._npa[
-                        slice(
-                            start %
-                            self.max_size,
-                            stop %
-                            self.max_size,
-                            i.step)] = v
-                    self._npa[
-                        slice(
-                            (start %
-                             self.max_size) + self.max_size, (stop %
-                                                              self.max_size) + self.max_size, i.step)] = v
-        elif isinstance(indexs, (int, long)):
-            i = indexs + self._index
-            self._npa[i % self.max_size] = v
-            self._npa[(i % self.max_size) + self.max_size] = v
-        elif isinstance(indexs, slice):
-            istart = indexs.start
-            if istart is None:
-                istart = 0
-            istop = indexs.stop
-            if indexs.stop is None:
-                istop = 0
-            start = istart + self._index
-            stop = istop + self._index
-            self._npa[
-                slice(
-                    start %
-                    self.max_size,
-                    stop %
-                    self.max_size,
-                    indexs.step)] = v
-            self._npa[
-                slice(
-                    (start %
-                     self.max_size) +
-                    self.max_size,
-                    (stop %
-                     self.max_size) +
-                    self.max_size,
-                    indexs.step)] = v
-        else:
-            raise TypeError()
-
-    def __getitem__(self, indexs):
-        current_array = self.getElements()
-        if isinstance(indexs, (list, tuple)):
-            rarray = []
-            for i in indexs:
-                if isinstance(i, (int, long)):
-                    rarray.append(current_array[i])
-                elif isinstance(i, slice):
-                    rarray.extend(current_array[i])
-            return np.asarray(rarray, dtype=self._dtype)
-        elif isinstance(indexs, (int, long, slice)):
-            return current_array[indexs]
-        else:
-            raise TypeError()
-
-    def __getattr__(self, a):
-        if self._index < self.max_size:
-            return getattr(self._npa[:self._index], a)
-        return getattr(
-            self._npa[
-                self._index %
-                self.max_size:(
-                    self._index %
-                    self.max_size) + self.max_size], a)
-
-    def __len__(self):
-        if self.isFull():
-            return self.max_size
-        return self._index
-
 
 class TimeSyncState(object):
     """Container class used to hold the data necessary to calculate the current
