a2e446824a0ba8eb073c9b85f2bdb52248fecb8b
Sol Simpson <sol@isolver-software.com>
2016-05-18 20:32:15 -0400

    Added more tests for iohub connection class

--- psychopy/tests/test_iohub/test_launch.py
+++ psychopy/tests/test_iohub/test_launch.py
@@ -5,18 +5,51 @@ from psychopy.tests.utils import skip_under_travis
 from psychopy.tests.test_iohub.testutil import startHubProcess, stopHubProcess, getTime
 
 @skip_under_travis
-def testDefaultServerLaunch():
+def testServerConnectionInstance():
     """
     """
     io = startHubProcess()
 
     # check that a kb and mouse have been created
-    keyboard=io.devices.keyboard
-    mouse=io.devices.mouse
+    keyboard = io.devices.keyboard
+    mouse = io.devices.mouse
     exp = io.devices.experiment
-    assert keyboard != None
-    assert mouse != None
-    assert exp != None
+    assert keyboard is not None
+    assert mouse is not None
+    assert exp is not None
 
+    assert io.getSessionID() is None
+    assert io.getExperimentID() is None
+
+    assert io.getExperimentMetaData() is None
+    assert io.getSessionMetaData() is None
+
+    assert isinstance(io.getHubServerConfig(), dict)
     stopHubProcess()
+
+
+@skip_under_travis
+def testKeyboardDeviceInstance():
+    """
+    """
+    io = startHubProcess()
+
+    # check that a kb 
+    keyboard=io.devices.keyboard
+    
+    assert keyboard != None
+
+    kb_name = keyboard.getName()
+    assert kb_name == 'keyboard'
+    
+    kb_iohub_class = keyboard.getIOHubDeviceClass()
+    assert kb_iohub_class == 'Keyboard', "keyboard.getIOHubDeviceClass() returned {} of type: {}".format(kb_iohub_class, type(kb_iohub_class))
+
+    kb_iohub_methods = keyboard.getDeviceInterface()
+    assert len(kb_iohub_methods) > 0
+    assert 'getEvents' in kb_iohub_methods
+    
+    stopHubProcess()
+
+
 
