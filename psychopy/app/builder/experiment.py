psychopy/psychopy

commit 37b8e6d4db5a857cfa08297f21d9e9e98a3c69e8

ENH: moved continueRoutine=True from Routine.writeStartCode to Routine.writeInitCode
closes #1143

This change allows Code Components to set continueRoutine=False in the
Begin Routine section rather than on every frame, which makes better
logical sense, because the writeInitCode occurs before the
Components.writeBeginRoutine()

psychopy/app/builder/experiment.py
@@ -1526,6 +1526,7 @@ class Routine(list):
         buff.writeIndentedLines(code % self.name)
         self._clockName = self.name + "Clock"
         buff.writeIndented('%s = core.Clock()\n' % self._clockName)
+        buff.writeIndented('continueRoutine = True\n')
         for thisCompon in self:
             thisCompon.writeInitCode(buff)
 
@@ -1556,8 +1557,7 @@ class Routine(list):
         code = ("for thisComponent in %sComponents:\n"
                 "    if hasattr(thisComponent, 'status'):\n"
                 "        thisComponent.status = NOT_STARTED\n"
-                '\n# -------Start Routine "%s"-------\n'
-                'continueRoutine = True\n')
+                '\n# -------Start Routine "%s"-------\n')
         buff.writeIndentedLines(code % (self.name, self.name))
         if useNonSlip:
             code = 'while continueRoutine and routineTimer.getTime() > 0:\n'

commit 4d98dbe1606f9f3ea4865526971c79ebc979208c

FF: Fixed problem with Flow not running past first Routine
Fixes bug introduced by commit 37b8e6d4d

In response to issue #1143 I moved the reset of `continueRoutine=True`
to earlier inthe script but accidentally moved it too far. It just needed
to go before the components ran their routineStart code not to the init
code!

psychopy/app/builder/experiment.py
@@ -1570,6 +1570,8 @@ class Routine(list):
         return statics
 
     def writeStartCode(self, buff):
+        """This is start of the *experiment* (before window is created)
+        """
         # few components will have this
         for thisCompon in self:
             # check just in case; try to ensure backwards compatibility _base
@@ -1577,6 +1579,8 @@ class Routine(list):
                 thisCompon.writeStartCode(buff)
 
     def writeStartCodeJS(self, buff):
+        """This is start of the *experiment*
+        """
         # few components will have this
         for thisCompon in self:
             # check just in case; try to ensure backwards compatibility _base
@@ -1588,7 +1592,6 @@ class Routine(list):
         buff.writeIndentedLines(code % self.name)
         self._clockName = self.name + "Clock"
         buff.writeIndented('%s = core.Clock()\n' % self._clockName)
-        buff.writeIndented('continueRoutine = True\n')
         for thisCompon in self:
             thisCompon.writeInitCode(buff)
 
@@ -1612,7 +1615,8 @@ class Routine(list):
         code = ('\n# ------Prepare to start Routine "%s"-------\n'
                 't = 0\n'
                 '%s.reset()  # clock\n'
-                'frameN = -1\n')
+                'frameN = -1\n'
+                'continueRoutine = True\n')
         buff.writeIndentedLines(code % (self.name, self._clockName))
         # can we use non-slip timing?
         maxTime, useNonSlip = self.getMaxTime()