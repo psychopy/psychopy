043391e0c877871fe257afcdc40a2819721a357a
Jon Peirce <jon.peirce@gmail.com>
2016-11-21 11:03:07 +0000

    RF: Sound refactored into separate files

---
+++ psychopy/sound/__init__.py
@@ -0,0 +1,118 @@
+"""Load and play sounds
+
+By default PsychoPy will try to use the following Libs, in this order, for
+sound reproduction but you can alter the order in
+preferences > general > audioLib:
+    ['sounddevice', 'pygame', 'pyo']
+For portaudio-based backends (all except for pygame) there is also a
+choice of the underlying sound driver (e.g. ASIO, CoreAudio etc).
+
+After importing sound, the sound lib and driver being used will be stored as::
+    `psychopy.sound.audioLib`
+    `psychopy.sound.audioDriver`
+
+For control of bitrate and buffer size you can call psychopy.sound.init before
+creating your first Sound object::
+
+    from psychopy import sound
+    sound.init(rate=44100, stereo=True, buffer=128)
+    s1 = sound.Sound('ding.wav')
+
+The history of sound libs in PsychoPy:
+
+    - we started with pygame but latencies were always poor
+    - we switched to pyo and latencies were better but very system-dependent.
+      The problem with pyo is that it has to be compiled which was always
+      more painful and prevents us from modifying it easily when needed. It
+      also doesn't support python3 as of 2016 and it doesn't support pip for
+      installation (e.g. in Anaconda)
+    - pysoundcard and sounddevice are new pure python portaudio libraries
+      They both install trivially with pip and support python3
+      Sounddevice appears to be under most recent development (and shares a
+      lot of the original pysoundcard code). In testing we've found the
+      latencies to be low
+
+As of PsychoPy 1.85 **sounddevice looks like it will be the best option** but
+while it is new there may be some teething problems! To be fair, when writing
+the sounddevice code we also changed the method of starting sounds (and pyo
+also supports the new method). It is likely that pyo can achieve the same
+low latencies as sounddevice, but the other advantages of sounddevice make it
+preferable.
+"""
+
+# Part of the PsychoPy library
+# Copyright (C) 2015 Jonathan Peirce
+# Distributed under the terms of the GNU General Public License (GPL).
+
+from __future__ import division
+import sys
+from psychopy import logging, prefs, exceptions
+
+pyoSndServer = None
+Sound = None
+audioLib = None
+audioDriver = None
+
+for thisLibName in prefs.general['audioLib']:
+
+    try:
+        if thisLibName == 'pyo':
+            from . import backend_pyo as backend
+            Sound = backend.SoundPyo
+        elif thisLibName == 'sounddevice':
+            from . import backend_pysound as backend
+            Sound = backend.SoundDeviceSound
+        elif thisLibName == 'pygame':
+            from . import backend_pygame as backend
+            Sound = backend.SoundPygame
+        elif thisLibName == 'pysoundcard':
+            from . import backend_pysound as backend
+            Sound = backend.SoundPySoundCard
+        else:
+            msg = ("Audio lib options are currently only 'pyo' or "
+                   "'pygame', not '%s'")
+            raise ValueError(msg % thisLibName)
+        # if we got this far we were sucessful in loading the lib
+        audioLib = thisLibName
+        init = backend.init
+        logging.info('sound is using audioLib: %s' % audioLib)
+        break
+    except exceptions.DependencyError:
+        msg = '%s audio lib was requested but not loaded: %s'
+        logging.warning(msg % (thisLibName, sys.exc_info()[1]))
+        continue  # to try next audio lib
+
+if audioLib is None:
+    raise exceptions.DependencyError(
+            "No sound libs could be loaded. Tried: {}\n"
+            "Check whether the necessary sound libs are installed"
+            .format(prefs.general['audioLib']))
+
+def _bestDriver(devNames, devIDs):
+    """Find ASIO or Windows sound drivers
+    """
+    preferredDrivers = prefs.general['audioDriver']
+    outputID = None
+    audioDriver = None
+    osEncoding = sys.getfilesystemencoding()
+    for prefDriver in preferredDrivers:
+        logging.info('Looking for {}'.format(prefDriver))
+        if prefDriver.lower() == 'directsound':
+            prefDriver = u'Primary Sound'
+        # look for that driver in available devices
+        for devN, devString in enumerate(devNames):
+            logging.info('Examining for {}'.format(devString))
+            try:
+                ds = devString.decode(osEncoding).encode('utf-8').lower()
+                if prefDriver.encode('utf-8').lower() in ds:
+                    audioDriver = devString.decode(osEncoding).encode('utf-8')
+                    outputID = devIDs[devN]
+                    logging.info('Success: {}'.format(devString))
+                    # we found a driver don't look for others
+                    return audioDriver, outputID
+            except (UnicodeDecodeError, UnicodeEncodeError):
+                logging.info('Failed: {}'.format(devString))
+                logging.warn('find best sound driver - could not '
+                             'interpret unicode in driver name')
+    else:
+        return None, None
---
+++ psychopy/sound/_base.py
@@ -0,0 +1,148 @@
+# Part of the PsychoPy library
+# Copyright (C) 2015 Jonathan Peirce
+# Distributed under the terms of the GNU General Public License (GPL).
+
+from __future__ import division
+import numpy
+from os import path
+from psychopy import logging
+from psychopy.constants import (STARTED, PLAYING, PAUSED, FINISHED, STOPPED,
+                                NOT_STARTED, FOREVER)
+from sys import platform
+
+
+if platform == 'win32':
+    mediaLocation = "C:\\Windows\Media"
+else:
+    mediaLocation = ""
+
+stepsFromA = {
+    'C': -9, 'Csh': -8,
+    'Dfl': -8, 'D': -7, 'Dsh': -6,
+    'Efl': -6, 'E': -5,
+    'F': -4, 'Fsh': -3,
+    'Gfl': -3, 'G': -2, 'Gsh': -1,
+    'Afl': -1, 'A': 0, 'Ash': +1,
+    'Bfl': +1, 'B': +2, 'Bsh': +2}
+knownNoteNames = sorted(stepsFromA.keys())
+
+
+def apodize(soundArray, sampleRate):
+    """Apply a Hamming window (5ms) to reduce a sound's 'click' onset / offset
+    """
+    hwSize = int(min(sampleRate // 200, len(soundArray) // 15))
+    hammingWindow = numpy.hamming(2 * hwSize + 1)
+    soundArray[:hwSize] *= hammingWindow[:hwSize]
+    for i in range(2):
+        soundArray[-hwSize:] *= hammingWindow[hwSize + 1:]
+    return soundArray
+
+
+class _SoundBase(object):
+    """Base class for sound object, from one of many ways.
+    """
+    # Must be provided by class SoundPygame or SoundPyo:
+    # def __init__()
+    # def play(self, fromStart=True, **kwargs):
+    # def stop(self, log=True):
+    # def getDuration(self):
+    # def getVolume(self):
+    # def setVolume(self, newVol, log=True):
+    # def _setSndFromFile(self, fileName):
+    # def _setSndFromArray(self, thisArray):
+
+    def setSound(self, value, secs=0.5, octave=4, hamming=True, log=True):
+        """Set the sound to be played.
+
+        Often this is not needed by the user - it is called implicitly during
+        initialisation.
+
+        :parameters:
+
+            value: can be a number, string or an array:
+                * If it's a number between 37 and 32767 then a tone will
+                  be generated at that frequency in Hz.
+                * It could be a string for a note ('A', 'Bfl', 'B', 'C',
+                  'Csh'. ...). Then you may want to specify which octave.
+                * Or a string could represent a filename in the current
+                  location, or mediaLocation, or a full path combo
+                * Or by giving an Nx2 numpy array of floats (-1:1) you can
+                  specify the sound yourself as a waveform
+
+            secs: duration (only relevant if the value is a note name or
+                a frequency value)
+
+            octave: is only relevant if the value is a note name.
+                Middle octave of a piano is 4. Most computers won't
+                output sounds in the bottom octave (1) and the top
+                octave (8) is generally painful
+        """
+        # Re-init sound to ensure bad values will raise error during setting:
+        self._snd = None
+
+        try:
+            # could be '440' meaning 440
+            value = float(value)
+        except (ValueError, TypeError):
+            # this is a string that can't be a number, eg, a file or note
+            pass
+        else:
+            # we've been asked for a particular Hz
+            if value < 37 or value > 20000:
+                msg = 'Sound: bad requested frequency %.0f'
+                raise ValueError(msg % value)
+            self._setSndFromFreq(value, secs, hamming=hamming)
+
+        if isinstance(value, basestring):
+            if value.capitalize() in knownNoteNames:
+                self._setSndFromNote(value.capitalize(), secs, octave,
+                                     hamming=hamming)
+            else:
+                # try finding a file
+                self.fileName = None
+                for filePath in ['', mediaLocation]:
+                    p = path.join(filePath, value)
+                    if path.isfile(p):
+                        self.fileName = p
+                        break
+                    elif path.isfile(p + '.wav'):
+                        self.fileName = p + '.wav'
+                        break
+                if self.fileName is None:
+                    msg = "setSound: could not find a sound file named "
+                    raise ValueError, msg + value
+                else:
+                    self._setSndFromFile(value)
+        elif type(value) in [list, numpy.ndarray]:
+            # create a sound from the input array/list
+            self._setSndFromArray(numpy.array(value))
+        # did we succeed?
+        if self._snd is None:
+            pass  # raise ValueError, "Could not make a "+value+" sound"
+        else:
+            if log and self.autoLog:
+                logging.exp("Set %s sound=%s" % (self.name, value), obj=self)
+            self.status = NOT_STARTED
+
+    def _setSndFromNote(self, thisNote, secs, octave, hamming=True):
+        # note name -> freq -> sound
+        freqA = 440.0
+        thisOctave = octave - 4
+        mult = 2.0**(stepsFromA[thisNote] / 12.)
+        thisFreq = freqA * mult * 2.0 ** thisOctave
+        self._setSndFromFreq(thisFreq, secs, hamming=hamming)
+
+    def _setSndFromFreq(self, thisFreq, secs, hamming=True):
+        # note freq -> array -> sound
+        if secs < 0:
+            # want infinite duration - create 1 sec sound and loop it
+            secs = 10.0
+            self.loops = -1
+        nSamples = int(secs * self.sampleRate)
+        outArr = numpy.arange(0.0, 1.0, 1.0 / nSamples)
+        outArr *= 2 * numpy.pi * thisFreq * secs
+        outArr = numpy.sin(outArr)
+        if hamming and nSamples > 30:
+            outArr = apodize(outArr, self.sampleRate)
+        self._setSndFromArray(outArr)
+
