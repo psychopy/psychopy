02937ba25dfe8d5d1f38eff74cfade684ee54bb4
Jon Peirce <jon.peirce@gmail.com>
2017-03-15 17:04:50 +0000

    BF: call ioinfo.io using https not http
    Using http is a problem for institutions that require a full https stack

--- psychojs/js/psychojs/main.js
+++ psychojs/js/psychojs/main.js
@@ -1,10 +1,10 @@
 /**
  * Main component of psychoJS
- * 
- * 
+ *
+ *
  * This file is part of the PsychoJS javascript engine of PsychoPy.
  * Copyright (c) 2016 Ilixa Ltd. (www.ilixa.com)
- * 
+ *
  * Distributed under the terms of the GNU General Public License (GPL).
  */
 
@@ -17,14 +17,14 @@ var psychoJS = {}
 
 /**
  * Initialise PsychoJS
- * 
+ *
  * <p>Note: 'init' must be called once the document is ready:</p>
  * <code>$(document).ready(function() {<br>
  * ...<br>
  * psychoJS.init();</code>
  *
  * @param {psychoJS.visual.Window} window - the psychoJS [Window]{@link psychoJS.visual.Window}
- * 
+ *
  */
 psychoJS.init = function(window) {
 	// component states:
@@ -35,20 +35,20 @@ psychoJS.init = function(window) {
 	// debugging:
 	psychoJS.debug = true;
 	if (psychoJS.debug) console.log("init PsychoJS");
-	
+
 	psychoJS.frameCount = 0;
-	
+
 	// note: we create the resource manager here, but its parameters
 	// will be set in the psychopy generated .html code
 	psychoJS.resourceManager = new psychoJS.io.ResourceManager();
-	
+
 	psychoJS.window = window;
-	
-		
+
+
 	// get IP info of participant
 	// note: since we make a GET call to http://ipinfo.io to get IP info,
 	// these will most certainly not be available immediately after the call
-	// to psychoJS.init. 
+	// to psychoJS.init.
 	psychoJS._IP = {};
 	psychoJS.getParticipantIPInfo();
 }
@@ -56,13 +56,13 @@ psychoJS.init = function(window) {
 
 /**
  * Get the IP information of the participant, asynchronously.
- * 
+ *
  * <p>Note: we use http://ipinfo.io</p>
  */
 psychoJS.getParticipantIPInfo = function() {
 	$.ajax({
 		type: "GET",
-		url: 'http://ipinfo.io',
+		url: 'https://ipinfo.io',
 		dataType: 'json',
 	}).then(
 		function (response) {
@@ -86,11 +86,11 @@ psychoJS.getParticipantIPInfo = function() {
 psychoJS.setupCallbacks = function() {
 	// key events:
 	document.addEventListener("keydown", psychoJS.event._keyDownHandler, false);
-	
+
 	// resize events:
 	$(window).on("resize", psychoJS.onResize);
 	$(window).on("orientationchange", psychoJS.onResize);
-	
+
 	return psychoJS.NEXT;
 }
 
@@ -108,7 +108,7 @@ var STAGE_HEIGHT = 320;
 psychoJS.onResize = function(event) {
 	var width = $(window).width();
 	var height = $(window).height();
-	
+
 	// maintain aspect ratio:
 	var ratioWidth = width / STAGE_WIDTH;
 	var ratioHeight = height / STAGE_HEIGHT;
@@ -119,14 +119,14 @@ psychoJS.onResize = function(event) {
 	}
 	var ratioWidth = Math.floor(STAGE_WIDTH * ratio);
 	var ratioHeight = Math.floor(STAGE_HEIGHT * ratio);
-	
+
 	psychoJS.window._renderer.view.style.width = ratioWidth + 'px';
 	psychoJS.window._renderer.view.style.height = ratioHeight + 'px';
 	psychoJS.window._renderer.view.style.left = width/2 - ratioWidth/2 + "px";
 	psychoJS.window._renderer.view.style.top = height/2 - ratioHeight/2 + "px";
-	
+
 	psychoJS.window._renderer.resize(ratioWidth, ratioHeight);
-	
+
 	psychoJS.window._container.position.x = ratioWidth/2;
 	psychoJS.window._container.position.y = ratioHeight/2;
 	// positive values of y are to the top:
@@ -141,12 +141,12 @@ psychoJS.onUpdate = function()
 {
 	++psychoJS.frameCount;
 	if (psychoJS.debug) console.log("frame " + psychoJS.debug);
-	
+
 	stats.begin();
-	
+
 	renderer.render(stage);
 	requestAnimationFrame(psychoJS.onUpdate);
-	
+
 	stats.end();
 }
 
