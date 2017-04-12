74156f3c35105d3c2b426a55d90369c8c702d899
Richard Höchenberger <richard.hoechenberger@gmail.com>
2017-04-12 14:45:52 +0200

    TEST: Add more tests for event.clearEvents()

--- psychopy/tests/test_misc/test_event.py
+++ psychopy/tests/test_misc/test_event.py
@@ -103,6 +103,25 @@ class _baseTest(object):
         for t in ['mouse', 'joystick', 'keyboard', None]:
             event.clearEvents(t)
 
+    def test_clearEvents_keyboard(self):
+        event._onPygletKey(symbol='x', modifiers=0, emulated=True)
+        event.clearEvents('keyboard')
+        assert not event._keyBuffer
+
+    def test_clearEvents_mouse(self):
+        """Keyboard buffer should not be affected
+        """
+        event._onPygletKey(symbol='x', modifiers=0, emulated=True)
+        event.clearEvents('mouse')
+        assert event._keyBuffer
+
+    def test_clearEvents_joystick(self):
+        """Keyboard buffer should not be affected
+        """
+        event._onPygletKey(symbol='x', modifiers=0, emulated=True)
+        event.clearEvents('joystick')
+        assert event._keyBuffer
+
     def test_keys(self):
         if travis:
             pytest.skip()  # failing on travis-ci
