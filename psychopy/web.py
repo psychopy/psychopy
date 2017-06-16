4ed9edaeaaae14e007f822bd24aa1125db170449
Jon Peirce <jon.peirce@gmail.com>
2017-06-16 10:51:56 +0100

    FF: Getting rid of code no longer needed to simplify py3 use
    closes #1308

--- psychopy/web.py
+++ psychopy/web.py
@@ -9,18 +9,12 @@
 
 from future import standard_library
 standard_library.install_aliases()
-from builtins import str
-from builtins import range
+
 from builtins import object
-import os
 import sys
-import base64
-import httplib2
-import mimetypes  # deprecated; use requests package instead
 import socket
 import re
 from psychopy import logging
-from psychopy.constants import PSYCHOPY_USERAGENT
 from psychopy import prefs
 
 if sys.version_info >= (3, 0):
@@ -31,7 +25,6 @@ if py3:
     import urllib.request
     import urllib.error
     import urllib.parse
-    import io  # pylint: disable=W0611
 else:
     import urllib2
     import urllib.request, urllib.error, urllib.parse
@@ -43,7 +36,6 @@ else:
             self.error = lib
             self.parse = lib
     urllib = FakeURLlib(urllib2)
-    import cStringIO as io  # pylint: disable=W0611
 
 # default 20s from prefs, min 2s
 TIMEOUT = max(prefs.connections['timeout'], 2.0)
@@ -310,240 +302,3 @@ def setupProxy(log=True):
 
     proxies = 0
     return 0
-
-# post_multipart is from {{{ http://code.activestate.com/recipes/146306/
-# (r1) ###
-
-
-def _post_multipart(host, selector, fields, files,
-                    encoding='utf-8', timeout=TIMEOUT,
-                    userAgent=PSYCHOPY_USERAGENT,
-                    basicAuth=None, https=False):
-    """Post fields and files to an http host as multipart/form-data.
-    fields is a sequence of (name, value) elements for regular form fields.
-    file is a 1-item sequence of (name, filename, value) elements for data
-    to be uploaded as files
-
-    Return the server's response page.
-    """
-    # as updated for HTTPConnection()
-    # as rewritten for any encoding http://www.nerdwho.com/blog/57/
-    #     enviando-arquivos-e-dados-ao-mesmo-tempo-via-http-post-usando-utf-8/
-    # JRG: added timeout, userAgent, basic auth, https
-
-    def _encode_multipart_formdata(fields, files, encoding='utf-8'):
-        """fields is a sequence of (name, value) elements for regular
-        form fields. files is a sequence of (name, filename, value)
-        elements for data to be uploaded as files
-
-        Return (content_type, body) ready for httplib2.HTTP instance
-        """
-        BOUNDARY = u'----------ThIs_Is_tHe_bouNdaRY_$'
-        CRLF = u'\r\n'
-        L = []
-
-        for (key, value) in fields:
-            L.append(u'--' + BOUNDARY)
-            L.append(u'Content-Disposition: form-data; name="%s"' % key)
-            L.append(u'Content-Type: text/plain;charset=%s' % encoding)
-            L.append(u'Content-Transfer-Encoding: 8bit')
-            L.append(u'')
-            L.append(value)
-
-        for (key, filename, value) in files:
-            L.append(u'--' + BOUNDARY)
-            msg = u'Content-Disposition: form-data; name="%s"; filename="%s"'
-            L.append(msg % (key, filename))
-            msg = u'Content-Type: %s;charset=%s'
-            L.append(msg % (_get_content_type(filename), encoding))
-            L.append(u'Content-Transfer-Encoding: base64')
-            L.append(u'')
-            L.append(base64.b64encode(value).decode())
-
-        L.append(u'--' + BOUNDARY + u'--')
-        L.append(u'')
-        body = CRLF.join(L)
-        content_type = u'multipart/form-data; boundary=%s' % BOUNDARY
-
-        return content_type, body
-
-    def _get_content_type(filename):
-        return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-
-    # start of _post_multipart main code:
-    content_type, body = _encode_multipart_formdata(fields, files)
-
-    # select https -- note there's NO verification of the server’s certificate
-    if https is True:
-        conn = httplib2.HTTPSConnection(host, timeout=timeout)
-    else:
-        conn = httplib2.HTTPConnection(host, timeout=timeout)
-    headers = {u'User-Agent': userAgent,
-               u'Charset': encoding,
-               u'Content-Type': content_type}
-    # apache basic auth (sent in clear text, https can help):
-    if basicAuth and type(basicAuth) == str:
-        user_cred = base64.encodestring(basicAuth).replace('\n', '')
-        headers.update({u"Authorization": u"Basic %s" % user_cred})
-
-    try:
-        conn.request(u'POST', selector, body, headers)
-    except Exception:  # ? don't seem to get a proper exception
-        msg = 'connection error (possible timeout after %ss)'
-        return -1, msg % str(timeout), 'timeout or error'
-
-    try:
-        result = conn.getresponse()
-    except Exception:
-        return -1, ('connection error (can be "socket.error: '
-                    '[Errno 54] Connection reset by peer")')
-    return result.status, result.reason, result.read()
-
-    # end of http://code.activestate.com/recipes/146306/ }}}
-
-
-def upload(selector, filename, basicAuth=None, host=None,
-           https=False, log=True):
-    """DEPRECATED: Upload a local file over the internet to a
-    configured http server.
-    Use the requests package instead. See http://www.python-requests.org/
-
-    This method handshakes with a php script on a remote server to transfer
-    a local file to another machine via http (using POST).
-
-    Returns "success" plus a sha256 digest of the file on the server and
-    a byte count. If the upload was not successful, an error code is
-    returned (eg, "too_large" if the file size exceeds the limit specified
-    server-side in up.php, or "no_file" if there was no POST attachment).
-
-    .. note::
-        The server that receives the files needs to be configured before
-        uploading will work. php files and notes for a sys-admin are
-        included in `psychopy/contrib/http/`. In particular, the php script
-        `up.php` needs to be copied to the server's web-space, with
-        appropriate permissions and directories, including apache basic
-        auth and https (if desired). The maximum size for an upload can
-        be configured within up.php
-
-    **Parameters:**
-
-        `selector` : (required, string)
-            a standard URL of the form `http://host/path/to/up.php`,
-            e.g., `http://upload.psychopy.org/test/up.php`
-
-            .. note::
-                Limited https support is provided (see below).
-
-        `filename` : (required, string)
-            the path to the local file to be transferred. The file can be
-            any format: text, utf-8, binary. All files are hex encoded while
-            in transit (increasing the effective file size).
-
-            .. note::
-                Encryption (*beta*) is available as a separate step. That is,
-                first :mod:`~psychopy.contrib.opensslwrap.encrypt()` the file,
-                then :mod:`~psychopy.web.upload()` the encrypted file in the
-                same way that you would any other file.
-
-        `basicAuth` : (optional)
-            apache 'user:password' string for basic authentication. If a `basicAuth`
-            value is supplied, it will be sent as the auth credentials (in cleartext);
-            using https will encrypt the credentials.
-        `host` : (optional)
-            The default process is to extract host information from the `selector`. The `host` option
-            allows you to specify a host explicitly (i.e., if it differs from the `selector`).
-        `https` : (optional)
-            If the remote server is configured to use https, passing the parameter
-            `https=True` will encrypt the transmission including all data and `basicAuth`
-            credentials. It is approximately as secure as using a self-signed X.509 certificate.
-
-            An important caveat is that the authenticity of the certificate returned from the
-            server is not checked, and so the certificate could potentially be spoofed
-            (see the warning under HTTPSConnection http://docs.python.org/library/httplib2.html).
-            Overall, using https can still be much more secure than not using it.
-            The encryption is good, but that of itself does not eliminate all risk.
-            Importantly, it is not as secure as one might expect, given that all major web browsers
-            do check certificate authenticity. The idea behind this parameter is to require people
-            to explicitly indicate that they want to proceed anyway, in effect saying
-            "I know what I am doing and accept the risks (of using un-verified certificates)".
-
-    Author: Jeremy R. Gray, 2012
-    """
-
-    logging.warning(
-        "Deprecated: psychopy.web.upload is superceded by requests (python package)")  # ~Nov 2015
-
-    requireInternetAccess()  # needed to upload over http
-
-    fields = [('name', 'PsychoPy_upload'), ('type', 'file')]
-    if not selector:
-        logging.error('upload: need a selector, http://<host>/path/to/up.php')
-        raise ValueError(
-            'upload: need a selector, http://<host>/path/to/up.php')
-    if not host:
-        host = selector.split('/')[2]
-        if log:
-            logging.info('upload: host extracted from selector = %s' % host)
-    if selector.startswith('https'):
-        if https is not True:
-            logging.error(
-                'upload: https not explicitly requested. use https=True to proceed anyway (see API for security caveats).')
-            raise ValueError(
-                'upload: https not fully supported (see API for caveats and usage), exiting.')
-        elif log:
-            logging.exp(
-                'upload: https requested; note that security is not fully assured (see API)')
-    elif https:
-        msg = 'upload: to use https, the selector URL must start with "https"'
-        logging.error(msg)
-        raise ValueError(msg)
-    if not os.path.isfile(filename):
-        logging.error('upload: file not found (%s)' % filename)
-        raise ValueError('upload: file not found (%s)' % filename)
-    # base64 encoded in _encode_multipart_formdata()
-    contents = open(filename).read()
-    file = [('file_1', filename, contents)]
-
-    # initiate the POST:
-    if log:
-        logging.exp('upload: uploading file %s to %s' %
-                    (os.path.abspath(filename), selector))
-    try:
-        status, reason, result = _post_multipart(host, selector, fields, file,
-                                                 basicAuth=basicAuth, https=https)
-    except TypeError:
-        status = 'no return value from _post_multipart(). '
-        reason = 'config error?'
-        result = status + reason
-    except urllib.error.URLError as ex:
-        logging.error('upload: URL Error. (no internet connection?)')
-        raise ex
-
-    # process the result:
-    if status == 200:
-        result_fields = result.split()
-        # result = 'status_msg digest' # if using up.php
-        if result_fields[0] == 'good_upload':
-            outcome = 'success' + ' ' + result
-        else:
-            outcome = result  # failure code
-    elif status == 404:
-        outcome = '404 Not_Found: server config error'
-    elif status == 403:
-        outcome = '403 Forbidden: server config error'
-    elif status == 401:
-        outcome = '401 Denied: failed apache Basic authorization, or config error'
-    elif status == 400:
-        outcome = '400 Bad request: failed, possible config error'
-    else:
-        outcome = str(status) + ' ' + reason
-
-    if status == -1 or status > 299 or type(status) == str:
-        logging.error('upload: ' + outcome[:102])
-    else:
-        if outcome.startswith('success'):
-            if log:
-                logging.info('upload: ' + outcome[:102])
-        else:
-            logging.error('upload: ' + outcome[:102])
-    return outcome
