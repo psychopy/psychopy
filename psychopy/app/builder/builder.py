e8bd7bdfd2f84a20b5e0d4a2d08119de7cc7a56c
Adam Kimbler <akimbler@fiu.edu>
2017-08-13 11:03:47 -0400

    Update builder.py

--- psychopy/app/builder/builder.py
+++ psychopy/app/builder/builder.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from builtins import range, object
+from builtins import range, object, str
 import wx
 from wx.lib import platebtn, scrolledpanel
 try:
