d41d6fcaa5f8eb7749300ac5fb24d1237a2d1af9
Richard Höchenberger <richard.hoechenberger@gmail.com>
2017-10-05 15:52:19 +0200

    TESTS: Add tests for TrialHandler2 output files

--- psychopy/tests/test_data/test_TrialHandlerExt.py
+++ psychopy/tests/test_data/test_TrialHandlerExt.py
@@ -21,7 +21,7 @@ from psychopy.tests import utils
 from psychopy.constants import PY3
 
 thisPath = os.path.split(__file__)[0]
-fixturesPath = os.path.join(thisPath,'..','data')
+fixturesPath = os.path.join(thisPath, '..', 'data')
 
 
 class TestTrialHandlerExt(object):
@@ -44,7 +44,7 @@ class TestTrialHandlerExt(object):
 
         # Make sure the file is there
         data_filename = base_data_filename + '.csv'
-        assert os.path.exists(data_filename), "File not found: %s" %os.path.abspath(data_filename)
+        assert os.path.exists(data_filename), "File not found: %s" % os.path.abspath(data_filename)
 
         # Make sure the header line is correct
         # We open the file with universal newline support (PEP-278).
@@ -79,6 +79,7 @@ class TestTrialHandlerExt(object):
             # Make sure the correct number of files for the loop are there. (No overwriting by default).
             matches = len(glob.glob(os.path.join(self.temp_dir, self.rootName + "*.psydat")))
             assert matches==count, "Found %d matching files, should be %d" % (matches, count)
+
     def test_psydat_filename_collision_overwriting(self):
         for count in [1, 10, 20]:
             trials = data.TrialHandlerExt([], 1, autoLog=False)
@@ -104,12 +105,12 @@ class TestTrialHandlerExt(object):
         # maybe PsychoPy version of the .psyexp??
 
         dat = fromFile(os.path.join(fixturesPath,'multiKeypressTrialhandler.psydat'))
-        #test csv output
+        # test csv output
         dat.saveAsText(pjoin(self.temp_dir, 'testMultiKeyTrials.csv'),
                        appendFile=False)
         utils.compareTextFiles(pjoin(self.temp_dir, 'testMultiKeyTrials.csv'),
                                pjoin(fixturesPath,'corrMultiKeyTrials.csv'))
-        #test xlsx output
+        # test xlsx output
         dat.saveAsExcel(pjoin(self.temp_dir, 'testMultiKeyTrials.xlsx'),
                         appendFile=False)
         utils.compareXlsxFiles(pjoin(self.temp_dir, 'testMultiKeyTrials.xlsx'),
@@ -120,7 +121,7 @@ class TestTrialHandlerExt(object):
             for count in range(1,3):
                 trials = data.TrialHandlerExt([], 1, autoLog=False)
                 trials.data.addDataType('trialType')
-                for trial in trials:#need to run trials or file won't be saved
+                for trial in trials:  # need to run trials or file won't be saved
                     trials.addData('trialType', 0)
                 base_data_filename = pjoin(self.temp_dir, self.rootName)
 
@@ -163,7 +164,7 @@ class TestTrialHandlerExt(object):
             conditions.append({'trialType':trialType})
 
         trials= data.TrialHandlerExt(trialList=conditions, seed=100, nReps=3,
-                                  method='random', autoLog=False)
+                                     method='random', autoLog=False)
         # simulate trials
         rng = np.random.RandomState(seed=self.random_seed)
 
@@ -173,14 +174,15 @@ class TestTrialHandlerExt(object):
             trials.addData('resp', resp)
             trials.addData('rand', randResp)
 
-        # test summarised data outputs      #this omits values
+        # test summarised data outputs
+        # this omits values
         trials.saveAsText(pjoin(self.temp_dir, 'testRandom.tsv'),
                           stimOut=['trialType'], appendFile=False)
         utils.compareTextFiles(pjoin(self.temp_dir, 'testRandom.tsv'),
                                pjoin(fixturesPath,'corrRandom.tsv'))
         # test wide data outputs
         trials.saveAsWideText(pjoin(self.temp_dir, 'testRandom.csv'),
-                              delim=',', appendFile=False)#this omits values
+                              delim=',', appendFile=False)  # this omits values
         utils.compareTextFiles(pjoin(self.temp_dir, 'testRandom.csv'),
                                pjoin(fixturesPath,'corrRandom.csv'))
 
