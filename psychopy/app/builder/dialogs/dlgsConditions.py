psychopy/psychopy

commit 02c6c037b06985772722058e9bce0bfba7e24f04

FF: wx.FlexGridSizer() needs third parameter (gap) in wxpython 4

psychopy/app/builder/dialogs/dlgsConditions.py
@@ -459,7 +459,7 @@ class DlgConditions(wx.Dialog):
             # data matrix on top, buttons below
             self.border = wx.FlexGridSizer(2, 1)
         else:
-            self.border = wx.FlexGridSizer(4)
+            self.border = wx.FlexGridSizer(4, 1, wx.Size(0,0))
         self.border.Add(self.sizer, proportion=1,
                         flag=wx.ALL | wx.EXPAND, border=8)
 

commit 23d34881441db5c2242b0f268cfbfa710c52f08c

FF: A fix for wx4 just broke the conditinos viewer in wx3
This fixes a wx3 bug introduced in commit 02c6c037b069857727

psychopy/app/builder/dialogs/dlgsConditions.py
@@ -458,6 +458,8 @@ class DlgConditions(wx.Dialog):
         if wx.version()[0] == '2':
             # data matrix on top, buttons below
             self.border = wx.FlexGridSizer(2, 1)
+        elif wx.version()[0] == '3':
+            self.border = wx.FlexGridSizer(4)
         else:
             self.border = wx.FlexGridSizer(4, 1, wx.Size(0,0))
         self.border.Add(self.sizer, proportion=1,