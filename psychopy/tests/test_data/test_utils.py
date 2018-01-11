c564f8968a7d7d462ef74a8319688e1570db03b7
dvbridges <david-bridges@hotmail.co.uk>
2018-01-11 15:10:05 +0000

    TEST: More tests to test_data/test_utils.py

--- psychopy/tests/test_data/test_utils.py
+++ psychopy/tests/test_data/test_utils.py
@@ -11,6 +11,7 @@ fixturesPath = os.path.join(thisDir, '..', 'data')
 class Test_utilsClass:
 
     def test_importConditions(self):
+        fileName = os.path.join(fixturesPath, 'trialTypes.xlsx')
         expected_cond = utils.OrderedDict(
             [('text', 'red'),
              ('congruent', 1),
@@ -18,8 +19,28 @@ class Test_utilsClass:
              ('letterColor', 'red'),
              ('n', 2),
              ('float', 1.1)])
-        conds = utils.importConditions(os.path.join(fixturesPath, 'trialTypes.xlsx'))
+        conds = utils.importConditions(fileName)
         assert conds[0] == expected_cond
+
+        # test for None in filename with _assertValidVarNames
+        assert utils.importConditions(fileName=None) == []
+        assert utils.importConditions(fileName=None, returnFieldNames=True) == ([], [])
+        # Test value error for non-existent file
+        with pytest.raises(ValueError) as errMsg:
+            utils.importConditions(fileName='raiseErrorfileName')
+        assert 'Conditions file not found: %s' % os.path.abspath('raiseErrorfileName') in str(errMsg)
+        # Check file extensions in nested pandasToDictList()
+        conds = utils.importConditions(os.path.join(fixturesPath, 'trialTypes.csv'))
+        assert conds[0] == expected_cond
+        conds = utils.importConditions(os.path.join(fixturesPath, 'trialTypes.xls'))
+        assert conds[0] == expected_cond
+        conds = utils.importConditions(os.path.join(fixturesPath, 'trialTypes.pkl'))
+        assert conds[0] == expected_cond
+        # trialTypes.pkl saved in list of list format (see trialTypes.docx)
+        # test assertion for invalid file type
+        with pytest.raises(IOError) as errMsg:
+            utils.importConditions(os.path.join(fixturesPath, 'trialTypes.docx'))
+        assert ('Your conditions file should be an ''xlsx, csv or pkl file') in str(errMsg)
 
     def test_isValidVariableName(self):
         assert utils.isValidVariableName('Name') == (True, '')
@@ -55,6 +76,46 @@ class Test_utilsClass:
         assert utils.indicesFromString("6::2") == slice(6, None, 2)
         assert utils.indicesFromString("1,4,8") == [1, 4, 8]
 
+    def test_bootStraps(self):
+        import numpy as np
+        data = ['a','b','c']
+        assert isinstance(utils.bootStraps(data, n=1), np.ndarray)
+        assert utils.bootStraps(data,n = 1).shape == (1, 3, 1)
+        assert utils.bootStraps(data, n = 1).size == 3
+        assert utils.bootStraps(data, n=1).ndim == len(utils.bootStraps(data,n = 1).shape)
+
+    def test_functionFromStaircase(self):
+        import numpy as np
+        intensities = np.arange(0,1,.1)
+        responses = [1 if x >= .5 else 0 for x in intensities]
+        bin10, binUniq = 10, 'unique'
+        # try unequal dimension concatenation exception
+        with pytest.raises(Exception):
+            utils.functionFromStaircase(intensities, responses[:9], bin10)
+        assert isinstance(utils.functionFromStaircase(intensities, responses, bin10), tuple)
+        assert utils.functionFromStaircase(intensities, responses, binUniq).__len__() == 3
+        # test outputs from function
+        assert len(utils.functionFromStaircase(intensities, responses, bin10)[0]) == len(intensities)
+        assert len(utils.functionFromStaircase(intensities, responses, bin10)[1]) == len(responses)
+        assert len(utils.functionFromStaircase(intensities, responses, bin10)[2]) == len([1]*bin10)
+        assert len(utils.functionFromStaircase(intensities, responses, binUniq)[0]) == len(intensities)
+        assert len(utils.functionFromStaircase(intensities, responses, binUniq)[1]) == len(responses)
+        assert len(utils.functionFromStaircase(intensities, responses, binUniq)[2]) == len([1]*bin10)
+
+    def test_getDateStr(self):
+        from psychopy.constants import PY3
+        import time
+        assert utils.getDateStr() == time.strftime("%Y_%b_%d_%H%M", time.localtime())
+        # Test decoding exception
+        if PY3:
+            pass
+        else:
+            with pytest.raises(UnicodeDecodeError):
+                utils.getDateStr()
+
+
 if __name__ == '__main__':
     pytest.main()
+
+
 
