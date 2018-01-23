fb61cd6ad3ec5c054e4d619b96fa75cfb2f9b6d7
Hiroyuki Sogo <hsogo12600@gmail.com>
2018-01-24 08:42:18 +0900

    TESTS: move get_devices_infos, get_output_devices and get_input_devices to backend_pyo_utils

--- psychopy/sound/backend_pyo.py
+++ psychopy/sound/backend_pyo.py
@@ -15,7 +15,6 @@ from psychopy.constants import (STARTED, PLAYING, PAUSED, FINISHED, STOPPED,
                                 NOT_STARTED, FOREVER)
 try:
     import pyo
-    import sounddevice
 except ImportError as err:
     # convert this import error to our own, pyo probably not installed
     raise exceptions.DependencyError(repr(err))
@@ -25,6 +24,9 @@ from ._base import _SoundBase
 import atexit
 import threading
 pyoSndServer = None
+
+from .backend_pyo_utils import (get_input_devices, get_output_devices,
+                                get_devices_infos)
 
 def _bestDriver(devNames, devIDs):
     """Find ASIO or Windows sound drivers
@@ -54,44 +56,6 @@ def _bestDriver(devNames, devIDs):
     else:
         return None, None
 
-def get_devices_infos():
-    devices = sounddevice.query_devices()
-    in_devices = {}
-    out_devices = {}
-    for id, device in enumerate(devices):
-        if device['max_input_channels'] > 0:
-            param = {'host api index':device['hostapi'],
-                     'latency':device['default_low_input_latency'],
-                     'default sr':device['default_samplerate'],
-                     'name':device['name']}
-            in_devices[id] = param
-        if device['max_output_channels'] > 0:
-            param = {'host api index':device['hostapi'],
-                     'latency':device['default_low_output_latency'],
-                     'default sr':device['default_samplerate'],
-                     'name':device['name']}
-            out_devices[id] = param
-    return (in_devices, out_devices)
-
-def get_output_devices():
-    devices = sounddevice.query_devices()
-    names = []
-    ids = []
-    for id, device in enumerate(devices):
-        if device['max_output_channels'] > 0:
-            names.append(device['name'])
-            ids.append(id)
-    return (names, ids)
-
-def get_input_devices():
-    devices = sounddevice.query_devices()
-    names = []
-    ids = []
-    for id, device in enumerate(devices):
-        if device['max_input_channels'] > 0:
-            names.append(device['name'])
-            ids.append(id)
-    return (names, ids)
 
 def getDevices(kind=None):
     """Returns a dict of dict of audio devices of sepcified `kind`
