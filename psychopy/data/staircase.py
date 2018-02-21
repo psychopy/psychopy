psychopy/psychopy

commit 9dc4e7e23536d65b6025a8ece86ad822d7f815cc

RF: fixes to openpyxl.worksheet.cell due to API changes

psychopy/data/staircase.py
@@ -604,46 +604,46 @@ class StairHandler(_BaseTrialHandler):
 
         # write the data
         # reversals data
-        ws.cell('A1').value = 'Reversal Intensities'
-        ws.cell('B1').value = 'Reversal Indices'
+        ws['A1'] = 'Reversal Intensities'
+        ws['B1'] = 'Reversal Indices'
         for revN, revIntens in enumerate(self.reversalIntensities):
-            _cell = _getExcelCellName(col=0, row=revN + 1)  # col 0
-            ws.cell(_cell).value = str(revIntens)
-            _cell = _getExcelCellName(col=1, row=revN + 1)  # col 1
-            ws.cell(_cell).value = str(self.reversalPoints[revN])
+            ws.cell(column=1, row=revN+2,
+                    value=u"{}".format(revIntens))
+            ws.cell(column=2, row=revN+2,
+                    value=u"{}".format(self.reversalPoints[revN]))
 
         # trials data
-        ws.cell('C1').value = 'All Intensities'
-        ws.cell('D1').value = 'All Responses'
+        ws['C1'] = 'All Intensities'
+        ws['D1'] = 'All Responses'
         for intenN, intensity in enumerate(self.intensities):
-            ws.cell(_getExcelCellName(col=2, row=intenN + 1)
-                    ).value = str(intensity)
-            ws.cell(_getExcelCellName(col=3, row=intenN + 1)
-                    ).value = str(self.data[intenN])
+            ws.cell(column=3, row=intenN+2,
+                    value=u"{}".format(intensity))
+            ws.cell(column=4, row=intenN+2,
+                    value=u"{}".format(self.data[intenN]))
 
         # add other data
-        col = 4
+        col = 5
         if self.otherData is not None:
             # for varName in self.otherData:
             for key, val in list(self.otherData.items()):
-                ws.cell(_getExcelCellName(col=col, row=0)
-                        ).value = str(key)
+                ws.cell(column=col, row=1,
+                        value=u"{}".format(key))
                 for oDatN in range(len(self.otherData[key])):
-                    ws.cell(
-                        _getExcelCellName(col=col, row=oDatN + 1)
-                    ).value = str(self.otherData[key][oDatN])
+                    ws.cell(colummn=col, row=oDatN+2,
+                            value=u"{}".format(self.otherData[key][oDatN]))
                 col += 1
 
         # add self.extraInfo
         if self.extraInfo is not None and not matrixOnly:
-            ws.cell(_getExcelCellName(col=startingCol,
-                                      row=0)).value = 'extraInfo'
-            rowN = 1
+            ws.cell(column=startingCol, row=1,
+                    value='extraInfo')
+            rowN = 2
             for key, val in list(self.extraInfo.items()):
-                _cell = _getExcelCellName(col=col, row=rowN)
-                ws.cell(_cell).value = str(key) + u':'
+                ws.cell(column=col, row=rowN,
+                        value=u"{}:".format(key))
                 _cell = _getExcelCellName(col=col+1, row=rowN)
-                ws.cell(_cell).value = str(val)
+                ws.cell(column=col+2, row=rowN+1,
+                        value=u"{}".format(val))
                 rowN += 1
 
 

psychopy/data/base.py
@@ -356,9 +356,8 @@ class _BaseTrialHandler(_ComparisonMixin):
                     # if it can convert to a number (from numpy) then do it
                     val = float(entry)
                 except Exception:
-                    val = str(entry)
-                _cell = _getExcelCellName(col=colN, row=lineN)
-                ws.cell(_cell).value = val
+                    val = u"{}".format(entry)
+                ws.cell(column=colN+1, row=lineN+1, value=val)
 
         wb.save(filename=fileName)
 

psychopy/tests/utils.py
@@ -134,11 +134,12 @@ def compareXlsxFiles(pathToActual, pathToCorrect):
             expVal = expVal.value
             # intercept lists-of-floats, which might mismatch by rounding error
             isListableFloatable = False
-            if str(expVal).startswith('['):
-                expValList = eval(str(expVal))
+            if u"{}".format(expVal).startswith('['):
+                expValList = eval(u"{}".format(expVal))
                 try:
                     expVal = np.array(expValList, dtype=float)
-                    actVal = np.array(eval(str(actVal)), dtype=float) # should go through if expVal does...
+                    actVal = np.array(eval(u"{}".format(actVal)),
+                                      dtype=float) # should go through if expVal does...
                     isListableFloatable = True
                 except Exception:
                     pass # non-list+float-able at this point = default

commit bf6e399c5edbaa6fec82b0fa958ce7680b9223f5

FF: typo in staircase.saveAsExcel()
fixes #1729 (thanks @jano15 )
which was a bug introduced by 9dc4e7e23536d65b6025a8ece86ad822d7f815cc

psychopy/data/staircase.py
@@ -628,7 +628,7 @@ class StairHandler(_BaseTrialHandler):
                 ws.cell(column=col, row=1,
                         value=u"{}".format(key))
                 for oDatN in range(len(self.otherData[key])):
-                    ws.cell(colummn=col, row=oDatN+2,
+                    ws.cell(column=col, row=oDatN+2,
                             value=u"{}".format(self.otherData[key][oDatN]))
                 col += 1
 