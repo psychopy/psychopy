4c16bf6c43a79fd9403e05039bf11646657a300a
dvbridges <david-bridges@hotmail.co.uk>
2018-02-27 16:42:14 +0000

    BF: Resets ratingscale label color if using hover marker Rating Scale leaves the text label selected in the following trials when using hover as a marker style. This fix resets label text to original text color in thereset function, thus resetting label color on every trial.

--- psychopy/visual/ratingscale.py
+++ psychopy/visual/ratingscale.py
@@ -1300,6 +1300,10 @@ class RatingScale(MinimalStim):
         """
         # only resets things that are likely to have changed when the
         # ratingScale instance is used by a subject
+        # reset label color if using hover
+        if self.markerStyle == 'hover':
+            for labels in self.labels:
+                labels.setColor(self.textColor, log=False)
         self.noResponse = True
         # restore in case it turned gray, etc
         self.resetMarker = str(self.marker)
