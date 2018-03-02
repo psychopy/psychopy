c14603b8b7385425d6895c4e46fc5d7981a6c122
Jonathan Peirce <jon@peirce.org.uk>
2018-03-02 13:27:46 +0000

    PKG: some changes to win packaging script

--- buildWinDistributions.ps1
+++ buildWinDistributions.ps1
@@ -12,10 +12,13 @@ $names = @("PsychoPy2", "PsychoPy2_PY3")
 $v = [Io.File]::ReadAllText("C:\Users\lpzjwp\code\psychopy\git\version").Trim()
 
 for ($i=1; $i -lt 3; $i++) {
-    & $pips[$i] uninstall psychopy -y
-    # install the current version to site-packages
-    & xcopy /I /Y psychopy\*.txt $pyPaths[$i]
-    & xcopy /Y C:\Windows\System32\avbin.dll $pyPaths[$i]\avbin.dll
+    # try to uninstall psychopy from site-packages
+    Invoke-Expression "{0} uninstall psychopy -y" -f $pips[$i]
+    # re-install the current version as editable/developer
+    Invoke-Expression "{0} install . --no-deps" -f $pips[$i]
+	echo Installed current PsychoPy
+    xcopy /I /Y psychopy\*.txt $pyPaths[$i]
+    xcopy /Y C:\Windows\System32\avbin.dll $pyPaths[$i]\avbin.dll
     if ($i -eq '1') {
         xcopy /Y C:\Windows\System32\py*27.dll C:\Python27
     }
@@ -29,10 +32,10 @@ for ($i=1; $i -lt 3; $i++) {
 
     echo 'moving files to ..\dist'
 
-    # uninstall psychopy from site-packages
-    & $pips[$i] uninstall psychopy -y
+    # try to uninstall psychopy from site-packages
+    Invoke-Expression "{0} uninstall psychopy -y" -f $pips[$i]
     # re-install the current version as editable/developer
-    & $pips[$i] install -e .
+    Invoke-Expression "{0} install -e . --no-deps" -f $pips[$i]
 }
 
 Move-Item -Force "StandalonePsychoPy*.exe" ..\dist\
