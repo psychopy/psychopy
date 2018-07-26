e1ee3241710fc2b4ef6cfd9adf0fc9b504b42e57
Hiroyuki Sogo <hsogo12600@gmail.com>
2018-07-26 16:09:05 +0900

    FIX: fix typo

--- psychopy/experiment/components/slider/__init__.py
+++ psychopy/experiment/components/slider/__init__.py
@@ -137,7 +137,7 @@ class SliderComponent(BaseVisualComponent):
                 updates='constant', allowedUpdates=[],
                 hint=_translate(
                         "By default the labels will be on the bottom or "
-                        "left of the scale, but this can bee flipped to the "
+                        "left of the scale, but this can be flipped to the "
                         "other side."),
                 label=_translate('Flip'),
                 categ='Appearance')
