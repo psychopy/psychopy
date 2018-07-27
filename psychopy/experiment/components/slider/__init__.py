psychopy/psychopy

commit b5286f6a7852afdf6792b149dec62c8f9c3f903e

BF: Slider will now end routine on selection if required

psychopy/experiment/components/slider/__init__.py
@@ -204,6 +204,15 @@ class SliderComponent(BaseVisualComponent):
     def writeRoutineStartCode(self, buff):
         buff.writeIndented("%(name)s.reset()\n" % (self.params))
 
+    def writeFrameCode(self, buff):
+        super(SliderComponent, self).writeFrameCode(buff)  # Write basevisual frame code
+        forceEnd = self.params['forceEndRoutine'].val
+        if forceEnd:
+            code = ("\n# Check %(name)s for response to end routine\n"
+                    "if %(name)s.getRating() is not None and %(name)s.status == STARTED:\n"
+                    "    continueRoutine = False")
+        buff.writeIndentedLines(code % (self.params))
+
     def writeRoutineEndCode(self, buff):
         name = self.params['name']
         if len(self.exp.flow._loopList):

commit f4580f5a61daf37fc4efc8ee8546cbb35ebb99f9

BF: slider from builder failed to compile if forceEnd was False
Error was introduced in b5286f6a7852afdf6792b149dec62c8f9c3f903e

psychopy/experiment/components/slider/__init__.py
@@ -211,7 +211,7 @@ class SliderComponent(BaseVisualComponent):
             code = ("\n# Check %(name)s for response to end routine\n"
                     "if %(name)s.getRating() is not None and %(name)s.status == STARTED:\n"
                     "    continueRoutine = False")
-        buff.writeIndentedLines(code % (self.params))
+            buff.writeIndentedLines(code % (self.params))
 
     def writeRoutineEndCode(self, buff):
         name = self.params['name']