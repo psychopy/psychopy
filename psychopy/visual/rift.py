c7f99f354282d21a8772173ac0cee9b742f77d3f
mdc <cutonem@yorku.ca>
2018-08-20 11:19:54 -0400

    DOCS: Minor edits to 'setBuffer' docs.

--- psychopy/visual/rift.py
+++ psychopy/visual/rift.py
@@ -808,10 +808,7 @@ class Rift(window.Window):
         GL.glEnable(GL.GL_BLEND)
 
     def setBuffer(self, buffer, clear=True):
-        """Set the active view and use the appropriate frustum settings. For
-        stereoscopic displays, this demuxes drawing commands to the appropriate
-        view buffer. Set clear=False to prevent clearing the buffer prior to
-        use.
+        """Set the active stereo draw buffer.
 
         Warning! The window.Window.size property will return the buffer's
         dimensions in pixels instead of the window's when setBuffer is set to
