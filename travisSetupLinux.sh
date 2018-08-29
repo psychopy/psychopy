8272ce1e311c207930112c89442e48688a7a840c
Jon Peirce <jon.peirce@gmail.com>
2018-08-29 17:10:34 +0100

    TESTS: travis_retry not working from sh script still

--- travisSetupLinux.sh
+++ travisSetupLinux.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-source travis_retry.sh  # so that we can use travis_retry commands
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    "$@"
+    result=$?
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
 
 # Set up the install environment for travis linux installations
 
