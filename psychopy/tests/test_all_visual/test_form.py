72c297650566b368f0516d7843c0355603682cef
David Bridges <david-bridges@hotmail.co.uk>
2018-09-09 19:21:15 +0100

    TEST: Reduce number of form items created to reduce travis log length

--- psychopy/tests/test_all_visual/test_form.py
+++ psychopy/tests/test_all_visual/test_form.py
@@ -4,13 +4,16 @@
 from __future__ import division
 
 import pytest
+import warnings
 from psychopy.visual.window import Window
 from psychopy.visual.form import Form
 from psychopy.visual.text import TextStim
 from psychopy.visual.slider import Slider
 
+
 class Test_Form(object):
     """Test suite for Form component"""
+
     def setup_class(self):
         self.questions = []
         self.win = Window(units='height', allowStencil=True)
@@ -23,8 +26,7 @@ class Test_Form(object):
                       "aLayout": 'vert'}
         self.questions.append(self.genderItem)
         # then a set of ratings
-        items = ["running", "cake", "eating sticks", "programming",
-                 "tickling", "being tickled", "cycling", "driving", "swimming"]
+        items = ["running", "cake"]
         for item in items:
             entry = {"qText": "How much do you like {}".format(item),
                      "qWidth": 0.7,
@@ -33,10 +35,10 @@ class Test_Form(object):
                      "aOptions": ["Lots", "Not a lot"],
                      "aLayout": 'horiz'}
             self.questions.append(entry)
-        self.survey = Form(self.win, surveyItems=self.questions, size=(1.0, 0.7), pos=(0.0, 0.0))
+        self.survey = Form(self.win, surveyItems=self.questions, size=(1.0, 0.3), pos=(0.0, 0.0))
 
     def test_set_questions(self):
-        survey = Form(self.win, surveyItems=[], size=(1.0, 0.7), pos=(0.0, 0.0), itemPadding=0.1)
+        survey = Form(self.win, surveyItems=[], size=(1.0, 0.3), pos=(0.0, 0.0))
         textStim, qHeight, qWidth = survey._setQuestion(self.genderItem)
 
         assert type(textStim) == TextStim
@@ -44,7 +46,7 @@ class Test_Form(object):
         assert type(qWidth) == float
 
     def test_set_response(self):
-        survey = Form(self.win, surveyItems=[], size=(1.0, 0.7), pos=(0.0, 0.0), itemPadding=0.1)
+        survey = Form(self.win, surveyItems=[], size=(1.0, 0.3), pos=(0.0, 0.0))
         textStim, qHeight, qWidth = survey._setQuestion(self.genderItem)
         sliderStim, aHeight = survey._setResponse(self.genderItem, textStim)
 
@@ -64,8 +66,8 @@ class Test_Form(object):
             assert self.survey.getRespHeight(item['aOptions']) == (len(item['aOptions']) * self.survey.textHeight)
 
     def test_form_size(self):
-        assert self.survey.size[0] == (1.0, 0.7)[0]  # width
-        assert self.survey.size[1] == (1.0, 0.7)[1]  # height
+        assert self.survey.size[0] == (1.0, 0.3)[0]  # width
+        assert self.survey.size[1] == (1.0, 0.3)[1]  # height
 
     def test_aperture_size(self):
         assert self.survey.aperture.size[0] == self.survey.size[0]
@@ -90,10 +92,10 @@ class Test_Form(object):
         assert self.survey.units == 'height'
 
     def test_virtual_height(self):
-        assert self.survey.virtualHeight == self.survey._baseYpositions[-1] - self.survey.itemPadding
+        assert self.survey.virtualHeight == round(self.survey._baseYpositions[-1] - self.survey.itemPadding, 2)
 
     def test_baseYpositions(self):
-        survey = Form(self.win, surveyItems=self.questions, size=(1.0, 0.7), pos=(0.0, 0.0))
+        survey = Form(self.win, surveyItems=self.questions, size=(1.0, 0.3), pos=(0.0, 0.0))
         testPositions = []
         survey.virtualHeight = 0
         for item in survey.surveyItems:
@@ -119,7 +121,7 @@ class Test_Form(object):
     def test_screen_status(self):
         assert self.survey._inRange(self.survey._items['question'][0])
         with pytest.raises(AssertionError):
-            assert self.survey._inRange(self.survey._items['question'][9])
+            assert self.survey._inRange(self.survey._items['question'][2])
 
     def teardown_class(self):
         self.win.close()
