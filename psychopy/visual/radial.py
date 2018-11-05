b224c7d0f84677582e41d2642d29eb26793ddb6f
igmmgi <ian.mackenzie@uni-tuebingen.de>
2018-11-05 11:23:33 +0100

    BF: __del__ nethod within radial.py causes ModuleNotFoundError: import of pyglet halted; None in sys.modules
    remove __dell_ method so that __del__ method of grating is called

--- psychopy/visual/radial.py
+++ psychopy/visual/radial.py
@@ -616,10 +616,3 @@ class RadialStim(GratingStim):
         GL.glDisableClientState(GL.GL_TEXTURE_COORD_ARRAY)
 
         GL.glEndList()
-
-    def __del__(self):
-        """Remove textures from graphics card to prevent crash
-        """
-        if not self.useShaders:
-            GL.glDeleteLists(self._listID, 1)
-        self.clearTextures()
