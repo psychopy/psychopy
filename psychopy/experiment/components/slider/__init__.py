65d52c537b1eb02c8643f0d8ebe814b159c7a8d3
Jon Peirce <jon.peirce@gmail.com>
2019-01-15 18:41:31 +0000

    ENH: applying start/stop data collection to other Components

--- psychopy/experiment/components/slider/__init__.py
+++ psychopy/experiment/components/slider/__init__.py
@@ -6,6 +6,7 @@
 # Distributed under the terms of the GNU General Public License (GPL).
 
 from __future__ import absolute_import, print_function
+from builtins import super  # provides Py3-style super() using python-future
 
 from os import path
 from psychopy.experiment.components import BaseVisualComponent, Param, \
@@ -242,3 +243,6 @@ class SliderComponent(BaseVisualComponent):
             if self.params['storeHistory'].val == True:
                 code = "%s.addData('%s.history', %s.getHistory())\n"
                 buff.writeIndented(code % (loopName, name, name))
+
+        # get parent to write code too (e.g. store onset/offset times)
+        super().writeRoutineEndCode(buff)
