f99bef4a9d0bd5a17586b8fb96fb539d390ea874
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-01-30 12:41:57 +0100

    ENH/BF: Workaround for stderr confusion after importing sounddevice
    Fixes GH-2230.
    
    Old behavior:
    ```
    In [1]: from psychopy.sound import Sound
    pygame 1.9.4
    Hello from the pygame community. https://www.pygame.org/contribute.html
    
    In [2]: input('foo> ')
    bar.
    Out[2]: 'bar.'
    ```
    
    New behavior:
    ```
    In [1]: from psychopy.sound import Sound
    pygame 1.9.4
    Hello from the pygame community. https://www.pygame.org/contribute.html
    
    In [2]: input('foo> ')
    foo> bar.
    Out[2]: 'bar.'
    ```

--- psychopy/sound/backend_sounddevice.py
+++ psychopy/sound/backend_sounddevice.py
@@ -18,6 +18,7 @@ from ._base import _SoundBase, HammingWindow
 
 try:
     import sounddevice as sd
+    import readline  # Work around GH-2230.
 except Exception:
     raise DependencyError("sounddevice not working")
 try:
