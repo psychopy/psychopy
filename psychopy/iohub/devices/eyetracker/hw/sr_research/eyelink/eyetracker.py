5ef2b05f45f357d5b359ecb724103211b245e46f
lukassnoek <lukassnoek@gmail.com>
2019-02-22 13:59:08 +0100

    Fix bug when remove extension

--- psychopy/iohub/devices/eyetracker/hw/sr_research/eyelink/eyetracker.py
+++ psychopy/iohub/devices/eyetracker/hw/sr_research/eyelink/eyetracker.py
@@ -158,7 +158,7 @@ class EyeTracker(EyeTrackerDevice):
             if default_native_data_file_name:
                 if isinstance(default_native_data_file_name, (str, unicode)):
                     r = default_native_data_file_name.rfind('.')
-                    if default_native_data_file_name > 0:
+                    if r > 0:
                         if default_native_data_file_name[r:] == 'edf'.lower():
                             default_native_data_file_name = default_native_data_file_name[
                                 :r]
