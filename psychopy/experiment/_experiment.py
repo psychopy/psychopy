3411ea05431c15d0d5e7854bc9afd4a377da3dab
Jon Peirce <jon.peirce@gmail.com>
2019-03-11 15:25:07 +0000

    Revert "NF: Allow routines to be disabled"
    This reverts commit 69e8dbcdb05678a7cddea2063a12c15876627d62.

--- psychopy/experiment/_experiment.py
+++ psychopy/experiment/_experiment.py
@@ -185,28 +185,9 @@ class Experiment(object):
         else:
             localDateTime = data.getDateStr(format="%B %d, %Y, at %H:%M")
 
-        # Remove disabled routines and components, but leave original
-        # experiment unchanged.
+        # Remove disabled components, but leave original experiment unchanged.
         self_copy = deepcopy(self)
-        for routine_name, routine in list(self_copy.routines.items()):  # PY2/3 compat
-            if routine.params['disabled']:
-                # We remove all occurrences of this routine from the flow --
-                # there might be multiple ones!
-                #
-                # First, get all routines from the flow except the current
-                # (disabled) one.
-                routines = list(filter(lambda routine_:
-                                       routine_.name != routine_name,
-                                       self_copy.flow))
-
-                # We now replace the list of routines in the flow with the
-                # "cleaned" list of routines.
-                self_copy.flow[:] = routines
-
-                # We dropped an entire routine -- no need to look at individual
-                # components here anymore!
-                break
-
+        for _, routine in list(self_copy.routines.items()):  # PY2/3 compat
             for component in routine:
                 try:
                     if component.params['disabled'].val:
@@ -301,15 +282,8 @@ class Experiment(object):
         routinesNode = xml.SubElement(self.xmlRoot, 'Routines')
         # routines is a dict of routines
         for routineName, routine in self.routines.items():
-            routineNode = self._setXMLparam(parent=routinesNode,
-                                            param=routine,
-                                            name=routineName)
-
-            self._setXMLparam(parent=routineNode,
-                              param=Param(val=routine.params['disabled'],
-                                          valType='bool'),
-                              name='disabled')
-
+            routineNode = self._setXMLparam(
+                parent=routinesNode, param=routine, name=routineName)
             # a routine is based on a list of components
             for component in routine:
                 componentNode = self._setXMLparam(
@@ -595,27 +569,12 @@ class Experiment(object):
             if routineGoodName != routineNode.get('name'):
                 modifiedNames.append(routineNode.get('name'))
             self.namespace.user.append(routineGoodName)
-
             routine = Routine(name=routineGoodName, exp=self)
-
             # self._getXMLparam(params=routine.params, paramNode=routineNode)
             self.routines[routineNode.get('name')] = routine
-
             for componentNode in routineNode:
-                componentType = componentNode.tag
-                if componentType == 'Param':
-                    # This is a top-level "Param" node in this routine node,
-                    # not an actual component!
-                    #
-                    # We can retrieve the `disabled` parameter of this routine
-                    # from here.
-                    params = dict()
-                    self._getXMLparam(params=params,
-                                      paramNode=componentNode)
-                    routine.params['disabled'] = params['disabled'].val
-                    del params
-                    break
 
+                componentType = componentNode.tag
                 if componentType in allCompons:
                     # create an actual component of that type
                     component = allCompons[componentType](
@@ -626,7 +585,6 @@ class Experiment(object):
                     component = allCompons['UnknownComponent'](
                         name=componentNode.get('name'),
                         parentName=routineNode.get('name'), exp=self)
-
                 # check for components that were absent in older versions of
                 # the builder and change the default behavior
                 # (currently only the new behavior of choices for RatingScale,
