aa306d1ab4572c1bf24418b54e8d59d56bda7983
Jon Peirce <jon.peirce@gmail.com>
2019-04-17 12:25:22 +0100

    TESTS: switch from pytest_namespace to pytest_configure
    pytest_namespace is gone from pytest 4.1

--- psychopy/tests/conftest.py
+++ psychopy/tests/conftest.py
@@ -4,11 +4,5 @@
 import pytest
 from psychopy.app._psychopyApp import PsychoPyApp
 
-@pytest.fixture(scope="session", autouse=True)
-def pytest_namespace():
-    app = PsychoPyApp(testMode=True, showSplash=False)
-    return {'app': app}
-
-@pytest.mark.usefixtures('pytest_namespace')
-def pytest_sessionfinish(session, exitstatus):
-    pytest.app.quit()
+def pytest_configure():
+    pytest.app = PsychoPyApp(testMode=True, showSplash=False)
