a7987ef59483e04116f04cb671b6cdc9e0ac7a15
mdcutone <061413662GitHub>
2019-05-18 12:22:41 -0400

    EN: Initial commit of 'mathtools'

---
+++ psychopy/tools/mathtools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Various math functions.
+
+"""
+
+# Part of the PsychoPy library
+# Copyright (C) 2018 Jonathan Peirce
+# Distributed under the terms of the GNU General Public License (GPL).
+
+__all__ = []
