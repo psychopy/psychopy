35197e7ddfc9c475210a55319cebea2da11eca8a
mdcutone <061413662GitHub>
2019-05-18 12:35:01 -0400

    NF: Added 'slerp' and 'normalize'

--- psychopy/tools/mathtools.py
+++ psychopy/tools/mathtools.py
@@ -8,4 +8,81 @@
 # Copyright (C) 2018 Jonathan Peirce
 # Distributed under the terms of the GNU General Public License (GPL).
 
-__all__ = []
+__all__ = ['normalize', 'slerp']
+
+import numpy as np
+
+
+def normalize(v, dtype='float32'):
+    """Normalize a vector.
+
+    v : tuple, list or ndarray of float
+        Vector to normalize.
+    dtype : str or obj
+        Data type to use for all computations (eg. 'float32', 'float64', float,
+        etc.)
+
+    Returns
+    -------
+    ndarray
+        Normalized vector `v`.
+
+    """
+    v = np.asarray(v, dtype=dtype)
+    norm = np.linalg.norm(v)
+    if norm != 0.0:
+        v /= norm
+
+    return norm
+
+
+def slerp(q0, q1, t, dtype='float32'):
+    """Spherical linear interpolation (SLERP) between two quaternions.
+
+    Interpolation occurs along the shortest arc between the initial and final
+    quaternion.
+
+    Parameters
+    ----------
+    q0 : tuple, list or ndarray of float
+        Initial quaternion in form [x, y, z, w] where w is real and x, y, z
+        are imaginary components.
+    q1 : tuple, list or ndarray of float
+        Final quaternion in form [x, y, z, w] where w is real and x, y, z
+        are imaginary components.
+    t : float
+        Interpolation factor [0, 1].
+    dtype : str or obj
+        Data type to use for all computations (eg. 'float32', 'float64', float,
+        etc.)
+
+    Returns
+    -------
+    ndarray
+        Quaternion [x, y, z, w] at `t`.
+
+    """
+    # Implementation based on code found here:
+    #  https://en.wikipedia.org/wiki/Slerp
+    #
+    q0 = normalize(q0, dtype=dtype)
+    q1 = normalize(q1, dtype=dtype)
+
+    dot = np.dot(q0, q1)
+    if dot < 0.0:
+        q1 = -q1
+        dot = -dot
+
+    # small angle, use linear interpolation instead and return
+    if dot > 0.9995:
+        interp = q0 + t * (q1 - q0)
+        return normalize(interp, dtype=dtype)
+
+    theta0 = np.arccos(dot)
+    theta = theta0 * t
+    sinTheta = np.sin(theta)
+    sinTheta0 = np.sin(theta0)
+    s0 = np.cos(theta) - dot * sinTheta / sinTheta0
+    s1 = sinTheta / sinTheta0
+
+    return (q0 * s0) + (q1 * s1)
