154dad96e2664a30b1d4682a7f1a46365424da80
mdc <cutonem@yorku.ca>
2019-05-23 16:42:03 -0400

    BF: revert custom change to conf.py file I commited.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,8 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-import sys, os
-sys.path.insert(0, os.path.abspath(r'C:\Users\mdc\PycharmProjects\psychopy'))
-import psychopy
+
+import sys, os, psychopy
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
