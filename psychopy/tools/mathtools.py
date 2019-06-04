c8f50d9efd66383e699eb3151ba52e401a2754c6
mdc <cutonem@yorku.ca>
2019-06-04 11:32:48 -0400

    NF: Additional mathtools functions.

--- psychopy/tools/mathtools.py
+++ psychopy/tools/mathtools.py
@@ -10,19 +10,17 @@
 
 __all__ = ['normalize', 'lerp', 'slerp', 'multQuat', 'quatFromAxisAngle',
            'matrixFromQuat', 'scaleMatrix', 'rotationMatrix',
-           'translationMatrix']
+           'translationMatrix', 'concatenate', 'applyMatrix', 'invertQuat',
+           'quatToAxisAngle', 'poseToMatrix']
 
 import numpy as np
 
 
-def normalize(v, dtype='float32'):
+def normalize(v):
     """Normalize a vector or quaternion.
 
     v : tuple, list or ndarray of float
         Vector to normalize.
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -35,37 +33,36 @@ def normalize(v, dtype='float32'):
       returned.
 
     """
-    v = np.asarray(v, dtype=dtype)
+    v = np.asarray(v)
     norm = np.linalg.norm(v)
     if norm == 1.0:  # already normalized
         return v
     elif norm != 0.0:
         v /= norm
     else:
-        return np.zeros(v.shape, dtype=dtype)
+        return np.zeros_like(v)
 
     return v
 
 
-def lerp(v0, v1, t, dtype='float32'):
+def lerp(v0, v1, t, out=None):
     """Linear interpolation (LERP) between two vectors/coordinates.
 
     Parameters
     ----------
     v0 : tuple, list or ndarray of float
-        Initial vector in form [x, y, z].
+        Initial vector. Can be 2D where each row is a point.
     v1 : tuple, list or ndarray of float
-        Final vector in form [x, y, z].
+        Final vector. Must be the same shape as `v0`.
     t : float
-        Interpolation factor [0, 1].
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
+        Interpolation weight factor [0, 1].
+    out : ndarray, optional
+        Optional output array. Must have the same `shape` and `dtype` as `v0`.
 
     Returns
     -------
     ndarray
-        Vector [x, y, z] at `t`.
+        Vector  at `t` with same shape as `v0` and `v1`.
 
     Examples
     --------
@@ -76,13 +73,26 @@ def lerp(v0, v1, t, dtype='float32'):
         midpoint = lerp(u, v, 0.5)  # 0.5 to interpolate half-way between points
 
     """
-    v0 = np.asarray(v0, dtype=dtype)
-    v1 = np.asarray(v1, dtype=dtype)
-    t = float(t)
-    return (1.0 - t) * v0 + t * v1
+    v0 = np.asarray(v0)
+    v1 = np.asarray(v1)
+    assert v0.shape == v1.shape  # make sure the inputs have the same dims
+    origShape = v0.shape
+    v0, v1 = np.atleast_2d(v0, v1)
+
+    if out is None:
+        toReturn = np.zeros_like(v0)
+    else:
+        toReturn = out
+
+    ncols = v0.shape[1]
+    t0 = 1.0 - t
+    toReturn[:, 0:ncols] = t0 * v0[:, 0:ncols] + t * v1[:, 0:ncols]
+
+    if out is None:
+        return np.reshape(toReturn, origShape)
 
 
-def slerp(q0, q1, t, dtype='float32'):
+def slerp(q0, q1, t):
     """Spherical linear interpolation (SLERP) between two quaternions.
 
     Interpolation occurs along the shortest arc between the initial and final
@@ -97,10 +107,7 @@ def slerp(q0, q1, t, dtype='float32'):
         Final quaternion in form [x, y, z, w] where w is real and x, y, z
         are imaginary components.
     t : float
-        Interpolation factor [0, 1].
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
+        Interpolation weight factor [0, 1].
 
     Returns
     -------
@@ -116,8 +123,8 @@ def slerp(q0, q1, t, dtype='float32'):
     # Implementation based on code found here:
     #  https://en.wikipedia.org/wiki/Slerp
     #
-    q0 = normalize(q0, dtype=dtype)
-    q1 = normalize(q1, dtype=dtype)
+    q0 = normalize(q0)
+    q1 = normalize(q1)
 
     dot = np.dot(q0, q1)
     if dot < 0.0:
@@ -127,7 +134,7 @@ def slerp(q0, q1, t, dtype='float32'):
     # small angle, use linear interpolation instead and return
     if dot > 0.9995:
         interp = q0 + t * (q1 - q0)
-        return normalize(interp, dtype=dtype)
+        return normalize(interp)
 
     theta0 = np.arccos(dot)
     theta = theta0 * t
@@ -139,7 +146,54 @@ def slerp(q0, q1, t, dtype='float32'):
     return (q0 * s0) + (q1 * s1)
 
 
-def quatFromAxisAngle(axis, angle, degrees=False, dtype='float32'):
+def quatToAxisAngle(q, degrees=False):
+    """Convert a quaternion to `axis` and `angle` representation.
+
+    This allows you to use quaternions to set the orientation of stimuli that
+    have an `ori` property.
+
+    Parameters
+    ----------
+    q : tuple, list or ndarray of float
+        Quaternion in form [x, y, z, w] where w is real and x, y, z
+        are imaginary components.
+    degrees : bool
+        Indicate `angle` is to returned in degrees, otherwise `angle` will be
+        returned in radians.
+
+    Returns
+    -------
+    tuple
+        Axis and angle of quaternion in form ([ax, ay, az], angle). If `degrees`
+        is `True`, the angle returned is in degrees, radians if `False`.
+
+    Examples
+    --------
+    Using a quaternion to rotate a stimulus each frame::
+
+        # initial orientation, axis rotates in the Z direction
+        qr = quatFromAxisAngle(0.0, [0., 0., -1.], degrees=True)
+        # rotation per-frame, here it's 0.1 degrees per frame
+        qf = quatFromAxisAngle(0.1, [0., 0., -1.], degrees=True)
+
+        # ---- within main experiment loop ----
+        # myStim is a GratingStim or anything with an 'ori' argument which
+        # accepts angle in degrees
+        qr = multQuat(qr, qf)  # cumulative rotation
+        _, angle = quatToAxisAngle(qr)  # discard axis, only need angle
+        myStim.ori = angle
+        myStim.draw()
+
+    """
+    q = normalize(q)
+    v = np.sqrt(np.sum(np.square(q[:3])))
+    axis = (q[:3] / v) + 0.0
+    angle = 2.0 * np.arctan2(v, q[3])
+
+    return axis, np.degrees(angle) if degrees else angle
+
+
+def quatFromAxisAngle(axis, angle, degrees=False):
     """Create a quaternion to represent a rotation about `axis` vector by
     `angle`.
 
@@ -153,9 +207,6 @@ def quatFromAxisAngle(axis, angle, degrees=False, dtype='float32'):
     degrees : bool
         Indicate `angle` is in degrees, otherwise `angle` will be treated as
         radians.
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -172,15 +223,15 @@ def quatFromAxisAngle(axis, angle, degrees=False, dtype='float32'):
 
     """
     halfRad = np.radians(float(angle)) / 2.0 if degrees else float(angle) / 2.0
-    q = np.zeros((4,), dtype=dtype)
-    axis = normalize(axis, dtype=dtype)
+    q = np.zeros((4,))
+    axis = normalize(axis)
     np.multiply(axis, np.sin(halfRad), out=q[:3])
     q[3] = np.cos(halfRad)
 
     return q + 0.0  # remove negative zeros
 
 
-def multQuat(q0, q1, out=None, dtype='float32'):
+def multQuat(q0, q1, out=None):
     """Multiply quaternion `q0` and `q1`.
 
     The orientation of the returned quaternion is the combination of the input
@@ -194,9 +245,6 @@ def multQuat(q0, q1, out=None, dtype='float32'):
     out : ndarray or None
         Alternative array to write values. Must be `shape` == (4,) and same
         `dtype` as the `dtype` argument.
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -209,11 +257,11 @@ def multQuat(q0, q1, out=None, dtype='float32'):
     * Quaternions are normalized prior to multiplication.
 
     """
-    q0 = normalize(q0, dtype=dtype)
-    q1 = normalize(q1, dtype=dtype)
+    q0 = normalize(q0)
+    q1 = normalize(q1)
 
     if out is None:
-        qr = np.zeros((4,), dtype=dtype)
+        qr = np.zeros_like(q0)
     else:
         qr = out
 
@@ -226,7 +274,52 @@ def multQuat(q0, q1, out=None, dtype='float32'):
     return qr
 
 
-def matrixFromQuat(q, out=None, dtype='float32'):
+def invertQuat(q):
+    """Get tht multiplicative inverse of a quaternion.
+
+    This gives a quaternion which rotates in the opposite direction with equal
+    magnitude. Multiplying a quaternion by its inverse returns an identity
+    quaternion as both orientations cancel out.
+
+    Parameters
+    ----------
+    q : ndarray, list, or tuple of float
+        Quaternion to invert in form [x, y, z, w] where w is real and x, y, z
+        are imaginary components.
+
+    Returns
+    -------
+    ndarray
+        Inverse of quaternion `q`.
+
+    Examples
+    --------
+    Show that multiplying a quaternion by its inverse returns an identity
+    quaternion where [x=0, y=0, z=0, w=1]::
+
+        angle = 90.0
+        axis = [0., 0., -1.]
+        q = quatFromAxisAngle(axis, angle, degrees=True)
+        qinv = invertQuat(q)
+        qr = multQuat(q, qinv)
+        qi = np.array([0., 0., 0., 1.])  # identity quaternion
+        print(np.allclose(qi, qr))   # True
+
+    Notes
+    -----
+    * Quaternions are normalized prior to inverting.
+
+    """
+    qn = normalize(q)
+    # conjugate the quaternion
+    conj = np.zeros((4,))
+    conj[:3] = -1.0 * qn[:3]
+    conj[3] = qn[3]
+
+    return conj / np.sqrt(np.sum(np.square(qn)))
+
+
+def matrixFromQuat(q, out=None):
     """Create a rotation matrix from a quaternion.
 
     Parameters
@@ -237,9 +330,6 @@ def matrixFromQuat(q, out=None, dtype='float32'):
     out : ndarray or None
         Alternative array to write values. Must be `shape` == (4,4,) and same
         `dtype` as the `dtype` argument.
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -263,16 +353,20 @@ def matrixFromQuat(q, out=None, dtype='float32'):
                              [0., 1., 0., 1.],
                              [1., 1., 0., 1.]])
         newPoints = points.dot(rotMat)
-    
+
+    Notes
+    -----
+    * Quaternions are normalized prior to conversion.
+
     """
     # based off implementations from
     # https://github.com/glfw/glfw/blob/master/deps/linmath.h
-    q = normalize(q, dtype=dtype)
+    q = normalize(q)
     b, c, d, a = q[:]
     vsqr = np.square(q)
 
     if out is None:
-        R = np.zeros((4, 4,), dtype=dtype)
+        R = np.zeros((4, 4,), dtype=q.dtype)
     else:
         R = out
 
@@ -300,7 +394,7 @@ def matrixFromQuat(q, out=None, dtype='float32'):
         return R
 
 
-def scaleMatrix(s, dtype='float32'):
+def scaleMatrix(s):
     """Create a scaling matrix.
 
     The resulting matrix is the same as a generated by a `glScale` call.
@@ -309,9 +403,6 @@ def scaleMatrix(s, dtype='float32'):
     ----------
     s : ndarray, tuple, or list of float
         Scaling factors [sx, sy, sz].
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -320,7 +411,8 @@ def scaleMatrix(s, dtype='float32'):
 
     """
     # from glScale
-    S = np.zeros((4, 4,), dtype=dtype)
+    s = np.asarray(s)
+    S = np.zeros((4, 4,), dtype=s.dtype)
     S[0, 0] = s[0]
     S[1, 1] = s[1]
     S[2, 2] = s[2]
@@ -329,7 +421,7 @@ def scaleMatrix(s, dtype='float32'):
     return S
 
 
-def rotationMatrix(angle, axis, dtype='float32'):
+def rotationMatrix(angle, axis):
     """Create a rotation matrix.
 
     The resulting matrix will rotate points about `axis` by `angle`. The
@@ -341,9 +433,6 @@ def rotationMatrix(angle, axis, dtype='float32'):
         Rotation angle in degrees.
     axis : ndarray, list, or tuple of float
         Axis vector components.
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -355,7 +444,7 @@ def rotationMatrix(angle, axis, dtype='float32'):
     * Vector `axis` is normalized before creating the matrix.
 
     """
-    axis = normalize(axis, dtype=dtype)
+    axis = normalize(axis)
     angle = np.radians(angle)
     c = np.cos(angle)
     s = np.sin(angle)
@@ -365,7 +454,7 @@ def rotationMatrix(angle, axis, dtype='float32'):
     x, y, z = axis
     cd = 1.0 - c
 
-    R = np.zeros((4, 4,), dtype=dtype)
+    R = np.zeros((4, 4,), dtype=axis.dtype)
     R[0, 0] = x2 * cd + c
     R[0, 1] = x * y * cd - zs
     R[0, 2] = x * z * cd + ys
@@ -383,7 +472,7 @@ def rotationMatrix(angle, axis, dtype='float32'):
     return R + 0.0  # remove negative zeros
 
 
-def translationMatrix(t, dtype='float32'):
+def translationMatrix(t):
     """Create a translation matrix.
 
     The resulting matrix is the same as generated by a `glTranslate` call.
@@ -392,9 +481,6 @@ def translationMatrix(t, dtype='float32'):
     ----------
     t : ndarray, tuple, or list of float
         Translation vector [tx, ty, tz].
-    dtype : str or obj
-        Data type to use for all computations (eg. 'float32', 'float64', float,
-        etc.)
 
     Returns
     -------
@@ -402,7 +488,204 @@ def translationMatrix(t, dtype='float32'):
         4x4 translation matrix in row-major order.
 
     """
-    T = np.identity(4, dtype=dtype)
+    t = np.asarray(t)
+    T = np.identity(4, dtype=t.dtype)
     T[:3, 3] = t
 
     return T
+
+
+def concatenate(*args, out=None):
+    """Concatenate matrix transformations.
+
+    Combine transformation matrices into a single matrix. This is similar to
+    what occurs when building a matrix stack in OpenGL using `glRotate`,
+    `glTranslate`, and `glScale` calls. Matrices are multiplied together from
+    right-to-left, or the last argument to first. Note that changing the order
+    of the input matrices changes the final result.
+
+    Parameters
+    ----------
+    *args
+        4x4 matrices to combine of type `ndarray`. Matrices are multiplied from
+        right-to-left.
+    out : ndarray
+        Optional 4x4 output array. All computations will use the data type of
+        this array.
+
+    Returns
+    -------
+    ndarray
+        Concatenation of input matrices as a 4x4 matrix in row-major order.
+        `None` is returned if `out` was specified.
+
+    Examples
+    --------
+    Create an SRT (scale, rotate, and translate) matrix to convert model-space
+    coordinates to world-space::
+
+        S = scaleMatrix([2.0, 2.0, 2.0])  # scale model 2x
+        R = rotationMatrix(-90., [0., 0., -1])  # rotate -90 about -Z axis
+        T = translationMatrix([0., 0., -5.])  # translate point 5 units away
+        SRT = concatenate(S, R, T)
+
+        # transform a point in model-space coordinates to world-space
+        pointModel = np.array([0., 1., 0., 1.])
+        pointWorld = np.matmul(SRT, pointModel.T)  # point in WCS
+
+    Create a model-view matrix from a world-space pose represented by an
+    orientation (quaternion) and position (vector). The resulting matrix will
+    transform model-space coordinates to eye-space::
+
+        # stimulus pose as quaternion and vector
+        stimOri = quatFromAxisAngle([0., 0., -1.], -45.0)
+        stimPos = [0., 1.5, -5.]
+
+        # create model matrix
+        R = matrixFromQuat(stimOri)
+        T = translationMatrix(stimPos)
+        M = concatenate(R, T)  # model matrix
+
+        # create a view matrix, can also be represented as 'pos' and 'ori'
+        eyePos = [0., 1.5, 0.]
+        eyeFwd = [0., 0., -1.]
+        eyeUp = [0., 1., 0.]
+        V = lookAt(eyePos, eyeFwd, eyeUp)  # from viewtools
+
+        # modelview matrix
+        MV = concatenate(M, V)
+
+    Furthermore, you can go from model-space to homogeneous clip-space by
+    concatenating the projection, view, and model matrices::
+
+        # compute projection matrix, functions here are from 'viewtools'
+        screenWidth = 0.52
+        screenAspect = w / h
+        scrDistance = 0.55
+        frustum = computeFrustum(screenWidth, screenAspect, scrDistance)
+        P = perspectiveProjectionMatrix(*frustum)
+
+        # multiply model-space points by MVP to convert them to clip-space
+        MVP = concatenate(M, V, P)
+        pointModel = np.array([0., 1., 0., 1.])
+        pointClipSpace = np.matmul(MVP, pointModel.T)
+
+    """
+    if out is None:
+        toReturn = np.identity(4)
+        use_dtype = toReturn.dtype
+    else:
+        use_dtype = out.dtype
+        out[:, :] = np.identity(4, dtype=use_dtype)
+        toReturn = out
+
+    for mat in args:
+        np.matmul(np.asarray(mat, dtype=use_dtype), toReturn, out=toReturn)
+
+    if out is None:
+        return toReturn
+
+
+def applyMatrix(m, points, out=None):
+    """Apply a transformation matrix over a 2D array of points.
+
+    Parameters
+    ----------
+    m : ndarray
+        Transformation matrix.
+    points : ndarray
+        2D array of points/coordinates to transform, where each row is a single
+        point and the number of columns should match the dimensions of the
+        matrix.
+    out : ndarray, optional
+        Optional output array to write values. Must be same `shape` and `dtype`
+        as `points`.
+
+    Returns
+    -------
+    ndarray or None
+        Transformed points. If `None` if `out` was specified.
+
+    Examples
+    --------
+    Transform an array of points by some transformation matrix::
+
+        S = scaleMatrix([5.0, 5.0, 5.0])  # scale 2x
+        R = rotationMatrix(180., [0., 0., -1])  # rotate 180 degrees
+        T = translationMatrix([0., 1.5, -3.])  # translate point up and away
+        M = concatenate(S, R, T)  # create transform matrix
+
+        # points to transform, must be 2D!
+        points = np.array([[0., 1., 0., 1.], [-1., 0., 0., 1.]]) # [x, y, z, w]
+        newPoints = applyMatrix(M, points)  # apply the transformation
+
+    Extract the 3x3 rotation sub-matrix from a 4x4 matrix and apply it to
+    points. Here the result in written to an already allocated array::
+
+        points = np.array([[0., 1., 0.], [-1., 0., 0.]])  # [x, y, z]
+        outPoints = np.zeros(points.shape)
+        M = rotationMatrix(90., [1., 0., 0.])
+        M3x3 = M[:3, :3]  # extract rotation groups from the 4x4 matrix
+        # apply transformations, write to result to existing array
+        applyMatrix(M3x3, points, out=outPoints)
+
+    """
+    m = np.asarray(m)
+    points = np.asarray(points)
+    assert points.ndim == 2
+
+    if out is None:
+        toReturn = np.zeros(points.shape, dtype=points.dtype)
+    else:
+        # make sure we have the same dtype as the input
+        assert out.dtype == points.dtype and out.shape == points.shape
+        toReturn = out
+
+    np.dot(points, m.T, out=toReturn)
+    # toReturn[:, :] = points.dot(m.T)
+
+    if out is None:
+        return toReturn
+
+
+def poseToMatrix(pos, ori, out=None):
+    """Convert a pose to a 4x4 transformation matrix.
+
+    A pose is represented by a position coordinate `pos` and orientation
+    quaternion `ori`.
+
+    Parameters
+    ----------
+    pos : ndarray, tuple, or list of float
+        Position vector [x, y, z].
+    ori : tuple, list or ndarray of float
+        Orientation quaternion in form [x, y, z, w] where w is real and x, y, z
+        are imaginary components.
+    out : ndarray, optional
+        Optional output array for 4x4 matrix. All computations will use the data
+        type of this array.
+
+    Returns
+    -------
+    ndarray
+        4x4 transformation matrix. Returns `None` if `out` is specified.
+
+    """
+    if out is None:
+        use_dtype = pos.dtype
+        toReturn = np.zeros((4, 4,), dtype=use_dtype)
+    else:
+        use_dtype = out.dtype
+        toReturn = out
+
+    pos = np.asarray(pos, dtype=use_dtype)
+    ori = np.asarray(ori, dtype=use_dtype)
+    transMat = translationMatrix(pos)
+    rotMat = matrixFromQuat(ori)
+
+    np.matmul(rotMat, transMat, out=toReturn)
+
+    if out is None:
+        return toReturn
+
+
