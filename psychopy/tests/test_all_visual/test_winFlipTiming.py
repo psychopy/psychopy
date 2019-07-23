61ea578d50d255d454d2ed5b119ae96483c047a7
Jon Peirce <jon.peirce@gmail.com>
2019-07-23 15:55:31 +0100

    ENH: Window.nextFlipTime -> Window.getFutureFlipTime(targetTime)
    Rather than always giving the time for the next flip we want the
    flip time closest to some arbitrary point in future
    
    Also added tests for the new function

---
+++ psychopy/tests/test_all_visual/test_winFlipTiming.py
@@ -0,0 +1,83 @@
+
+
+from builtins import object
+from psychopy import visual, clock
+import pytest
+import numpy as np
+
+try:
+    from psychtoolbox import GetSecs
+    havePTB = True
+except ImportError:
+    havePTB = False
+
+class Test_WinFlipTiming(object):
+    def setup_class(self):
+        self.win = visual.Window(size=(200, 200), units='pix',
+                                 allowGUI=False, autoLog=False)
+
+    def teardown_class(self):
+        self.win.close()
+
+    def _runSeriesOfFlips(self, usePTB):
+        if usePTB:
+            getTime = GetSecs
+        else:
+            getTime = clock.monotonicClock.getTime
+
+        self.win.flip()
+        now = clock.monotonicClock.getTime()
+        next = self.win.getFutureFlipTime(ptb=usePTB)
+
+        errsNext = []
+        # check nextFrame against reality for 10 frames
+        for frameN in range(10):
+            self.win.flip()
+            this = getTime()
+            ## print('this', this)
+            ## print('err', next-this)
+            errsNext.append(next-this)
+            #then update next
+            next = self.win.getFutureFlipTime(ptb=usePTB)
+            ## print('next', next)
+        return errsNext
+
+    def test_winFutureFlip(self):
+        """test window.viewScale and .viewPos simultaneous
+        negative-going scale should mirror-reverse, and position should
+        account for that visually, the green square/rect should move clockwise
+        around the text
+
+        Non-zero viewOri would not currently pass with a nonzero viewPos
+        """
+        self.win.flip()
+        now = clock.monotonicClock.getTime()
+        next = self.win._frameTimes[-1] + self.win.monitorFramePeriod
+
+        errs = self._runSeriesOfFlips(usePTB=False)
+        print()
+        print('getFutureFlipTime(0): mean={:.6f}, std={:6f}, all={}'
+              .format(np.mean(errs), np.std(errs), errs))
+        assert np.mean(errs)<0.005  # checks for any systematic bias
+
+        if havePTB:
+            errs = self._runSeriesOfFlips(usePTB=True)
+            print('PTB getFutureFlipTime(0): mean={:.6f}, std={:6f}, all={}'
+                  .format(np.mean(errs), np.std(errs), errs))
+            assert np.mean(errs)<0.005  # checks for any systematic bias
+
+        now = clock.monotonicClock.getTime()
+        predictedFrames = []
+        print('now={:.5f}, lastFrame={:.5f}'.format(now, self.win._frameTimes[-1]))
+        print('delay requestT expectT diff'.format(now, self.win._frameTimes[-1]))
+        for delay in np.arange(0, 0.04, 0.001):
+            expect = self.win.getFutureFlipTime(now+delay)
+            requested = now+delay
+            diff = expect-requested
+            print("{:.4f}, {:.5f} {:.5f} {:.5f}".format(delay, requested, expect, diff))
+            # should always be within 1/2 frame
+            assert abs(diff) < self.win.monitorFramePeriod/2.0
+
+
+if __name__ == "__main__":
+    pytest.main(__file__)
