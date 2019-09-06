psychopy/psychopy

commit 1b205df597fb71b7fad2a4f16e0452d73653a693

BF: mac not running studies if they had spaces in filename!

psychopy/app/builder/builder.py
@@ -2185,11 +2185,6 @@ class BuilderFrame(wx.Frame):
             else:
                 _opts = wx.EXEC_ASYNC | wx.EXEC_SHOW_CONSOLE
         else:
-            # for unix this signifies a space in a filename
-            fullPath = fullPath.replace(' ', '\ ')
-            # for unix this signifies a space in a filename
-            pythonExec = sys.executable.replace(' ', '\ ')
-            # the quotes would break a unix system command
             command = [pythonExec, '-u', fullPath]
             _opts = wx.EXEC_ASYNC | wx.EXEC_MAKE_GROUP_LEADER
         # update app controls

commit e431b9f9be181e1cc4e658b8b635b988d993ea67

FF: commit 1b205df59 caused pythonExec no longer to be defined

psychopy/app/builder/builder.py
@@ -2185,7 +2185,7 @@ class BuilderFrame(wx.Frame):
             else:
                 _opts = wx.EXEC_ASYNC | wx.EXEC_SHOW_CONSOLE
         else:
-            command = [pythonExec, '-u', fullPath]
+            command = [sys.executable, '-u', fullPath]
             _opts = wx.EXEC_ASYNC | wx.EXEC_MAKE_GROUP_LEADER
         # update app controls
         self.toolbar.EnableTool(self.bldrBtnRun.Id, False)