7624c9fde63fd7aabf649d84a511f2d1ae38565e
David Bridges <david-bridges@hotmail.co.uk>
2019-09-18 16:27:26 +0100

    BF: Redefines key code 43 to tab on Mac (was escape)

--- psychopy/hardware/keyboard.py
+++ psychopy/hardware/keyboard.py
@@ -455,7 +455,7 @@ keyNamesMac = {
     29: 'z',
     30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7',
     37: '8', 38: '9', 39: '0',
-    40: 'return', 41: 'escape', 42: 'backspace', 43: 'escape', 44: 'space',
+    40: 'return', 41: 'escape', 42: 'backspace', 43: 'tab', 44: 'space',
     45: 'minus', 46: 'equal',
     47: 'bracketleft', 48: 'bracketright', 49: 'backslash', 51: 'semicolon',
     52: 'apostrophe', 53: 'grave', 54: 'comma', 55: 'period', 56: 'slash',
