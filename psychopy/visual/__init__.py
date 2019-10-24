7256634650b5a08810d1a08fe0a5d04ac51106cf
mdcutone <cutonem@yorku.ca>
2019-10-24 17:32:08 -0400

    RF; Move light sources in `stim3d` class.

--- psychopy/visual/__init__.py
+++ psychopy/visual/__init__.py
@@ -86,6 +86,7 @@ from psychopy.visual.textbox import TextBox
 from psychopy.visual.rift import Rift
 
 # 3D stimuli support
+from psychopy.visual.stim3d import LightSource
 from psychopy.visual.stim3d import RigidBodyPose
 from psychopy.visual.stim3d import SphereStim
 from psychopy.visual.stim3d import BoxStim
