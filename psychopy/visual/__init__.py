afe7757b26a678c24a2dee1750112872ad60db7e
mdcutone <cutonem@yorku.ca>
2019-10-28 12:24:49 -0400

    NF: Added 3d stim demo.

--- psychopy/visual/__init__.py
+++ psychopy/visual/__init__.py
@@ -87,6 +87,7 @@ from psychopy.visual.rift import Rift
 
 # 3D stimuli support
 from psychopy.visual.stim3d import LightSource
+from psychopy.visual.stim3d import PhongMaterial
 from psychopy.visual.stim3d import RigidBodyPose
 from psychopy.visual.stim3d import SphereStim
 from psychopy.visual.stim3d import BoxStim
