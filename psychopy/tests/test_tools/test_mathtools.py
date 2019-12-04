f82b3d9a0a6e93e628b00c79fe0a19c4b57cd43a
mdcutone <cutonem@yorku.ca>
2019-12-04 14:02:13 -0500

    TESTS: Stress test for view/projection matrix inverses.

--- psychopy/tests/test_tools/test_mathtools.py
+++ psychopy/tests/test_tools/test_mathtools.py
@@ -3,6 +3,7 @@
 """
 
 from psychopy.tools.mathtools import *
+from psychopy.tools.viewtools import *
 import numpy as np
 import pytest
 
@@ -163,13 +164,13 @@ def test_invertMatrix():
     """Test of the `invertMatrix()` function.
 
     Checks if the function can handle both homogeneous (rotation and translation
-    only) and regular case.
+    only) and regular cases.
 
     """
     np.random.seed(123456)
     N = 1000
     axes = np.random.uniform(-1.0, 1.0, (N, 3,))  # random axes
-    angles = np.random.uniform(0.0, 360.0, (N,))  # random angles
+    angles = np.random.uniform(-180.0, 180.0, (N,))  # random angles
     trans = np.random.uniform(-1000.0, 1000.0, (N, 3,))  # random translations
     scales = np.random.uniform(0.0001, 1000.0, (N, 3,))  # random scales
 
@@ -177,6 +178,9 @@ def test_invertMatrix():
     ident = np.identity(4)
     for i in range(N):
         r = rotationMatrix(angles[i], axes[i, :])
+
+        assert isOrthogonal(r)
+
         t = translationMatrix(trans[i, :])
 
         # combine them
@@ -193,9 +197,13 @@ def test_invertMatrix():
     rout = np.zeros((4, 4))
     tout = np.zeros((4, 4))
     sout = np.zeros((4, 4))
+    inv = np.zeros((4, 4))
     ident = np.identity(4)
     for i in range(N):
         rotationMatrix(angles[i], axes[i, :], out=rout)
+
+        assert isOrthogonal(rout)
+
         translationMatrix(trans[i, :], out=tout)
         scaleMatrix(scales[i, :], out=sout)
 
@@ -204,9 +212,51 @@ def test_invertMatrix():
 
         assert isAffine(m)  # must always be TRUE
 
-        inv = invertMatrix(m, homogeneous=False)
+        invertMatrix(m, homogeneous=False, out=inv)
 
         assert np.allclose(np.matmul(inv, m), ident)
+
+    # test with view/projection matrices
+    scrDims = np.random.uniform(0.01, 10.0, (N, 2,))
+    viewDists = np.random.uniform(0.025, 10.0, (N,))
+    eyeOffsets = np.random.uniform(-0.1, 0.1, (N,))
+
+    for i in range(N):
+        scrWidth = scrDims[i, 0]
+        scrAspect = scrDims[i, 0] / scrDims[i, 1]
+        viewDist = viewDists[i]
+
+        # nearClip some distance between screen and eye
+        nearClip = np.random.uniform(0.001, viewDist, (1,))
+
+        # nearClip some distance beyond screen
+        fcMin = viewDist + nearClip
+        farClip = np.random.uniform(fcMin, 1000.0, (1,))
+
+        frustum = computeFrustum(
+            scrWidth,
+            scrAspect,
+            viewDist,
+            eyeOffset=eyeOffsets[i],
+            nearClip=nearClip,
+            farClip=farClip)
+
+        proj = perspectiveProjectionMatrix(*frustum)
+        assert not isAffine(proj)
+
+        r = rotationMatrix(angles[i], axes[i, :])
+        assert isOrthogonal(r)
+
+        t = translationMatrix(trans[i, :])
+        view = np.matmul(t, r)
+        assert isAffine(view)
+
+        # combine them
+        vp = np.matmul(proj, view)
+
+        inv = invertMatrix(vp, homogeneous=False, out=inv)
+
+        assert np.allclose(np.matmul(inv, vp), ident)
 
 
 @pytest.mark.mathtools
