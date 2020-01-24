679ce18a8c3dfc99c59ed0d3f6ab3672cee2d9de
mdcutone <cutonem@yorku.ca>
2020-01-24 13:48:32 -0500

    NF: Initial commit of plugin system.

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -23,7 +23,7 @@ __git_sha__ = 'n/a'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
-           "event", "data", "sound", "microphone"]
+           "event", "data", "sound", "microphone", "plugins"]
 
 # for developers the following allows access to the current git sha from
 # their repository
