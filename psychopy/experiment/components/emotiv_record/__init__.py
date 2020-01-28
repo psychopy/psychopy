cf494f00c2ffb468abb1d54f8db374b1606e93ac
Bill King <bill@emotiv.com>
2020-01-28 18:30:53 +1100

    first draft of adding support of emotiv hardware

---
+++ psychopy/experiment/components/emotiv_record/__init__.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 11:20:49 2017
+
+@author: mrbki
+"""
+from os import path
+from psychopy.experiment.components import BaseComponent, Param, getInitVals
+from psychopy.localization import _translate
+
+
+OBJECT_NAME = 'cortex_obj'
+
+thisFolder = path.abspath(path.dirname(__file__))
+iconFile = path.join(thisFolder, 'emotiv.jpg')
+tooltip = _translate('Initialize EMOTIV data handler')
+
+
+class EmotivRecordingComponent(BaseComponent):  # or (VisualComponent)
+    def __init__(self, exp, parentName, name='cortex_rec'):
+        super(EmotivRecordingComponent, self).__init__(
+            exp, parentName, name=name,
+            startType='time (s)', startVal=0,
+            stopType='duration (s)', stopVal=1.0,
+            startEstim='', durationEstim='',
+            saveStartStop=False
+        )
+        self.exp.requirePsychopyLibs(['emotiv'])
+
+        # msg = _translate("Is first routine so initialize connection")
+        # self.params['firstCall'] = Param(
+        #     firstCall, valType='bool',
+        #     allowedVals=[True, False],
+        #     updates='constant', allowedUpdates=[],
+        #     hint=msg,
+        #     label=_localized["firstCall"])
+        #
+        # msg = _translate("Is last routine so close everything down")
+        # self.params['lastCall'] = Param(
+        #     lastCall, valType='bool',
+        #     allowedVals=[True, False],
+        #     updates='constant', allowedUpdates=[],
+        #     hint=msg,
+        #     label=_localized["lastCall"])
+        #
+    def writeInitCode(self, buff):
+        inits = getInitVals(self.params, 'PsychoPy')
+        code = ('{} = visual.BaseVisualStim('.format(inits['name']) +
+                'win=win, name="{}")\n'.format(inits['name'])
+                )
+        buff.writeIndentedLines(code)
+        code = ('{} = emotiv.Cortex()\n'.format(OBJECT_NAME))
+        buff.writeIndentedLines(code)
+
+    def writeFrameCode(self, buff):
+        pass
+
+    def writeExperimentEndCode(self, buff):
+        code = (
+                "core.wait(1) # Wait for EEG data to be packaged\n" +
+                "{}.close_session()\n".format(OBJECT_NAME)
+        )
+        buff.writeIndentedLines(code)
