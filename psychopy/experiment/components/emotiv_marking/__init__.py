cf4f286a6c002f1fac3f1f99ceed89479bdb2968
Bill King <bill@emotiv.com>
2020-01-30 10:24:59 +1100

    FF: Fixed missing defaults message

--- psychopy/experiment/components/emotiv_marking/__init__.py
+++ psychopy/experiment/components/emotiv_marking/__init__.py
@@ -18,8 +18,8 @@ iconFile = path.join(thisFolder, 'marker.jpg')
 tooltip = _translate('Mark a period of EEG')
 
 _localized = {
-    'label': _translate('Marker Label'),
-    'value': _translate('Marker Value'),
+    'marker_label': _translate('Marker Label'),
+    'marker_value': _translate('Marker Value'),
     'stop_marker': _translate('Stop Marker')
 }
 
@@ -29,7 +29,7 @@ class EmotivMarkingComponent(BaseComponent):  # or (VisualComponent)
                  startType='time (s)', startVal=0.0,
                  stopType='time (s)', stopVal=2,
                  startEstim='', durationEstim='0.1',
-                 label='label', value=1,
+                 label='label', value='1',
                  stop_marker=False):
         super(EmotivMarkingComponent, self).__init__(
             exp, parentName, name=name,
@@ -37,19 +37,23 @@ class EmotivMarkingComponent(BaseComponent):  # or (VisualComponent)
             stopType=stopType, stopVal=stopVal,
             startEstim=startEstim, durationEstim=0.01)
 
+        # params
+        _allow2 = ['constant', 'set every repeat']  # list
         msg = _translate(
-            "Label of the marker to be inserted")
-        self.params['label'] = Param(
+            "Label of the marker to be inserted (interpreted as a string)")
+        self.params['marker_label'] = Param(
             label, valType='str',
+            updates='constant', allowedUpdates=_allow2[:],
             hint=msg,
-            label=_localized['label'])
+            label=_localized['marker_label'])
 
         msg = _translate(
-            "Value of the marker to be inserted (interpreted as a string")
-        self.params['value'] = Param(
+            "Value of the marker to be inserted (interpreted as a string)")
+        self.params['marker_value'] = Param(
             value, valType='str',
+            updates='constant', allowedUpdates=_allow2[:],
             hint=msg,
-            label=_localized['value'])
+            label=_localized['marker_value'])
 
         msg = _translate("Check this box to include a stop marker")
         self.params['stop_marker'] = Param(
@@ -80,8 +84,8 @@ class EmotivMarkingComponent(BaseComponent):  # or (VisualComponent)
         self.writeParamUpdates(buff, 'set every frame')
         code = ("{}.inject_marker(value=str({}), label={})\n"
                     .format(OBJECT_NAME,
-                            self.params['value'],
-                            self.params['label']))
+                            self.params['marker_value'],
+                            self.params['marker_label']))
         buff.writeIndented(code)
         code = "{}.start_sent = True\n".format(self.params["name"])
         buff.writeIndented(code)
@@ -91,6 +95,7 @@ class EmotivMarkingComponent(BaseComponent):  # or (VisualComponent)
         if self.params['stopVal'].val not in ('', None, -1, 'None'):
             # writes an if statement to determine whether to draw etc
             self.writeStopTestCode(buff)
+#TODO understand if this line should be included            self.writeParamUpdates(buff, 'set every frame')
             code = "{}.status = FINISHED\n".format(self.params['name'])
             buff.writeIndented(code)
             if self.params['stop_marker'].val:
@@ -98,42 +103,3 @@ class EmotivMarkingComponent(BaseComponent):  # or (VisualComponent)
                 buff.writeIndented(code)
             buff.setIndentLevel(-1, relative=True)
         buff.setIndentLevel(-1, relative=True)
-
-# ==============================================================================
-#     def writeRoutineEndCode(self, buff):
-#         code = "del {}\n".format(self.params['name'])
-#         buff.writeIndentedLines(code)
-#         pass
-# ==============================================================================
-
-# ==============================================================================
-#     def writeStartTestCode(self, buff):
-#         """Test whether we need to start
-#         """
-#         lastLog.write('checking whether to start or not')
-#         if self.params['startType'].val == 'time (s)':
-#             # if startVal is an empty string then set to be 0.0
-#             if (isinstance(self.params['startVal'].val, basestring) and
-#                     not self.params['startVal'].val.strip()):
-#                 self.params['startVal'].val = '0.0'
-#             code = ("if t >= %(startVal)s "
-#                     "and %(name)s.status == NOT_STARTED:\n")
-#         elif self.params['startType'].val == 'frame N':
-#             code = ("if frameN >= %(startVal)s "
-#                     "and %(name)s.status == NOT_STARTED:\n")
-#         elif self.params['startType'].val == 'condition':
-#             code = ("if (%(startVal)s) "
-#                     "and %(name)s.status == NOT_STARTED:\n")
-#         else:
-#             msg = "Not a known startType (%(startType)s) for %(name)s"
-#             raise CodeGenerationException(msg % self.params)
-# 
-#         buff.writeIndented(code % self.params)
-# 
-#         buff.setIndentLevel(+1, relative=True)
-#         code = ("# keep track of start time/frame for later\n"
-#                 "%(name)s.tStart = t\n"
-#                 "%(name)s.frameNStart = frameN  # exact frame index\n")
-#         buff.writeIndentedLines(code % self.params)
-# 
-# ==============================================================================
