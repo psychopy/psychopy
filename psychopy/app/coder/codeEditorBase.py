bd52ebe44641edab2fe1974dda17e33e350cb4a1
mdcutone <cutonem@yorku.ca>
2020-02-11 17:47:04 -0500

    NF: Initial commit of code structure.

--- psychopy/app/coder/codeEditorBase.py
+++ psychopy/app/coder/codeEditorBase.py
@@ -32,6 +32,7 @@ class BaseCodeEditor(wx.stc.StyledTextCtrl):
         self.AUTOCOMPLETE = True
         self.autoCompleteDict = {}
         self._commentType = {'Py': '#', 'JS': '//', 'Both': '//' or '#'}
+        self.needsSourceTreeUpdate = False
 
         # doesn't pause strangely
         self.locals = None  # will contain the local environment of the script
