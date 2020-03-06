5d0e453cc1429c8c4a13960f69fa4ea9b5b45023
mdcutone <061413662GitHub>
2020-03-05 23:08:31 -0500

    FF: Remove test code.

--- psychopy/app/coder/coder.py
+++ psychopy/app/coder/coder.py
@@ -1977,7 +1977,6 @@ class CoderFrame(wx.Frame):
     def onIdle(self, event):
         # check the script outputs to see if anything has been written to
         # stdout
-        raise RuntimeError
         if self.scriptProcess is not None:
             if self.scriptProcess.IsInputAvailable():
                 stream = self.scriptProcess.GetInputStream()
@@ -2317,7 +2316,7 @@ class CoderFrame(wx.Frame):
 
             self.currentDoc.setLexerFromFileName()  # chose the best lexer
 
-            self.setFileModified(True)
+            self.setFileModified(False)
             self.currentDoc.SetFocus()
             self.statusBar.SetStatusText(self.currentDoc.getFileType(), 2)
 
