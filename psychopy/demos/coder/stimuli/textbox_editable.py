078da59fa7dc6219cbbe48dd18913a7315570386
Jon Peirce <jon.peirce@gmail.com>
2020-05-13 22:02:27 +0100

    DOCS: updated TextBox2 demo and moved it to demos folder

--- psychopy/demos/coder/stimuli/textbox_editable.py
+++ psychopy/demos/coder/stimuli/textbox_editable.py
@@ -16,6 +16,7 @@ c = core.Clock()
 from psychopy.visual.textbox2 import TextBox2, allFonts
 
 arial = "Arial Unicode MS"
+arial = "Arial"
 
 win = visual.Window([800, 800], monitor='testMonitor', backend='glfw')
 logging.exp("{:.3f}: created window".format(c.getTime()))
@@ -36,7 +37,7 @@ logging.flush()
 
 txt1 = TextBox2(win, color=[0.5, 0, 0, 0], text='Toptastic', font='Times',
                 pos=(-1, 4), letterHeight=2, units='deg',
-                anchor='left',
+                anchor='right',
                 borderColor='red',
                 fillColor='lightgrey',
                 editable=True)
@@ -44,8 +45,8 @@ txt1.draw()
 
 x, y = 0, 0
 #
-center = visual.Rect(win, width=2, height=2, units='pix')
-center.draw()
+# center = visual.Rect(win, width=2, height=2, units='pix')
+# center.draw()
 
 txt2 = TextBox2(win, color=[0, 0, 0, 0], text=loremIpsum, font=arial,
                 pos=(x, y), anchor='center', size=(700, -1), units='pix',
@@ -55,7 +56,7 @@ txt2 = TextBox2(win, color=[0, 0, 0, 0], text=loremIpsum, font=arial,
                 fillColor=None,
                 editable=True)
 print ('line lens:', txt2._lineLenChars)
-
+print ("line 2 botoms", txt2._lineBottoms)
 txt2.draw()
 
 logging.exp("{:.3f}: drew altered Arial text".format(c.getTime()))
@@ -65,21 +66,13 @@ logging.exp("{:.3f}: drew TextBox Times (no preload)".format(c.getTime()))
 
 logging.flush()
 
-stims = [center, txt1, txt2, ]
+stims = [txt1, txt2, ]
 win.flip()
 while True:
     # txt2.pos += 0.001
     for stim in stims:
         stim.draw()
     keys = event.getKeys()
-    if 'left' in keys:
-        txt2._moveCaret(chars=-1)
-    if 'right' in keys:
-        txt2._moveCaret(chars=+1)
-    if 'up' in keys:
-        txt2._moveCaret(lines=-1)
-    if 'down' in keys:
-        txt2._moveCaret(lines=+1)
     if 'q' in keys:
         core.quit()
 
