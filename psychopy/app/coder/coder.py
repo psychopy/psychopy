7ac9566951e04925a8e8ecd8f33bae5cafdf0809
Todd <TEParsons>
2020-06-11 14:36:55 +0100

    BF: Removed RuntimeError tester which was left in by mistake

--- psychopy/app/coder/coder.py
+++ psychopy/app/coder/coder.py
@@ -742,7 +742,7 @@ class CodeEditor(BaseCodeEditor, CodeEditorFoldingMixin):
 
         # quote line
         elif keyCode == ord("'"):
-            raise RuntimeError
+            #raise RuntimeError
             start, end = self.GetSelection()
             if end - start > 0:
                 txt = self.GetSelectedText()
