1b9370903a36b2d95a463507c6388741c29ab975
Todd <TEParsons>
2020-06-23 11:37:25 +0100

    BF: Remove literal comparator

--- psychopy/visual/shape.py
+++ psychopy/visual/shape.py
@@ -143,7 +143,7 @@ class BaseShapeStim(BaseVisualStim, ColorMixin, ContainerMixin):
 
         # if the fillColor and lineColor are not set but color is
         # then the user probably wants color applied to both
-        if (lineColor is (1.0, 1.0, 1.0)  # check if exactly as the default arg
+        if (lineColor == (1.0, 1.0, 1.0)  # check if exactly as the default arg
                 and fillColor is None
                 and color is not None):
             self.color = color
@@ -152,7 +152,7 @@ class BaseShapeStim(BaseVisualStim, ColorMixin, ContainerMixin):
         self.depth = depth
         self.ori = numpy.array(ori, float)
         self.size = numpy.array([0.0, 0.0]) + size  # make sure that it's 2D
-        if vertices is not ():  # flag for when super-init'ing a ShapeStim
+        if vertices != ():  # flag for when super-init'ing a ShapeStim
             self.vertices = vertices  # call attributeSetter
         self.autoDraw = autoDraw  # call attributeSetter
 
