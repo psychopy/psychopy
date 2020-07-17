8d36b22bfb3440a6d148c4d8eae971738e3c4606
Jon Peirce <jon.peirce@gmail.com>
2020-07-17 21:09:57 +0100

    ENH: only need to apply the new theme to the loaded doc not entire frame

--- psychopy/app/coder/coder.py
+++ psychopy/app/coder/coder.py
@@ -2175,7 +2175,7 @@ class CoderFrame(wx.Frame, ThemeMixin):
             self.Show()  # if the user had closed the frame it might be hidden
         if readonly:
             self.currentDoc.SetReadOnly(True)
-        self._applyAppTheme()
+        self.currentDoc._applyAppTheme()
 
     def fileOpen(self, event=None, filename=None):
         if not filename:
