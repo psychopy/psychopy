38ac839bdc0b02648754aa74815dcf46c35a6eef
Jon Peirce <jon.peirce@gmail.com>
2020-08-05 17:31:04 +0100

    PKG: bump version number and drop py27 from build
    [skip ci]

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-__version__ = '2020.2.0b1'
+__version__ = '2020.2.0dev1'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
 __author__ = 'Jonathan Peirce'
 __author_email__ = 'jon.peirce@gmail.com'
@@ -23,7 +23,7 @@ __git_sha__ = 'n/a'
 __build_platform__ = 'n/a'
 
 __all__ = ["gui", "misc", "visual", "core",
-           "event", "data", "sound", "microphone", "plugins"]
+           "event", "data", "sound", "microphone"]
 
 # for developers the following allows access to the current git sha from
 # their repository
