3edb748eb360aa42db5da3639f288b47acd419c3
mdcutone <061413662GitHub>
2020-09-09 17:26:39 -0400

    EN: Add the ability to refresh the components list after startup. Needed to handle loading Builder components from plugins.

--- psychopy/app/builder/builder.py
+++ psychopy/app/builder/builder.py
@@ -2019,50 +2019,8 @@ class ComponentsPanel(scrolledpanel.ScrolledPanel):
                                              style=wx.BORDER_NONE)
         self._maxBtnWidth = 0  # will store width of widest button
         self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.componentButtons = []
-        self.components = experiment.getAllComponents(
-            self.app.prefs.builder['componentsFolders'])
-        categories = ['Favorites']
-        categories.extend(components.getAllCategories(
-            self.app.prefs.builder['componentsFolders']))
-        # get rid of hidden components
-        for hiddenComp in self.frame.prefs['hiddenComponents']:
-            if hiddenComp in self.components:
-                del self.components[hiddenComp]
-        # also remove settings - that's in toolbar not components panel
-        del self.components['SettingsComponent']
-        # get favorites
-        self.favorites = FavoriteComponents(componentsPanel=self)
-        # create labels and sizers for each category
-        self.componentFromID = {}
-        self.panels = {}
-        # to keep track of the objects (sections and section labels)
-        # within the main sizer
-        self.sizerList = []
 
-        for categ in categories:
-            if categ in _localized:
-                label = _localized[categ]
-            else:
-                label = categ
-            _style = platebtn.PB_STYLE_DROPARROW | platebtn.PB_STYLE_SQUARE
-            sectionBtn = PsychopyPlateBtn(self, -1, label, style=_style, name=categ)
-            # Link to onclick functions
-            sectionBtn.Bind(wx.EVT_LEFT_DOWN, self.onSectionBtn)
-            sectionBtn.Bind(wx.EVT_RIGHT_DOWN, self.onSectionBtn)
-            # Set button background and link to onhover functions
-            #sectionBtn.Bind(wx.EVT_ENTER_WINDOW, self.onHover)
-            #sectionBtn.Bind(wx.EVT_LEAVE_WINDOW, self.offHover)
-            self.panels[categ] = wx.FlexGridSizer(cols=1)
-            self.sizer.Add(sectionBtn, flag=wx.EXPAND)
-            self.sizerList.append(sectionBtn)
-            self.sizer.Add(self.panels[categ], flag=wx.ALIGN_CENTER)
-            self.sizerList.append(self.panels[categ])
-        maxWidth = self.makeComponentButtons()
-        self._rightClicked = None
-        # start all except for Favorites collapsed
-        for section in categories[1:]:
-            self.toggleSection(self.panels[section])
+        self.buildComponentsList()  # build the icon tray
 
         self.Bind(wx.EVT_SIZE, self.on_resize)
         self.SetSizer(self.sizer)
@@ -2310,6 +2268,84 @@ class ComponentsPanel(scrolledpanel.ScrolledPanel):
         btn = evt.GetEventObject()
         btn.SetBackgroundColour(cs['panel_bg'])
         btn.SetForegroundColour(cs['text'])
+
+    def buildComponentsList(self, evt=None):
+        """Build the component buttons.
+
+        This function handles creating the icon tray at when the applications
+        starts. This function can be called to refresh the list if plugins
+        registering Builder components have been loaded after `loadPlugin` was
+        called.
+
+        Parameters
+        ----------
+        evt : wxEvent
+            Optional event object. This is needed if this method is bound to a
+            GUI event.
+
+        """
+        # clear the windows in the sizer
+        if self.sizer.Children:
+            self.sizer.Clear(True)
+
+        # get the components currently installed
+        self.componentButtons = []
+        self.components = experiment.getAllComponents(
+            self.app.prefs.builder['componentsFolders'])
+        categories = ['Favorites']
+        categories.extend(components.getAllCategories(
+            self.app.prefs.builder['componentsFolders']))
+
+        # get rid of hidden components
+        for hiddenComp in self.frame.prefs['hiddenComponents']:
+            if hiddenComp in self.components:
+                del self.components[hiddenComp]
+
+        # also remove settings - that's in toolbar not components panel
+        del self.components['SettingsComponent']
+
+        # get favorites
+        self.favorites = FavoriteComponents(componentsPanel=self)
+
+        # create labels and sizers for each category
+        self.componentFromID = {}
+        self.panels = {}
+
+        # to keep track of the objects (sections and section labels)
+        # within the main sizer
+        self.sizerList = []
+
+        for categ in categories:
+            if categ in _localized:
+                label = _localized[categ]
+            else:
+                label = categ
+            _style = platebtn.PB_STYLE_DROPARROW | platebtn.PB_STYLE_SQUARE
+            sectionBtn = PsychopyPlateBtn(
+                self, -1, label, style=_style, name=categ)
+
+            # Link to onclick functions
+            sectionBtn.Bind(wx.EVT_LEFT_DOWN, self.onSectionBtn)
+            sectionBtn.Bind(wx.EVT_RIGHT_DOWN, self.onSectionBtn)
+
+            # Set button background and link to onhover functions
+            #sectionBtn.Bind(wx.EVT_ENTER_WINDOW, self.onHover)
+            #sectionBtn.Bind(wx.EVT_LEAVE_WINDOW, self.offHover)
+            self.panels[categ] = wx.FlexGridSizer(cols=1)
+            self.sizer.Add(sectionBtn, flag=wx.EXPAND)
+            self.sizerList.append(sectionBtn)
+            self.sizer.Add(self.panels[categ], flag=wx.ALIGN_CENTER)
+            self.sizerList.append(self.panels[categ])
+
+        maxWidth = self.makeComponentButtons()
+        self._rightClicked = None
+
+        # start all except for Favorites collapsed
+        for section in categories[1:]:
+            self.toggleSection(self.panels[section])
+
+        if evt is not None:
+            evt.Skip()
 
 
 class FavoriteComponents(object):
