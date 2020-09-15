7abb6df5678ecf07397c82f7ece5fc78b2cd202f
Todd <TEParsons>
2020-09-15 09:54:00 +0100

    ENH: Use Color class when testing color conversions

--- psychopy/tests/test_misc/test_ColorConv.py
+++ psychopy/tests/test_misc/test_ColorConv.py
@@ -1,46 +1,103 @@
-from psychopy.tools.colorspacetools import hsv2rgb
+from psychopy.colors import Color
 import numpy
 
-#We need more tests of these conversion routines. Feel free to jump in and help! ;-)
+expectedHSV = numpy.array((
+       (  0, 1.0, 1.0),
+       (  0, 1.0, 0.5),
+       (  0, 0.5, 0.5),
+       ( 30, 1.0, 1.0),
+       ( 60, 1.0, 1.0),
+       ( 90, 1.0, 1.0),
+       (120, 1.0, 1.0),
+       (150, 1.0, 1.0),
+       (180, 1.0, 1.0),
+       (210, 1.0, 1.0),
+       (240, 1.0, 1.0),
+       (270, 1.0, 1.0),
+       (300, 1.0, 1.0),
+       (330, 1.0, 1.0),
+       (360, 1.0, 1.0)))
+expectedRGB = numpy.array([( 1.0, -1.0, -1.0),
+                           ( 1.0, -1.0, -1.0),
+                           ( 0.5, -0.5, -0.5),
+                           ( 1.0,  0.0, -1.0),
+                           ( 1.0,  1.0, -1.0),
+                           ( 0.0,  1.0, -1.0),
+                           (-1.0,  1.0, -1.0),
+                           (-1.0,  1.0,  0.0),
+                           (-1.0,  1.0,  1.0),
+                           (-1.0,  0.0,  1.0),
+                           (-1.0, -1.0,  1.0),
+                           ( 0.0, -1.0,  1.0),
+                           ( 1.0, -1.0,  1.0),
+                           ( 1.0, -1.0,  0.0),
+                           ( 1.0, -1.0, -1.0)])
+expectedRGB1 = numpy.array([(1.0,  0.0,  0.0),
+                            (1.0,  0.0,  0.0),
+                            (0.75, 0.25, 0.25),
+                            (1.0,  0.5,  0.0),
+                            (1.0,  1.0,  0.0),
+                            (0.5,  1.0,  0.0),
+                            (0.0,  1.0,  0.0),
+                            (0.0,  1.0,  0.5),
+                            (0.0,  1.0,  1.0),
+                            (0.0,  0.5,  1.0),
+                            (0.0,  0.0,  1.0),
+                            (0.5,  0.0,  1.0),
+                            (1.0,  0.0,  1.0),
+                            (1.0,  0.0,  0.5),
+                            (1.0,  0.0,  0.0)])
+expectedRGB255 = numpy.array([(255,   0,   0),
+                              (255,   0,   0),
+                              (191,  63,  63),
+                              (255, 127,   0),
+                              (255, 255,   0),
+                              (127, 255,   0),
+                              (  0, 255,   0),
+                              (  0, 255, 127),
+                              (  0, 255, 255),
+                              (  0, 127, 255),
+                              (  0,   0, 255),
+                              (127,   0, 255),
+                              (255,   0, 255),
+                              (255,   0, 127),
+                              (255,   0,   0)])
+expectedHex = numpy.array(['#ff0000',
+                           '#ff0000',
+                           '#bf3f3f',
+                           '#ff7f00',
+                           '#ffff00',
+                           '#7fff00',
+                           '#00ff00',
+                           '#00ff7f',
+                           '#00ffff',
+                           '#007fff',
+                           '#0000ff',
+                           '#7f00ff',
+                           '#ff00ff',
+                           '#ff007f',
+                           '#ff0000'])
+expectedNamed = numpy.array(['red',
+                             'red',
+                             None,
+                             None,
+                             'yellow',
+                             'chartreuse',
+                             'lime',
+                             'springgreen',
+                             'aqua',
+                             None,
+                             'blue',
+                             None,
+                             'fuchsia',
+                             None,
+                             'red'])
 
 
 def test_HSV_RGB():
-    HSV=numpy.array([
-       [  0,   1,   1],
-       [  0,   1, 0.5],#reducing Value reduces intensity of primary gun
-       [  0,   0.5, 0.5],#reducing Saturation increases intensity of other guns
-       [ 30,   1,   1],
-       [ 60,   1,   1],
-       [ 90,   1,   1],
-       [120,   1,   1],
-       [150,   1,   1],
-       [180,   1,   1],
-       [210,   1,   1],
-       [240,   1,   1],
-       [270,   1,   1],
-       [300,   1,   1],
-       [330,   1,   1],
-       [360,   1,   1]])
-    expectedRGB=numpy.array([
-       [ 1. ,  -1. ,  -1. ],
-       [ 0.,  -1. ,  -1. ],
-       [ 0., -0.5,  -0.5],
-       [ 1. ,  0.,  -1. ],
-       [ 1. ,  1. ,  -1. ],
-       [ 0.,  1. ,  -1. ],
-       [ -1. ,  1. ,  -1. ],
-       [ -1. ,  1. ,  0.],
-       [ -1. ,  1. ,  1. ],
-       [ -1. ,  0.,  1. ],
-       [ -1. ,  -1. ,  1. ],
-       [ 0.,  -1. ,  1. ],
-       [ 1. ,  -1. ,  1. ],
-       [ 1. ,  -1. ,  0.],
-       [ 1. ,  -1. ,  -1. ]])
-    RGB = numpy.array(
-       [hsv2rgb(hsv) for hsv in HSV]
-    )
-    assert numpy.allclose(RGB,expectedRGB,0.0001)
+   RGB = [Color(col, 'hsv').rgb for col in expectedHSV]
+   assert numpy.allclose(RGB, expectedRGB, 0.0001)
+
 
 if __name__=='__main__':
     test_HSV_RGB()
