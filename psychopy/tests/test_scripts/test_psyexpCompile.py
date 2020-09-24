b1b634c02bd31055f16ddbd50d1382d12f92ca3e
Todd <TEParsons>
2020-09-24 15:02:56 +0100

    TEST: Test that code from each tab of Code Component is compiled in scripts

--- psychopy/tests/test_scripts/test_psyexpCompile.py
+++ psychopy/tests/test_scripts/test_psyexpCompile.py
@@ -32,3 +32,30 @@ class TestDisabledComponents(object):
         with io.open(outfile, mode='r', encoding='utf-8-sig') as f:
             script = f.read()
             assert 'visual.TextStim' not in script
+
+    def test_all_code_component_tabs(self):
+        psyexp_file = os.path.join(TESTS_DATA_PATH,
+                                   'CodeComponent_eachtab.psyexp')
+        # Check py code from each tab exists
+        outfile = os.path.join(self.temp_dir, 'outfile.py')
+        psyexpCompile.compileScript(infile=psyexp_file, outfile=outfile)
+        with io.open(outfile, mode='r', encoding='utf-8-sig') as f:
+            script = f.read()
+        assert '___before_experiment___' in script
+        assert '___begin_experiment___' in script
+        assert '___begin_routine___' in script
+        assert '___each_frame___' in script
+        assert '___end_routine___' in script
+        assert '___end_experiment___' in script
+
+        # Check js code from each tab exists
+        outfile = os.path.join(self.temp_dir, 'outfile.js')
+        psyexpCompile.compileScript(infile=psyexp_file, outfile=outfile)
+        with io.open(outfile, mode='r', encoding='utf-8-sig') as f:
+            script = f.read()
+        assert '___before_experiment___;' in script
+        assert '___begin_experiment___;' in script
+        assert '___begin_routine___;' in script
+        assert '___each_frame___;' in script
+        assert '___end_routine___;' in script
+        assert '___end_experiment___;' in script
