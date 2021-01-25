c8b690d2238fd19dbb453e1b33c37944d70f269c
Todd <TEParsons>
2021-01-25 12:12:12 +0000

    ENH: Remove dollar sign from colorCtrl unless it is also code

--- psychopy/app/builder/dialogs/paramCtrls.py
+++ psychopy/app/builder/dialogs/paramCtrls.py
@@ -312,7 +312,7 @@ class ColorCtrl(wx.TextCtrl, _ValidatorMixin):
         self.valType = valType
         # Add sizer
         self._szr = wx.BoxSizer(wx.HORIZONTAL)
-        if not valType == "str":
+        if valType == "code":
             # Add $ for anything to be interpreted verbatim
             self.dollarLbl = wx.StaticText(parent, -1, "$", size=wx.Size(-1, -1), style=wx.ALIGN_RIGHT)
             self.dollarLbl.SetToolTip(_translate("This parameter will be treated as code - we have already put in the $, so you don't have to."))
