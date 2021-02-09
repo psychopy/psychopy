ac34530ab4117825adafa9e025e64ff58b8a84ab
Todd <TEParsons>
2021-02-09 09:00:34 +0000

    BF: Default opacity to None (i.e. do not enforce the same opacity across colors) rather than 1, meaning that e.g. (fillColor=None, borderColor=white) will look as expected. Fixes #3494 again, but properly this time.

--- psychopy/visual/rect.py
+++ psychopy/visual/rect.py
@@ -119,7 +119,7 @@ class Rect(BaseShapeStim):
                  pos=(0, 0),
                  size=None,
                  ori=0.0,
-                 opacity=1.0,
+                 opacity=None,
                  contrast=1.0,
                  depth=0,
                  interpolate=True,
