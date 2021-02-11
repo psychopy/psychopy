5005ee3117bd4b5842fb715d079775e93c8e2ce2
Todd <TEParsons>
2021-02-11 10:03:40 +0000

    BF: Append style to styleTweaks rather than add - handles listification better

--- psychopy/visual/slider.py
+++ psychopy/visual/slider.py
@@ -651,7 +651,7 @@ class Slider(MinimalStim, ColorMixin):
 
         # Legacy: If given a tweak, apply it as a tweak rather than a style
         if style in self.knownStyleTweaks + self.legacyStyleTweaks:
-            self.styleTweaks += style
+            self.styleTweaks.append(style)
 
     @attributeSetter
     def styleTweaks(self, styleTweaks):
