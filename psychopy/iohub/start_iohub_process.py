14d4959fdf93cfa318106b81ad654638d0b82d1d
Sol Simpson <sol@isolver-software.com>
2021-02-17 14:13:07 -0400

    ENH: Make gevent use original libev backend on windows
    ENH: Make gevent use original libev backend on windows. gevent now defaults to using libuv, which is limited to a 1 msec event loop, causing extra delay in iohub keyboard event timestamping. Switching to use libev on windows makes performance more like older 2.7 distro's.

--- psychopy/iohub/start_iohub_process.py
+++ psychopy/iohub/start_iohub_process.py
@@ -8,6 +8,15 @@ import os
 import sys
 import tempfile
 import gevent
+try:
+    if os.name == 'nt':
+        # Try to get gevent to use libev, not the default libuv.
+        # Libuv only has 1 msec loop resolution (at least on Windows
+        # not sure about other OS'es)
+        gevent.config.loop = "libev-cext"
+except:
+    pass
+
 import psutil
 import psychopy.clock as clock
 import psychopy.iohub
