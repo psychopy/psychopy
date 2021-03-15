b0be00990b10f32858f5cb08b24eb2946d7f1576
Todd <TEParsons>
2021-03-15 16:39:17 +0000

    NF: Add eye tracker recording class to hardware (currently does nothing)

--- psychopy/experiment/components/eyetracker/__init__.py
+++ psychopy/experiment/components/eyetracker/__init__.py
@@ -36,13 +36,13 @@ class EyetrackerComponent(BaseComponent):
                                startEstim=startEstim, durationEstim=durationEstim)
         self.type = 'Eyetracker'
         self.url = "https://www.psychopy.org/builder/components/eyetracker.html"
-        self.exp.requirePsychopyLibs(['iohub'])
+        self.exp.requirePsychopyLibs(['iohub', 'hardware'])
         # params
         self.order = ['config']  # first param after the name
 
         # useful params for the eyetracker - keep to a minimum if possible! ;-)
         self.params['config'] = Param(
-            configFile, valType='code', categ='Hardware',
+            config, valType='code', categ='Hardware',
             hint=_translate("Config routine for eyetracker"))
 
         self.params['rois'] = Param(
@@ -52,125 +52,80 @@ class EyetrackerComponent(BaseComponent):
             label=_translate("ROIs")
         )
 
-
-
     def writePreWindowCode(self, buff):
-        buff.writeIndented("#%(name)s: do calibration\n" % self.params)
-
-        # these might move to a more general place later, when we're always
-        # planning on having iohub running
-        code = ("io_config = iohub.load(open(%('Config file')s,'r'), "
-                "Loader=iohub.Loader)\n"
-                "io = iohub.ioHubConnection(io_config)\n"
-                "eyetracker = io.getDevice('tracker')\n"
-                "eyetracker.runSetupProcedure()\n")
-        buff.writeIndentedLines(code % self.params)
+        pass
 
     def writeInitCode(self, buff):
-        pass  # do we need anything after window creation but before run starts?
+        inits = self.params
+        code = (
+            "from psychopy.hardware import eyetracker\n"
+            "%(name)s = eyetracker.EyeTrackerRecorder(win, \n"
+        )
+        buff.writeIndentedLines(code % inits)
+        buff.setIndentLevel(1, relative=True)
+        code = (
+                "config=%(config)s,\n"
+                "rois=%(rois)s)\n"
+        )
+        buff.writeIndentedLines(code % inits)
+        buff.setIndentLevel(-1, relative=True)
 
     def writeRoutineStartCode(self, buff):
         """Write the code that will be called at the start of the routine
         """
         # create some lists to store recorded values positions and events if we
         # need more than one
-        code = "# setup some python lists for storing info about the %(name)s\n" % self.params
-        buff.writeIndented(code)
-        # a list of vals for each val, rather than a scalar
-        if self.params['saveState'].val in ['every frame', 'on click']:
-            code = ("%(name)s.x = []\n"
-                    "%(name)s.y = []\n"
-                    # is this common or is Jon making it up?!
-                    "%(name)s.pupil = []\n")
-            buff.writeIndentedLines(code % self.params)
+        pass
 
     def writeFrameCode(self, buff):
         """Write the code that will be called every frame
         """
-        buff.writeIndented("# *%s* updates\n" % self.params['name'])
+        inits = self.params
+        buff.writeIndentedLines("# *%s* updates\n" % self.params['name'])
 
         # test for whether we're just starting to record
         # writes an if statement to determine whether to draw etc
         self.writeStartTestCode(buff)
-        code = ("%(name)s.status = STARTED\n"
-                "# clear events and start tracking\n"
-                "io.clearEvents('all')\n"
-                "%(name)s.setRecordingState(True)\n")
+        code = (
+                "%(name)s.status = STARTED\n"
+        )
         buff.writeIndentedLines(code % self.params)
-
-        # to get out of the if statement
+        buff.setIndentLevel(-1, relative=True)
+        # Get rating each frame
+        code = (
+            "if %(name)s.status == STARTED:\n"
+        )
+        buff.writeIndentedLines(code % self.params)
+        buff.setIndentLevel(1, relative=True)
+        code = (
+                "%(name)s.poll()\n"
+        )
+        buff.writeIndentedLines(code % self.params)
         buff.setIndentLevel(-1, relative=True)
 
         # test for stop (only if there was some setting for duration or stop)
         if self.params['stopVal'].val not in ['', None, -1, 'None']:
             # writes an if statement to determine whether to draw etc
             self.writeStopTestCode(buff)
-
             code = ("%(name)s.status = FINISHED\n"
                     "%(name)s.setRecordingState(False)\n")
             buff.writeIndentedLines(code % self.params)
-
             # to get out of the if statement
             buff.setIndentLevel(-2, relative=True)
 
-        # if STARTED and not FINISHED!
-        code = "if %(name)s.status == STARTED:  # only update if started and not finished!\n" % self.params
-        buff.writeIndented(code)
-
-        buff.setIndentLevel(1, relative=True)  # to get out of the if statement
-        dedentAtEnd = 1  # keep track of how far to dedent later
-
-        code = ("%(name)s.x, %(name)s.y = eyetracker.getPosition()\n"
-                "%(name)s.pupil = eyetracker.getPupilSize()\n")
-        buff.writeIndentedLines(code % self.params)
-
-        # actual each-frame checks
-        if self.params['saveState'].val in ['every frame']:
-            code = ("x, y = eyetracker.getPosition()\n"
-                    "%(name)s.x.append(x)\n"
-                    "%(name)s.y.append(y)\n"
-                    "%(name)s.pupil.append(eyetracker.getPupilSize())\n")
-            buff.writeIndented(code % self.params)
-
-        # dedent
-        # 'if' statement of the time test and button check
-        buff.setIndentLevel(-dedentAtEnd, relative=True)
-
     def writeRoutineEndCode(self, buff):
-        # some shortcuts
-        name = self.params['name'].val
-        # do this because the param itself is not a string!
-        store = self.params['saveState'].val
-        # check if we're in a loop (so saving is possible)
+        inits = self.params
+
         if len(self.exp.flow._loopList):
-            currLoop = self.exp.flow._loopList[-1]  # last (outer-most) loop
-        else:
-            currLoop = None
-
-        # if store=='final' then update value
-        if store == 'final' and currLoop != None:
-            code = ("# get info about the %(name)s\n"
-                    "%(name)s.x, %(name)s.y = eyetracker.getPosition()\n"
-                    "x, y = %(name)s.getPos()\n"
-                    "%(name)s.pupil = eyetracker.getPupilSize()\n")
-            buff.writeIndentedLines(code % self.params)
-
-        # then push to psychopy data file if store='final','every frame'
-        if store != 'never' and currLoop != None:
-            buff.writeIndented("# save %(name)s data\n" % self.params)
-            for property in ['x', 'y', 'pupil']:
-                buff.writeIndented(
-                    "%s.addData('%s.%s', %s.%s)\n" %
-                    (currLoop.params['name'], name, property, name, property))
-
-        # make sure eyetracking stops recording (in case it hasn't stopped
-        # already)
-        buff.writeIndented("eyetracker.setRecordingState(False)\n")
-
-        # get parent to write code too (e.g. store onset/offset times)
+            inits['loop'] = self.exp.flow._loopList[-1].params['name']
+            code = (
+                 "%(loop)s.addData('%(name)s.x', list(%(name)s.data['x']))\n"
+                 "%(loop)s.addData('%(name)s.y', list(%(name)s.data['y']))\n"
+                 "%(loop)s.addData('%(name)s.roi', list(%(name)s.data['roi']))\n"
+                 "%(loop)s.addData('%(name)s.pupil', list(%(name)s.data['pupil']))\n"
+            )
+            buff.writeIndentedLines(code % inits)
         super().writeRoutineEndCode(buff)
 
     def writeExperimentEndCode(self, buff):
-        buff.writeIndented("eyetracker.setConnectionState(False)\n")
-        # in future this should be done generally, not by the eyetracker
-        buff.writeIndented("io.quit()\n")
+        pass
