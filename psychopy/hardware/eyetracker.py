3f40c74e281d0e39d33be921987c20f01f265b98
Todd <TEParsons>
2021-03-15 16:39:17 +0000

    NF: Add eye tracker recording class to hardware (currently does nothing)

--- psychopy/hardware/eyetracker.py
+++ psychopy/hardware/eyetracker.py
@@ -1,8 +1,58 @@
 from psychopy.iohub.devices import eyetracker as iohub
 #from psychopy.iohub.devices.eyetracker import models, eventTypes
-models = []
-eventTypes = []
+models = [None]
+eventTypes = [None]
 from ast import literal_eval as listify
+import numpy as np
+import pandas as pd
+from psychopy.visual.basevisual import ContainerMixin
+from psychopy.constants import NOT_STARTED
+
+
+class EyeTrackerRecorder:
+    def __init__(self, win, config,
+                 rois=None):
+        self.win = win
+        self.config = config
+        self.rois = rois
+        self.status = NOT_STARTED
+
+        self.data = pd.DataFrame({'x': [], 'y': [], 'roi': [], 'pupil': []})
+
+    @property
+    def rois(self):
+        if hasattr(self, "_rois"):
+            return self._rois
+        else:
+            return {}
+
+    @rois.setter
+    def rois(self, value):
+        if value is None:
+            return
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+        rois = {}
+        for obj in value:
+            if isinstance(obj, ContainerMixin):
+                rois[obj.name] = obj
+            else:
+                raise TypeError("Region of interest must be a PsychoPy object with vertices and a `contains` method")
+        self._rois = rois
+
+    def getPosition(self):
+        # Get x and y coords of eye
+        return
+
+    def poll(self):
+        x, y = (0.2, 0.2)
+        pupil = 1
+        # Check if x and y are in any rois
+        rois = []
+        for name, roi in self.rois.items():
+            if roi.contains(x, y):
+                rois.append(name)
+        self.data = self.data.append({'x': x, 'y': y, 'roi': rois, 'pupil': pupil}, ignore_index=True)
 
 
 class EyeTrackerConfig(dict):
