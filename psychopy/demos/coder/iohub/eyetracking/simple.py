1cafa85f4f58ce9f56b56ae765cfbbad2ef66ff4
Sol Simpson <sol@isolver-software.com>
2021-03-29 09:12:55 -0300

    RF: updated simple eyetracking demo
    Demo now gets display device info from psychopy window passed to launchHubServer

--- psychopy/demos/coder/iohub/eyetracking/simple.py
+++ psychopy/demos/coder/iohub/eyetracking/simple.py
@@ -9,11 +9,12 @@ from __future__ import absolute_import, division, print_function
 from psychopy import core, visual
 from psychopy.iohub import launchHubServer, EventConstants
 
+
 # Eye tracker to use ('mouse', 'eyelink', 'gazepoint', or 'tobii')
 TRACKER = 'mouse'
 
 eyetracker_config = dict(name='tracker')
-devices_config = {'Display': {'reporting_unit_type': 'pix', 'device_number': 0}}
+devices_config = {}
 if TRACKER == 'eyelink':
     eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
     eyetracker_config['simulation_mode'] = False
@@ -28,104 +29,109 @@ elif TRACKER == 'mouse':
     devices_config['eyetracker.hw.mouse.EyeTracker'] = eyetracker_config
 else:
     print("{} is not a valid TRACKER name; please use 'mouse', 'eyelink', 'gazepoint', or 'tobii'.".format(TRACKER))
+    core.quit()
 
 # Number if 'trials' to run in demo
 TRIAL_COUNT = 2
 # Maximum trial time / time timeout
 T_MAX = 60.0
+win = visual.Window((1920, 1080),
+                    units='pix',
+                    fullscr=True,
+                    allowGUI=False,
+                    monitor='55w_60dist'
+                    )
 
-if devices_config:
-    # Since no experiment or session code is given, no iohub hdf5 file
-    # will be saved, but device events are still available at runtime.
-    io = launchHubServer(**devices_config)
+win.setMouseVisible(False)
 
-    # Get some iohub devices for future access.
-    keyboard = io.getDevice('keyboard')
-    display = io.getDevice('display')
-    tracker = io.getDevice('tracker')
 
-    # print("display: ", display.getCoordinateType())
-    
-    # run eyetracker calibration
-    tracker.runSetupProcedure()
+# Since no experiment or session code is given, no iohub hdf5 file
+# will be saved, but device events are still available at runtime.
+io = launchHubServer(window=win, **devices_config)
 
-    win = visual.Window(display.getPixelResolution(),
-                        units=display.getCoordinateType(),
-                        fullscr=True,
-                        allowGUI=False
-                        )
 
-    win.setMouseVisible(True)
-    
-    gaze_ok_region = visual.Circle(win, lineColor='black', radius=300, units='pix')
+# Get some iohub devices for future access.
+keyboard = io.getDevice('keyboard')
+display = io.getDevice('display')
+tracker = io.getDevice('tracker')
 
-    gaze_dot = visual.GratingStim(win, tex=None, mask='gauss', pos=(0, 0),
-                                  size=(40, 40), color='green', units='pix')
+win.winHandle.minimize()  # minimize the PsychoPy window
 
-    text_stim_str = 'Eye Position: %.2f, %.2f. In Region: %s\n'
-    text_stim_str += 'Press space key to start next trial.'
-    missing_gpos_str = 'Eye Position: MISSING. In Region: No\n'
-    missing_gpos_str += 'Press space key to start next trial.'
-    text_stim = visual.TextStim(win, text=text_stim_str,
-                                pos=[0, 0], height=24,
-                                color='black', units='pix',
-                                wrapWidth=win.size[0] * .9)
+# run eyetracker calibration
+tracker.runSetupProcedure()
 
-    # Run Trials.....
-    t = 0
-    while t < TRIAL_COUNT:
-        io.clearEvents()
-        tracker.setRecordingState(True)
-        run_trial = True
-        tstart_time = core.getTime()
-        while run_trial is True:
-            # Get the latest gaze position in dispolay coord space..
-            gpos = tracker.getLastGazePosition()
-            for evt in tracker.getEvents():
-                if evt.type != EventConstants.MONOCULAR_EYE_SAMPLE:
-                    print(evt)
-            # Update stim based on gaze position
-            valid_gaze_pos = isinstance(gpos, (tuple, list))
-            gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
-            if valid_gaze_pos:
-                # If we have a gaze position from the tracker, update gc stim
-                # and text stim.
-                if gaze_in_region:
-                    gaze_in_region = 'Yes'
-                else:
-                    gaze_in_region = 'No'
-                text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region)
+win.winHandle.maximize()  # maximize the PsychoPy window
+win.winHandle.activate()
 
-                gaze_dot.setPos(gpos)
+gaze_ok_region = visual.Circle(win, lineColor='black', radius=300, units='pix')
+
+gaze_dot = visual.GratingStim(win, tex=None, mask='gauss', pos=(0, 0),
+                              size=(40, 40), color='green', units='pix')
+
+text_stim_str = 'Eye Position: %.2f, %.2f. In Region: %s\n'
+text_stim_str += 'Press space key to start next trial.'
+missing_gpos_str = 'Eye Position: MISSING. In Region: No\n'
+missing_gpos_str += 'Press space key to start next trial.'
+text_stim = visual.TextStim(win, text=text_stim_str,
+                            pos=[0, 0], height=24,
+                            color='black', units='pix',
+                            wrapWidth=win.size[0] * .9)
+
+# Run Trials.....
+t = 0
+while t < TRIAL_COUNT:
+    io.clearEvents()
+    tracker.setRecordingState(True)
+    run_trial = True
+    tstart_time = core.getTime()
+    while run_trial is True:
+        # Get the latest gaze position in dispolay coord space..
+        gpos = tracker.getLastGazePosition()
+        for evt in tracker.getEvents():
+            if evt.type != EventConstants.MONOCULAR_EYE_SAMPLE:
+                print(evt)
+        # Update stim based on gaze position
+        valid_gaze_pos = isinstance(gpos, (tuple, list))
+        gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
+        if valid_gaze_pos:
+            # If we have a gaze position from the tracker, update gc stim
+            # and text stim.
+            if gaze_in_region:
+                gaze_in_region = 'Yes'
             else:
-                # Otherwise just update text stim
-                text_stim.text = missing_gpos_str
+                gaze_in_region = 'No'
+            text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region)
 
-            # Redraw stim
-            gaze_ok_region.draw()
-            text_stim.draw()
-            if valid_gaze_pos:
-                gaze_dot.draw()
+            gaze_dot.setPos(gpos)
+        else:
+            # Otherwise just update text stim
+            text_stim.text = missing_gpos_str
 
-            # Display updated stim on screen.
-            flip_time = win.flip()
+        # Redraw stim
+        gaze_ok_region.draw()
+        text_stim.draw()
+        if valid_gaze_pos:
+            gaze_dot.draw()
 
-            # Check any new keyboard char events for a space key.
-            # If one is found, set the trial end variable.
-            #
-            if keyboard.getPresses(keys=' '):
-                run_trial = False
-            elif core.getTime()-tstart_time > T_MAX:
-                run_trial = False
-        win.flip()
-        # Current Trial is Done
-        # Stop eye data recording
-        tracker.setRecordingState(False)
-        t += 1
+        # Display updated stim on screen.
+        flip_time = win.flip()
 
-    # All Trials are done
-    # End experiment
-    tracker.setConnectionState(False)
+        # Check any new keyboard char events for a space key.
+        # If one is found, set the trial end variable.
+        #
+        if keyboard.getPresses(keys=' '):
+            run_trial = False
+        elif core.getTime()-tstart_time > T_MAX:
+            run_trial = False
+    win.flip()
+    # Current Trial is Done
+    # Stop eye data recording
+    tracker.setRecordingState(False)
+    t += 1
 
-    io.quit()
+# All Trials are done
+# End experiment
+tracker.setConnectionState(False)
+
+io.quit()
 core.quit()
