9458f9e55e8dfe42c0853e23f960bb8ef35b51e1
Jon Peirce <jon.peirce@gmail.com>
2021-04-29 20:05:24 +0100

    TESTS: glfw backend causing segfault on mac under GH Actions

--- psychopy/tests/test_all_visual/test_glfw_backend.py
+++ psychopy/tests/test_all_visual/test_glfw_backend.py
@@ -1,10 +1,10 @@
 # Provisional (super simple) test file which doesn't really test anything else
 # besides simply opening a `pyGLFW` window. Add more complete `glfw` backend
 # tests to other scripts ASAP, then remove this file.
-
+from psychopy.tests import utils
 from psychopy.visual.window import Window
 
-
+@utils.skip_under_ghActions()
 def test_open_glfw_window():
     win = Window(winType='glfw', autoLog=False)
     assert win.winType == 'glfw'
