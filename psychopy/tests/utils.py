31c76cef2aa58c65be723768aae27ca8c633d65c
Jon Peirce <jon.peirce@gmail.com>
2021-04-30 18:05:29 +0100

    TESTS: error in pytest.mark.skipif usage
    Need to give the reason using a named arg by the looks

--- psychopy/tests/utils.py
+++ psychopy/tests/utils.py
@@ -27,9 +27,9 @@ _anacondaTesting = bool("{}".format(os.environ.get('CONDA')).lower() == 'true')
 _githubActions = str(os.environ.get('GITHUB_WORKFLOW')) != 'None'
 _vmTesting = _travisTesting  or _githubActions
 # some pytest decorators for those conditions
-skip_under_travis = pytest.mark.skipif(_travisTesting, "Cannot run that test under Travis")
-skip_under_ghActions = pytest.mark.skipif(_githubActions, "Cannot run that test on GitHub Actions")
-skip_under_vm = pytest.mark.skipif(_vmTesting, "Cannot run that test on a virtual machine")
+skip_under_travis = pytest.mark.skipif(_travisTesting, reason="Cannot run that test under Travis")
+skip_under_ghActions = pytest.mark.skipif(_githubActions, reason="Cannot run that test on GitHub Actions")
+skip_under_vm = pytest.mark.skipif(_vmTesting, reason="Cannot run that test on a virtual machine")
 
 # define the path where to find testing data
 # so tests could be ran from any location
