62275ae2516fcebb902be7ffed099489c3f85557
Jon Peirce <jon.peirce@gmail.com>
2021-05-02 20:31:06 +0100

    TESTS: fixes for both mac and ubuntu
    for ubuntu
    - make sure gtk+3 is the dev install
    - install libSDL2
    
    for mac:
    - install pyo
    - tidied up microphone imports during tests

--- psychopy/tests/test_misc/test_microphone.py
+++ psychopy/tests/test_misc/test_microphone.py
@@ -6,6 +6,10 @@ import pytest
 import shutil, os, glob
 from tempfile import mkdtemp
 from os.path import abspath, dirname, join
+from psychopy import microphone
+from psychopy.microphone import _getFlacPath
+from psychopy import core
+
 from psychopy.tests.utils import skip_under_vm
 
 # py.test -k microphone --cov-report term-missing --cov microphone.py tests/
@@ -22,30 +26,27 @@ from psychopy.tests.utils import TESTS_PATH, TESTS_DATA_PATH
 class TestMicrophone(object):
     @classmethod
     def setup_class(self):
-        from psychopy import microphone
-        from psychopy.microphone import *
-        from psychopy.microphone import _getFlacPath
         global sound
         from psychopy import sound
-        switchOn(48000)
+        microphone.switchOn(48000)
         self.tmp = mkdtemp(prefix='psychopy-tests-microphone')
     @classmethod
     def teardown_class(self):
         if hasattr(self, 'tmp'):
             shutil.rmtree(self.tmp, ignore_errors=True)
-        switchOff()  # not needed, just get code coverage
+        microphone.switchOff()  # not needed, just get code coverage
 
     def test_AudioCapture_basics(self):
         microphone.haveMic = False
-        with pytest.raises(MicrophoneError):
-            AdvAudioCapture(autoLog=False)
+        with pytest.raises(microphone.MicrophoneError):
+            microphone.AdvAudioCapture(autoLog=False)
         microphone.haveMic = True
 
-        switchOn(16000, 1, 2048)
-        switchOn(48000)
+        microphone.switchOn(16000, 1, 2048)
+        microphone.switchOn(48000)
 
-        mic = AdvAudioCapture(saveDir=self.tmp, autoLog=False)
-        mic = AdvAudioCapture(saveDir=self.tmp+'_test', autoLog=False)
+        mic = microphone.AdvAudioCapture(saveDir=self.tmp, autoLog=False)
+        mic = microphone.AdvAudioCapture(saveDir=self.tmp+'_test', autoLog=False)
         mic.record(.10, block=False)  # returns immediately
         core.wait(.02)
         mic.stop()
@@ -56,10 +57,10 @@ class TestMicrophone(object):
 
     def test_AdvAudioCapture(self):
         filename = os.path.join(self.tmp, 'test_mic.wav')
-        mic = AdvAudioCapture(autoLog=False)
+        mic = microphone.AdvAudioCapture(autoLog=False)
         tone = sound.Sound(440, secs=.02, autoLog=False)
         mic.setMarker(tone=tone)
-        mic = AdvAudioCapture(filename=filename, saveDir=self.tmp, autoLog=False)
+        mic = microphone.AdvAudioCapture(filename=filename, saveDir=self.tmp, autoLog=False)
 
         mic.record(1, block=True)
         mic.setFile(mic.savedFile)  # same file name
@@ -118,7 +119,7 @@ class TestMicrophoneNoSound(object):
             t = join(TESTS_DATA_PATH, testFile)
             new_wav = join(self.tmp, testFile.replace('.dist', ''))
             shutil.copyfile(t, new_wav)
-            flac2wav(new_wav)
+            microphone.flac2wav(new_wav)
     @classmethod
     def teardown_class(self):
         if hasattr(self, 'tmp'):
@@ -126,7 +127,7 @@ class TestMicrophoneNoSound(object):
 
     def test_getFlacPath(self):
         microphone.FLAC_PATH = None
-        with pytest.raises(MicrophoneError):
+        with pytest.raises(microphone.MicrophoneError):
             _getFlacPath('this is not the flac you are looking for')
 
         microphone.FLAC_PATH = None
@@ -142,27 +143,27 @@ class TestMicrophoneNoSound(object):
         filename = os.path.join(self.tmp, 'test_bad_readWav')
         with open(filename, 'wb') as fd:
             fd.write(b'x')
-        with pytest.raises(SoundFileError):
-            readWavFile(filename)
+        with pytest.raises(microphone.SoundFileError):
+            microphone.readWavFile(filename)
 
         testFile = join(self.tmp, 'green_48000.wav')
-        newFile = wav2flac(testFile, keep=True)
-        flac2wav(newFile, keep=True)
+        newFile = microphone.wav2flac(testFile, keep=True)
+        microphone.flac2wav(newFile, keep=True)
 
-        newFile0 = wav2flac(testFile, keep=True, level=0)
-        newFile8 = wav2flac(testFile, keep=True, level=8)
+        newFile0 = microphone.wav2flac(testFile, keep=True, level=0)
+        newFile8 = microphone.wav2flac(testFile, keep=True, level=8)
         assert os.path.getsize(newFile0) >= os.path.getsize(newFile8)
 
-        wav2flac('.', keep=True)
-        flac2wav('.', keep=True)
-        wav2flac('', keep=True)
-        flac2wav('', keep=True)
+        microphone.wav2flac('.', keep=True)
+        microphone.flac2wav('.', keep=True)
+        microphone.wav2flac('', keep=True)
+        microphone.flac2wav('', keep=True)
 
-        wav2flac(self.tmp, keep=True)
+        microphone.wav2flac(self.tmp, keep=True)
 
     def test_Speech2Text(self):
         pytest.skip()  # google speech API gives Error 400: Bad request
-
+        from psychopy import web
         try:
             web.requireInternetAccess()
         except web.NoInternetAccessError:
@@ -171,7 +172,7 @@ class TestMicrophoneNoSound(object):
         # load a known sound file
         testFile = join(self.tmp, 'red_16000.wav')
 
-        gs = Speech2Text(filename=testFile)
+        gs = microphone.Speech2Text(filename=testFile)
         resp = gs.getResponse()
         assert resp.word == 'red'
 
@@ -179,9 +180,9 @@ class TestMicrophoneNoSound(object):
         tmp = join(self.tmp, 'tmp')
         os.mkdir(tmp)
         shutil.copy(testFile, tmp)
-        bs = BatchSpeech2Text(files=tmp)
+        bs = microphone.BatchSpeech2Text(files=tmp)
 
-        bs = BatchSpeech2Text(files=glob.glob(join(self.tmp, 'red_*.wav')))
+        bs = microphone.BatchSpeech2Text(files=glob.glob(join(self.tmp, 'red_*.wav')))
         while bs._activeCount():
             core.wait(.1, 0)
         resp = bs[0][1]
@@ -190,30 +191,30 @@ class TestMicrophoneNoSound(object):
 
     def test_DFT(self):
         testFile = join(self.tmp, 'red_16000.wav')
-        data, sampleRate = readWavFile(testFile)
+        data, sampleRate = microphone.readWavFile(testFile)
 
         with pytest.raises(OverflowError):
-            getDft([])
-        getDft(data)
-        getDftBins(data)
-        getDftBins(data, sampleRate=16000)
-        getDft(data, sampleRate=sampleRate)
-        getDft(data, wantPhase=True)
+            microphone.getDft([])
+        microphone.getDft(data)
+        microphone.getDftBins(data)
+        microphone.getDftBins(data, sampleRate=16000)
+        microphone.getDft(data, sampleRate=sampleRate)
+        microphone.getDft(data, wantPhase=True)
 
     def test_RMS(self):
         testFile = join(self.tmp, 'red_16000.wav')
-        data, sampleRate = readWavFile(testFile)
+        data, sampleRate = microphone.readWavFile(testFile)
 
-        rms = getRMS(data)
+        rms = microphone.getRMS(data)
         assert 588.60 < rms < 588.61
 
-        rmsb = getRMSBins(data, chunk=64)
+        rmsb = microphone.getRMSBins(data, chunk=64)
         assert 10.2 < rmsb[0] < 10.3
         assert len(rmsb) == 480
 
     def test_marker(self):
         testFile = join(self.tmp, 'green_48000.wav')
 
-        marker = getMarkerOnset(testFile)  # 19kHz marker sound
+        marker = microphone.getMarkerOnset(testFile)  # 19kHz marker sound
         assert 0.0666 < marker[0] < 0.06677  # start
         assert 0.0773 < marker[1] < 0.07734  # end
