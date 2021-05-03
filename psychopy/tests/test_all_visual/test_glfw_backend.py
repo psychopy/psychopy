561a40086908d368c40333892b53fdee23e9c1b3
Jon Peirce <jon.peirce@gmail.com>
2021-05-03 17:08:40 +0100

    TESTS: glfw still importing wrong skipif location

--- psychopy/tests/test_all_visual/test_glfw_backend.py
+++ psychopy/tests/test_all_visual/test_glfw_backend.py
@@ -1,11 +1,11 @@
 # Provisional (super simple) test file which doesn't really test anything else
 # besides simply opening a `pyGLFW` window. Add more complete `glfw` backend
 # tests to other scripts ASAP, then remove this file.
-from psychopy.tests import utils
+from psychopy.tests import skip_under_vm
 import pytest
 
 
-@pytest.mark.skipif(utils._vmTesting, reason="GLFW doesn't work on (macOS) virtual machine")
+@skip_under_vm(reason="GLFW doesn't work on (macOS) virtual machine")
 def test_open_glfw_window():
     from psychopy.visual.window import Window
     win = Window(winType='glfw', autoLog=False)
