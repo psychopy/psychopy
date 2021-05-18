a538f21881b02a680e9267aa8ab255f5ddeb7ae1
Sol Simpson <sol@isolver-software.com>
2021-05-18 11:19:42 -0300

    FF: Initial pass at validation api changes.....

--- psychopy/demos/coder/iohub/eyetracking/validation.py
+++ psychopy/demos/coder/iohub/eyetracking/validation.py
@@ -26,8 +26,14 @@ if __name__ == "__main__":
     # Get the eye tracker device.
     tracker = io.devices.tracker
 
+    win.winHandle.set_fullscreen(False)
+    win.winHandle.minimize()  # minimize the PsychoPy window
+
     # Run eyetracker calibration
-    r = tracker.runSetupProcedure()
+    #r = tracker.runSetupProcedure()
+
+    win.winHandle.set_fullscreen(True)
+    win.winHandle.maximize()  # maximize the PsychoPy window
 
     # ValidationProcedure setup
 
@@ -39,25 +45,25 @@ if __name__ == "__main__":
     # target_positions: Provide your own list of validation positions,
     target_positions = [(0.0, 0.0), (0.85, 0.85), (-0.85, 0.0), (0.85, 0.0), (0.85, -0.85), (-0.85, 0.85),
                         (-0.85, -0.85), (0.0, 0.85), (0.0, -0.85)]
-    # or:  use predefined validation point sets (three-point, five-point, nine-point, thirteen-point, seventeen-point)
-    #target_positions = 'nine-point'
+    #target_positions = 'NINE_POINTS'
 
     # Create a validation procedure, iohub must already be running with an
     # eye tracker device, or errors will occur.
     validation_proc = ValidationProcedure(win,
-                                          target=target_stim,
-                                          positions=target_positions,
-                                          randomize_positions=True,
-                                          animation_velocity=1.0,
-                                          animation_scale=3.0,
-                                          animation_duration=(0.2, 0.4),
+                                          target=target_stim,  # target stim
+                                          positions=target_positions,  # string constant or list of points
+                                          randomize_positions=True,  # boolean
+                                          expand_scale=2.0,  # float
+                                          target_duration=1.5,  # float
+                                          target_delay=1.0,  # float
+                                          enable_position_animation=True,
                                           color_space=None,  # None == use window color space
-                                          unit_type=None,  # Must be None for now.
-                                          progress_on_timeout=None,  # float or None
-                                          progress_on_key=' ',  # str, list of str, or None
-                                          gaze_cursor_color=(-1.0, 1.0, -1.0),
-                                          show_results_screen=True,
-                                          save_results_screen=True)
+                                          unit_type=None,
+                                          progress_on_key=" ",  # str or None
+                                          gaze_cursor=(-1.0, 1.0, -1.0),  # None or color value
+                                          show_results_screen=True,  # bool
+                                          save_results_screen=False,  # bool
+                                          )
 
     # Run the validation procedure. run() does not return until the validation is complete.
     validation_proc.run()
