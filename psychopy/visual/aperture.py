cd4fcab20e58c6bceff5a872a7932c381414d956
Todd <todd.e.parsons@googlemail.com>
2021-08-11 10:52:17 +0100

    ENH: Expose custom vertex params in Aperture

--- psychopy/visual/aperture.py
+++ psychopy/visual/aperture.py
@@ -102,6 +102,10 @@ class Aperture(MinimalStim, ContainerMixin):
             # (sin,cos):
             vertices = [(0.5 * sin(radians(theta)), 0.5 * cos(radians(theta)))
                         for theta in numpy.linspace(0, 360, nVert, False)]
+        elif isinstance(shape, int):
+            # if given a number, take it as a number of vertices and behave as if shape=='circle and nVerts==shape
+            vertices = [(0.5 * sin(radians(theta)), 0.5 * cos(radians(theta)))
+                        for theta in numpy.linspace(0, 360, shape, False)]
         elif shape == 'square':
             vertices = [[0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5], [0.5, 0.5]]
         elif shape == 'triangle':
