e92c620d24afc7c352864b543c037371bd9731c8
Pablo Prietz <pablo@prietz.org>
2021-08-26 14:27:21 +0200

    Fix imports

--- psychopy/iohub/devices/eyetracker/hw/pupil_labs/pupil_core/__init__.py
+++ psychopy/iohub/devices/eyetracker/hw/pupil_labs/pupil_core/__init__.py
@@ -2,4 +2,10 @@
 # Part of the PsychoPy library
 # Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
-from .eyetracker import EyeTracker, MonocularEyeSampleEvent, BinocularEyeSampleEvent
+from psychopy.iohub.devices.eyetracker.eye_events import (
+    MonocularEyeSampleEvent,
+    BinocularEyeSampleEvent,
+)
+from .eyetracker import EyeTracker
+
+__all__ = ["EyeTracker", "MonocularEyeSampleEvent", "BinocularEyeSampleEvent"]
