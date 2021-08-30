e39066a1a13b87d94e2dce553db3a4c38e04fa99
Matthew D. Cutone <mcutone@opensciencetools.org>
2021-08-30 12:08:47 -0400

    RF: Remove `from builtins import zip` from `iohub/devices/__init__.py`

--- psychopy/iohub/devices/__init__.py
+++ psychopy/iohub/devices/__init__.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-from builtins import zip
 import collections
 import copy
 import os
