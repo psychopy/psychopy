2a1106c19afe56874b5ed6e6f396da91f78b96cd
Matthew D. Cutone <mcutone@opensciencetools.org>
2021-08-30 11:49:41 -0400

    RF: Remove `from builtins import ...` from `parallel_vks.py`.

--- psychopy/voicekey/parallel_vks.py
+++ psychopy/voicekey/parallel_vks.py
@@ -4,7 +4,6 @@
 """Completely untested. Doesn't work at all on Mac (no parallel).
 """
 
-from builtins import hex
 from . signal import _BaseVoiceKeySignal
 from . vk_tools import get_time, sleep
 
