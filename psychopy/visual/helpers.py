61714a64d9549c08a9327e16720c9daa5b91e257
Matthew D. Cutone <mcutone@opensciencetools.org>
2021-08-30 11:48:59 -0400

    RF: Remove `from builtins import ...` from `helpers.py`.

--- psychopy/visual/helpers.py
+++ psychopy/visual/helpers.py
@@ -8,7 +8,6 @@
 # Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2021 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-from builtins import range
 import os
 import copy
 from pkg_resources import parse_version
