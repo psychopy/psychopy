213ee930552936cff30b8fe2148c2c40c09de215
Todd <todd.e.parsons@googlemail.com>
2021-09-22 16:27:35 +0100

    FF: Use correct API for me mode

--- psychopy/projects/pavlovia.py
+++ psychopy/projects/pavlovia.py
@@ -504,16 +504,16 @@ class PavloviaSearch(pandas.DataFrame):
         # Replace default filter
         if filterBy is None:
             filterBy = {}
-        # Add filter for "mine" only
+        # Define stem according to whether we're searching all projects or just owned
         if mine:
             session = getCurrentSession()
-            if "creatorId" not in filterBy:
-                filterBy['creatorId'] = []
-            filterBy['creatorId'] += [session.userID]
+            stem = f"api/v2/designers/{session.userID}"
+        else:
+            stem = "api/v2"
         # Ensure filter is a FilterTerm
         filterBy = self.FilterTerm(filterBy)
         try:
-            data = requests.get(f"https://pavlovia.org/api/v2/experiments?search={term}{filterBy}", timeout=2).json()
+            data = requests.get(f"https://pavlovia.org/{stem}/experiments?search={term}{filterBy}", timeout=2).json()
         except requests.exceptions.ReadTimeout:
             msg = "Could not connect to Pavlovia server. Please check that you are conencted to the internet. If you are connected, then the Pavlovia servers may be down. You can check their status here: https://pavlovia.org/status"
             raise ConnectionError(msg)
