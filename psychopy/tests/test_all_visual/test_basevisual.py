0d9cfe5e9a19ae8fa8fbb3fffe3be77bf6d4ddb2
Todd <todd.e.parsons@googlemail.com>
2021-11-22 14:49:47 +0000

    TEST: Flip after comparison in sizepos test

--- psychopy/tests/test_all_visual/test_basevisual.py
+++ psychopy/tests/test_all_visual/test_basevisual.py
@@ -353,12 +353,11 @@ class _TestUnitsMixin:
                     obj.pos = pos[units]
                     # Draw
                     obj.draw()
-                    win.flip()
-                    obj.draw()
                     # Compare screenshot
                     filename = f"{self.__class__.__name__}_{size['suffix']}_{pos['suffix']}.png"
                     #win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / filename)
                     utils.compareScreenshot(filename, win)
+                    win.flip()
         # Cleanup
         win.close()
         del obj
