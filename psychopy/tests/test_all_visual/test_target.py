a7a28de60025df7eb7642cb436dd795bb780e223
Todd <todd.e.parsons@googlemail.com>
2021-11-22 13:58:08 +0000

    TEST: Add test for radius setting for target

--- psychopy/tests/test_all_visual/test_target.py
+++ psychopy/tests/test_all_visual/test_target.py
@@ -1,4 +1,4 @@
-from psychopy import visual
+from psychopy import visual, layout
 from .test_basevisual import _TestColorMixin, _TestUnitsMixin
 
 class TestTarget(_TestColorMixin, _TestUnitsMixin):
@@ -17,3 +17,19 @@ class TestTarget(_TestColorMixin, _TestUnitsMixin):
         cls.win = visual.Window(size=(128, 128))
         cls.obj = visual.TargetStim(cls.win, "TargetStim", units='pix', pos=(-64, 64),
                                     innerRadius=20, radius=60, lineWidth=20)
+
+    def test_radius(self):
+        # Define some cases to test
+        cases = [
+            {"outer": 1, "inner": 0.5, "units": "height"},
+            {"outer": 100, "inner": 50, "units": "pix"},
+        ]
+
+        for case in cases:
+            # Set radiae (radiuses?) and units
+            self.obj.units = case['units']
+            self.obj.radius = case['outer']
+            self.obj.innerRadius = case['inner']
+            # Check that the target's size is set to twice the radius value
+            assert self.obj._size == layout.Size(case['outer']*2, case['units'], self.win)
+            assert self.obj.inner._size == layout.Size(case['inner']*2, case['units'], self.win)
