c685bd0418b073f5464097b72e8c12075365fe79
Todd <todd.e.parsons@googlemail.com>
2021-12-13 13:16:39 +0000

    TEST: Actually do comparison on size/pos tests

--- psychopy/tests/test_all_visual/test_basevisual.py
+++ psychopy/tests/test_all_visual/test_basevisual.py
@@ -357,8 +357,8 @@ class _TestUnitsMixin:
                     obj.draw()
                     # Compare screenshot
                     filename = f"{self.__class__.__name__}_{size['suffix']}_{pos['suffix']}.png"
-                    win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / filename)
-                    #utils.compareScreenshot(filename, win)
+                    #win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / filename)
+                    utils.compareScreenshot(filename, win)
         # Cleanup
         win.close()
         del obj
