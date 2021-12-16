e626ad034a5f1bc05b89d4c91a7404d62e099d90
Todd <todd.e.parsons@googlemail.com>
2021-12-16 12:13:47 +0000

    ENH: Slightly increase tolerance in possize test to account for OS differences in anti-aliasing

--- psychopy/tests/test_all_visual/test_basevisual.py
+++ psychopy/tests/test_all_visual/test_basevisual.py
@@ -356,7 +356,7 @@ class _TestUnitsMixin:
                     # Compare screenshot
                     filename = f"{self.__class__.__name__}_{size['suffix']}_{pos['suffix']}.png"
                     #win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / filename)
-                    utils.compareScreenshot(filename, win)
+                    utils.compareScreenshot(filename, win, crit=7)
                     win.flip()
         # Cleanup
         win.close()
