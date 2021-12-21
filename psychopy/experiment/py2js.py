88813f36787825bfa300bc185562b14f0d5363d6
Jon Peirce <jon.peirce@gmail.com>
2021-12-21 10:25:02 +0000

    BF: if parsing JS fails we might not see the error message
    If parsing to an IO buffer (instead of a filename) then there was an
    error in the except.
    Either way, there was no call to flush the logging.error so wouldn't
    be immediately visible.

--- psychopy/experiment/py2js.py
+++ psychopy/experiment/py2js.py
@@ -159,7 +159,11 @@ def addVariableDeclarations(inputProgram, fileName):
     try:
         ast = esprima.parseScript(inputProgram, {'range': True, 'tolerant': True})
     except esprima.error_handler.Error as err:
-        logging.error("{0} in {1}".format(err, path.split(fileName)[1]))
+        if fileName:
+            logging.error(f"Error parsing JS in {path.split(fileName)[1]}:\n{err}")
+        else:
+            logging.error(f"Error parsing JS: {err}")
+        logging.flush()
         return inputProgram  # So JS can be written to file
 
     # find undeclared vars in functions and declare them before the function
