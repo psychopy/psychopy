d5ccc92a045db0dc4a95905e46827d207aba457f
Todd <todd.e.parsons@googlemail.com>
2022-01-05 14:42:53 +0000

    BF: Don't clear keypresses after checking for esc, as it's now the same backend as experiment keys

--- psychopy/experiment/routines/_base.py
+++ psychopy/experiment/routines/_base.py
@@ -423,7 +423,7 @@ class Routine(list):
         # allow subject to quit via Esc key?
         if self.exp.settings.params['Enable Escape'].val:
             code = ('\n# check for quit (typically the Esc key)\n'
-                    'if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):\n'
+                    'if endExpNow or defaultKeyboard.getKeys(keyList=["escape"], clear=False):\n'
                     '    core.quit()\n')
             buff.writeIndentedLines(code)
 
