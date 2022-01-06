16f96cb2c982ea7974ab8430d07d164af7c4031e
Todd <todd.e.parsons@googlemail.com>
2022-01-06 16:45:11 +0000

    FF: Elif rather than if for GetValue

--- psychopy/app/builder/dialogs/__init__.py
+++ psychopy/app/builder/dialogs/__init__.py
@@ -671,7 +671,7 @@ class ParamNotebook(wx.Notebook, ThemeMixin):
             # Get value
             if hasattr(ctrl, "getValue"):
                 param.val = ctrl.getValue()
-            if hasattr(ctrl, "GetValue"):
+            elif hasattr(ctrl, "GetValue"):
                 param.val = ctrl.GetValue()
             elif isinstance(ctrl, wx.Choice):
                 if hasattr(ctrl, "_choices"):
