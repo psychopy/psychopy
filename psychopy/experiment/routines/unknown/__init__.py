3ef00604cc49db5418cb93c2463bb566a60b9092
Todd <todd.e.parsons@googlemail.com>
2022-01-10 10:51:00 +0000

    ENH: If component/routine is unknown, compile only a comment saying that it is unknown

--- psychopy/experiment/routines/unknown/__init__.py
+++ psychopy/experiment/routines/unknown/__init__.py
@@ -11,3 +11,19 @@ class UnknownRoutine(BaseStandaloneRoutine):
     def __init__(self, exp, name=''):
         BaseStandaloneRoutine.__init__(self, exp, name=name)
         self.type = 'UnknownStandaloneRoutine'
+
+    def writeInitCode(self, buff):
+        code = (
+            "\n"
+            "# Unknown standalone routine ignored: %(name)s\n"
+            "\n"
+        )
+        buff.writeIndentedLines(code % self.params)
+
+    def writeInitCodeJS(self, buff):
+        code = (
+            "\n"
+            "// Unknown standalone routine ignored: %(name)s\n"
+            "\n"
+        )
+        buff.writeIndentedLines(code % self.params)
