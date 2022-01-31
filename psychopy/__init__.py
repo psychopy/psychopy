c221cc94fe456015541508977e4e7664715ce9e2
Jon Peirce <jon.peirce@gmail.com>
2022-01-31 16:46:26 +0000

    PKG: updating versions, copyright etc

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Part of the PsychoPy library
-# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2021 Open Science Tools Ltd.
+# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
 # --------------------------------------------------------------------------
@@ -12,11 +12,11 @@
 import os
 import sys
 
-__version__ = '2022.1.0rc10'
+__version__ = '2022.1.0rc12'
 __license__ = 'GNU GPLv3 (or more recent equivalent)'
-__author__ = 'Jonathan Peirce'
-__author_email__ = 'jon.peirce@gmail.com'
-__maintainer_email__ = 'jon.peirce@gmail.com'
+__author__ = 'Open Science Tools Ltd'
+__author_email__ = 'support@opensciencetools.org'
+__maintainer_email__ = 'support@opensciencetools.org'
 __url__ = 'https://www.psychopy.org/'
 __download_url__ = 'https://github.com/psychopy/psychopy/releases/'
 __git_sha__ = 'n/a'
