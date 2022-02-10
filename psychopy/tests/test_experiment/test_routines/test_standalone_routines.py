641777c4b2ed4201fa447960899fa93ab7dad1e5
Todd <todd.e.parsons@googlemail.com>
2022-02-10 16:09:28 +0000

    TEST: Old params used test no longer needed

--- psychopy/tests/test_experiment/test_routines/test_standalone_routines.py
+++ psychopy/tests/test_experiment/test_routines/test_standalone_routines.py
@@ -34,33 +34,3 @@ class TestStandaloneRoutines:
             for name, routine in exp.routines.copy().items():
                 exp.flow.removeComponent(routine)
                 del exp.routines[name]
-
-    def test_params_used(self):
-        # Change eyetracking settings
-        self.expPy.settings.params['eyetracker'].val = "MouseGaze"
-        # Test both python and JS
-        for target, exp in {"PsychoPy": self.expPy, "PsychoJS": self.expJS}.items():
-            for rtName, routine in self.routines.items():
-                # Skip if not valid for this (or any) target
-                if target not in routine.targets:
-                    continue
-                # Make routine
-                rt = routine(exp)
-                rt = exp.addStandaloneRoutine(rtName, rt)
-                exp.flow.addRoutine(rt, 0)
-                # Compile script
-                script = exp.writeScript(target=target)
-                # Check that the string value of each param is present in the script
-                experiment.utils.scriptTarget = target
-                # Iterate through every param
-                for routine in exp.flow:
-                    for name, param in experiment.getInitVals(routine.params, target).items():
-                        # Conditions to skip...
-                        if not param.direct:
-                            # Marked as not direct
-                            continue
-                        if any(name in depend['param'] for depend in routine.depends):
-                            # Dependent on another param
-                            continue
-                        # Check that param is used
-                        assert str(param) in script, f"{target}.{type(routine).__name__}.{name}: <psychopy.experiment.params.Param: val={param.val}, valType={param.valType}>"
