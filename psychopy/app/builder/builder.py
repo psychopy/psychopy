c769ee093a0429464d1ae141975ea04617aed16f
Matthew D. Cutone <mcutone@opensciencetools.org>
2022-02-23 14:25:58 -0500

    NF/RF: `BaseAuiFrame` class (#4651)
    * RF: Make `BuilderFrame` and `CoderFrame` subclasses of `BaseAuiFrame`
    
    * RF: Remove redundant bindings for `ON_CLOSE`
    
    * DOCS: Fixed duplicate word.
    
    * DOCS: Fixed comment
    
    * DOCS: Add docstrings.
    
    * RF: Changed `id_` -> `id`
    
    * RF: Changed `id_` -> `id` in `BuilderFrame`
    
    * NF: Added title setting methods.
    
    * NF: Consistent title formats across application main windows.
    
    Co-authored-by: mdcutone

--- psychopy/app/builder/builder.py
+++ psychopy/app/builder/builder.py
@@ -48,6 +48,7 @@ from ... import experiment, prefs
 from .. import dialogs
 from ..themes import ThemeMixin
 from ..themes._themes import PsychopyDockArt, PsychopyTabArt, ThemeSwitcher
+from ..ui import BaseAuiFrame
 from psychopy import logging, data
 from psychopy.tools.filetools import mergeFolder
 from .dialogs import (DlgComponentProperties, DlgExperimentProperties,
@@ -117,7 +118,7 @@ class TemplateManager(dict):
                     self[categName][routineName] = copy.copy(thisExp.routines[routineName])
 
 
-class BuilderFrame(wx.Frame, ThemeMixin):
+class BuilderFrame(BaseAuiFrame, ThemeMixin):
     """Defines construction of the Psychopy Builder Frame"""
 
     routineTemplates = TemplateManager()
@@ -147,6 +148,9 @@ class BuilderFrame(wx.Frame, ThemeMixin):
         self.readmeFrame = None
         self.generateScript = generateScript
 
+        # default window title
+        self.winTitle = 'PsychoPy Builder (v{})'.format(self.app.version)
+
         if fileName in self.appData['frames']:
             self.frameData = self.appData['frames'][fileName]
         else:  # work out a new frame size/location
@@ -168,14 +172,13 @@ class BuilderFrame(wx.Frame, ThemeMixin):
             self.frameData['winX'], self.frameData['winY'] = (0, 0)
         if self.frameData['winY'] < 20:
             self.frameData['winY'] = 20
-        wx.Frame.__init__(self, parent=parent, id=id, title=title,
-                          pos=(int(self.frameData['winX']), int(
-                              self.frameData['winY'])),
-                          size=(int(self.frameData['winW']), int(
-                              self.frameData['winH'])),
-                          style=style)
-        self.Bind(wx.EVT_CLOSE, self.closeFrame)
-        #self.panel = wx.Panel(self)
+
+        BaseAuiFrame.__init__(self, parent=parent, id=id, title=title,
+                              pos=(int(self.frameData['winX']),
+                                   int(self.frameData['winY'])),
+                              size=(int(self.frameData['winW']),
+                                    int(self.frameData['winH'])),
+                              style=style)
 
         # detect retina displays (then don't use double-buffering)
         self.isRetina = \
@@ -214,9 +217,7 @@ class BuilderFrame(wx.Frame, ThemeMixin):
         self.updateReadme()  # check/create frame as needed
 
         # control the panes using aui manager
-        self._mgr = aui.AuiManager(
-            self,
-            aui.AUI_MGR_DEFAULT | aui.AUI_MGR_RECTANGLE_HINT)
+        self._mgr = self.getAuiManager()
 
         #self._mgr.SetArtProvider(PsychopyDockArt())
         #self._art = self._mgr.GetArtProvider()
@@ -1015,8 +1016,7 @@ class BuilderFrame(wx.Frame, ThemeMixin):
         """
         if newTitle is None:
             shortName = os.path.split(self.filename)[-1]
-            newTitle = '%s - PsychoPy Builder (v%s)' % (shortName, self.app.version)
-        self.SetTitle(newTitle)
+            self.setTitle(title=self.winTitle, document=shortName)
 
     def setIsModified(self, newVal=None):
         """Sets current modified status and updates save icon accordingly.
