bb6eb4e4996b084caa5b2897da06c341fa8f31db
Todd <todd.e.parsons@googlemail.com>
2022-04-11 11:09:46 +0100

    FF: Adjust size values to account for retina screens

--- psychopy/app/themes/icons.py
+++ psychopy/app/themes/icons.py
@@ -57,6 +57,10 @@ class BaseIcon:
             # If height is not an integer, try to make it one
             height = int(height)
 
+        # Adjust for retina
+        if retStr:
+            width = round(width * 2)
+            height = round(height * 2)
         # Store value
         self._size = (width, height)
         # Clear bitmap cache
