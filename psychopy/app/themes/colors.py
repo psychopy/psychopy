21f287c4b25e1445abe67db740a2f4b18feb7360
Todd <todd.e.parsons@googlemail.com>
2022-04-12 11:33:54 +0100

    EGG: Easter egg app colors mode which turns PsychoPy pink

--- psychopy/app/themes/colors.py
+++ psychopy/app/themes/colors.py
@@ -167,6 +167,30 @@ class AppColors(dict):
         "fl_flowline_fg": scheme['black'],
     }
 
+    pink = {
+        "text": scheme['black'],
+        "frame_bg": scheme['red'] + 4,
+        "docker_bg": scheme['red'] + 3,
+        "docker_fg": scheme['white'],
+        "panel_bg": scheme['red'] + 5,
+        "tab_bg": scheme['red'] + 7,
+        "bmpbutton_bg_hover": scheme['offwhite'] - 1,
+        "bmpbutton_fg_hover": scheme['black'],
+        "txtbutton_bg_hover": scheme['red'],
+        "txtbutton_fg_hover": scheme['offwhite'],
+        "rt_timegrid": scheme['grey'],
+        "rt_comp": scheme['blue'],
+        "rt_comp_force": scheme['orange'],
+        "rt_comp_disabled": scheme['offwhite'] - 2,
+        "rt_static": scheme['red'] * 75,
+        "rt_static_disabled": scheme['grey'] * 75,
+        "fl_routine_fg": scheme['offwhite'] + 1,
+        "fl_routine_bg_slip": scheme['blue'],
+        "fl_routine_bg_nonslip": scheme['green'],
+        "fl_flowline_bg": scheme['grey'],
+        "fl_flowline_fg": scheme['white'] + 1,
+    }
+
     def __getitem__(self, item):
         # When getting an attribute of this object, return the key from the theme-appropriate dict
         return getattr(self, theme.app)[item]
