10731412721ffe36dace11d96a5fa0cf0874f28c
David Hildebrand <david@hildebrand.name>
2022-05-10 12:23:41 -0400

    completing integration of CS100A

--- psychopy/hardware/__init__.py
+++ psychopy/hardware/__init__.py
@@ -73,7 +73,7 @@ def getAllPhotometers():
     from . import minolta, pr, gammasci
     from . import crs
 
-    photometers = [pr.PR650, pr.PR655, minolta.LS100, gammasci.S470]
+    photometers = [pr.PR650, pr.PR655, minolta.CS100A, minolta.LS100, gammasci.S470]
     if hasattr(crs, "ColorCAL"):
         photometers.append(crs.ColorCAL)
 
@@ -115,7 +115,7 @@ def findPhotometer(ports=None, device=None):
             then PsychoPy will sweep COM0-10 on win32 and search known
             likely port names on macOS and Linux.
 
-        device : string giving expected device (e.g. 'PR650', 'PR655',
+        device : string giving expected device (e.g. 'PR650', 'PR655', 'CS100A',
             'LS100', 'LS110', 'S470'). If this is not given then an attempt will be made
             to find a device of any type, but this often fails
 
