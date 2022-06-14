e3cbb387bd121428e5a33a9937798c016cb72921
Todd OST <todd.e.parsons@gmail.com>
2022-06-14 10:27:46 +0100

    BF: Show asterisk on Code component if EITHER code has contents

--- psychopy/app/builder/dialogs/dlgsCode.py
+++ psychopy/app/builder/dialogs/dlgsCode.py
@@ -400,7 +400,10 @@ class DlgCodeComponentProperties(wx.Dialog):
             # Add a visual indicator when tab contains code
             emptyCodeComp = CodeComponent('', '') # Spawn empty code component
             # If code tab is not empty and not the same as in empty code component, add an asterisk to tab name
-            if (self.params.get(pyName).val or self.params.get(jsName).val) and not (self.params.get(pyName).val == emptyCodeComp.params.get(pyName).val or self.params.get(jsName).val == emptyCodeComp.params.get(jsName).val):
+            hasContents = self.params.get(pyName).val or self.params.get(jsName).val
+            pyEmpty = self.params.get(pyName).val != emptyCodeComp.params.get(pyName).val
+            jsEmpty = self.params.get(jsName).val == emptyCodeComp.params.get(jsName).val
+            if hasContents and not (pyEmpty and jsEmpty):
                 tabLabel += ' *'
             self.codeNotebook.AddPage(panel, tabLabel)
 
