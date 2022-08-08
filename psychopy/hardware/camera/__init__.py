7d1f2f905fbfef1d69141cad98ef9a64e51e3054
mdcutone <matthew.cutone@gmail.com>
2022-08-08 13:23:21 -0400

    FF: Use the correct keyword `logger` for `write_videofile`

--- psychopy/hardware/camera/__init__.py
+++ psychopy/hardware/camera/__init__.py
@@ -2387,7 +2387,7 @@ def renderVideo(outputFile, videoFile, audioFile=None):
         videoClip.audio = CompositeAudioClip([audioClip])
 
     # transcode with the format the user wants
-    videoClip.write_videofile(outputFile, verbose=False, logging=None)
+    videoClip.write_videofile(outputFile, verbose=False, logger=None)
 
     return os.path.getsize(outputFile)
 
