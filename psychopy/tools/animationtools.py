9efd10055f98f7d5fd5b3a8438bc974a4caea3c1
Todd Parsons <todd.e.parsons@googlemail.com>
2022-08-23 13:55:51 +0100

    ENH: Add sinusoidalMovement function

--- psychopy/tools/animationtools.py
+++ psychopy/tools/animationtools.py
@@ -29,3 +29,21 @@ def sinusoidalGrowth(startSize, apexSize, duration, time):
     adj = (adj + 1) / 2
     # Adjust
     return startSize + (delta * adj)
+
+
+def sinusoidalMovement(startPos, apexPos, duration, time):
+    """
+    Move a stimulus over time by a sinusoidal function.
+
+    startPos : layout.Position
+        Position of the stimulus at the start of the animation
+    apexPos : layout.Position
+        Position of the stimulus at the apex of the animation
+    duration : int, float
+        How long (in seconds) should the animation take to go from its start to its apex?
+    time : float
+        How long (in seconds) has passed since the animation started?
+    """
+
+    # Position and Size are both Vectors, so growth and movement are the same calculation
+    return sinusoidalGrowth(startPos, apexPos, duration, time)
