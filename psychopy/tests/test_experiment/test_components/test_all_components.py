6f9b5f2cdfd294fc323f3ffd0507eb72f41b2906
Todd Parsons <todd.e.parsons@googlemail.com>
2022-09-28 13:06:47 +0100

    TEST: Add tests for components/stimuli handling depth

--- psychopy/tests/test_experiment/test_components/test_all_components.py
+++ psychopy/tests/test_experiment/test_components/test_all_components.py
@@ -1,3 +1,4 @@
+from psychopy.experiment.exports import IndentingBuffer
 from . import _TestBaseComponentsMixin, _TestDisabledMixin
 from psychopy import experiment
 import inspect
@@ -24,3 +25,38 @@ def test_all_components():
         for attr, meth in _TestDisabledMixin.__dict__.items():
             if inspect.ismethod(meth):
                 meth(tester)
+
+def test_all_have_depth():
+    # Define components which shouldn't have depth
+    exceptions = ("PanoramaComponent",)
+    # Create experiment
+    exp = experiment.Experiment()
+    rt = experiment.routines.Routine(exp=exp, name="testRoutine")
+    exp.addRoutine("testRoutine", rt)
+    exp.flow.addRoutine(rt, 0)
+    # Add one of each component to the routine
+    for compName, compClass in experiment.getAllComponents().items():
+        # Settings components don't count so don't include one at all
+        if compName in ("SettingsComponent",):
+            continue
+        comp = compClass(exp=exp, parentName="testRoutine", name=f"test{compClass.__name__}")
+        rt.addComponent(comp)
+    # For each component...
+    for comp in rt:
+        compName = type(comp).__name__
+        # This won't be relevant for non-visual stimuli
+        if compName in exceptions or not isinstance(comp, experiment.components.BaseVisualComponent):
+            continue
+
+        # Crate buffer to get component code
+        buff = IndentingBuffer(target="PsychoPy")
+        # Write init code
+        comp.writeInitCode(buff)
+        script = buff.getvalue()
+        # Unless excepted, check that depth is in the output
+        assert "depth=" in script.replace(" ", ""), (
+            f"Could not find any reference to depth in init code for {compName}:\n"
+            f"{script}\n"
+            f"Any component drawn to the screen should be given a `depth` on init. If this component is a special "
+            f"case, you can mark it as exempt by adding it to the `exceptions` variable in this test.\n"
+        )
