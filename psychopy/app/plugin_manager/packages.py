cefca564926aece953f90cc688e8e51eef0baa97
mdcutone <matthew.cutone@gmail.com>
2022-10-24 13:45:07 -0400

    RF: Move wx specific stuff into `psychopy.app.plugins` from `psychopy.plugins`

--- psychopy/app/plugin_manager/packages.py
+++ psychopy/app/plugin_manager/packages.py
@@ -2,10 +2,70 @@ import wx
 import sys
 import subprocess as sp
 
-from psychopy.plugins import InstallErrorDlg
 from psychopy.app import utils
 from psychopy.app.themes import handlers, icons
 from psychopy.localization import _translate
+
+
+
+
+class InstallErrorDlg(wx.Dialog, handlers.ThemeMixin):
+    def __init__(self, cmd="", stdout="", stderr="", mode="plugin"):
+        from psychopy.app.themes import fonts
+        # Capitalise mode string
+        mode = mode.title()
+        # Initialise
+        wx.Dialog.__init__(
+            self, None,
+            size=(480, 620),
+            title=mode + _translate(" install error"),
+            style=wx.RESIZE_BORDER | wx.CLOSE_BOX | wx.CAPTION
+        )
+        # Setup sizer
+        self.border = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.border)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.border.Add(self.sizer, proportion=1, border=6, flag=wx.ALL | wx.EXPAND)
+        # Create title sizer
+        self.title = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer.Add(self.title, border=6, flag=wx.ALL | wx.EXPAND)
+        # Create icon
+        self.icon = wx.StaticBitmap(
+            self, size=(32, 32),
+            bitmap=icons.ButtonIcon(stem="stop", size=32).bitmap
+        )
+        self.title.Add(self.icon, border=6, flag=wx.ALL | wx.EXPAND)
+        # Create title
+        self.titleLbl = wx.StaticText(self, label=mode + _translate(" could not be installed."))
+        self.titleLbl.SetFont(fonts.appTheme['h3'].obj)
+        self.title.Add(self.titleLbl, proportion=1, border=6, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
+        # Show what we tried
+        self.inLbl = wx.StaticText(self, label=_translate("We tried:"))
+        self.sizer.Add(self.inLbl, border=6, flag=wx.ALL | wx.EXPAND)
+        self.inCtrl = wx.TextCtrl(self, value=cmd, style=wx.TE_READONLY)
+        self.inCtrl.SetBackgroundColour("white")
+        self.inCtrl.SetFont(fonts.appTheme['code'].obj)
+        self.sizer.Add(self.inCtrl, border=6, flag=wx.ALL | wx.EXPAND)
+        # Show what we got
+        self.outLbl = wx.StaticText(self, label=_translate("We got:"))
+        self.sizer.Add(self.outLbl, border=6, flag=wx.ALL | wx.EXPAND)
+        self.outCtrl = wx.TextCtrl(self, value=f"{stdout}\n{stderr}",
+                                   size=(-1, 620), style=wx.TE_READONLY | wx.TE_MULTILINE)
+        self.outCtrl.SetFont(fonts.appTheme['code'].obj)
+        self.sizer.Add(self.outCtrl, proportion=1, border=6, flag=wx.ALL | wx.EXPAND)
+
+        # Make buttons
+        self.btns = self.CreateStdDialogButtonSizer(flags=wx.OK)
+        self.border.Add(self.btns, border=6, flag=wx.ALIGN_RIGHT | wx.ALL)
+
+        self.Layout()
+        self._applyAppTheme()
+
+    def ShowModal(self):
+        # Make error noise
+        wx.Bell()
+        # Show as normal
+        wx.Dialog.ShowModal(self)
 
 
 class PackageManagerPanel(wx.Panel, handlers.ThemeMixin):
@@ -375,6 +435,21 @@ class PackageDetailsPanel(wx.Panel, handlers.ThemeMixin):
         # todo: Required by...
 
         self.package = None
+
+        # Cache package information where possible to improve responsiveness of
+        # the UI.
+        self._packageInfoCache = {}
+        self._generatePackageInfoCache()
+
+    def _generatePackageInfoCache(self):
+        """Generate package info cache.
+
+        This iterates over all installed packages and obtains information about
+        them. The data is cached for later use instead of obtained when the user
+        clicks the item.
+
+        """
+
 
     @property
     def package(self):
