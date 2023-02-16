845f85c312c143b810701378291b313114e72fed
Todd Parsons <todd.e.parsons@googlemail.com>
2023-02-16 15:54:43 +0000

    ENH: Add PsychoJS as target for Progress bar

--- psychopy/experiment/components/progress/__init__.py
+++ psychopy/experiment/components/progress/__init__.py
@@ -10,7 +10,7 @@ class ProgressComponent(BaseVisualComponent):
 
     """
     categories = ['Stimuli']
-    targets = ['PsychoPy']
+    targets = ['PsychoPy', 'PsychoJS']
     iconFile = Path(__file__).parent / 'progress.png'
     tooltip = _translate('Progress: Present a progress bar, with values ranging from 0 to 1.')
     beta = True
