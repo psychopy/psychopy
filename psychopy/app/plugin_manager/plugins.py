fe02dab2582efc0531430a140fb5c95aa6731a4a
Todd Parsons <todd.e.parsons@googlemail.com>
2023-03-23 12:15:36 +0000

    BF: Handle when plugins icons have no alpha channel

--- psychopy/app/plugin_manager/plugins.py
+++ psychopy/app/plugin_manager/plugins.py
@@ -787,11 +787,12 @@ class PluginDetailsPanel(wx.Panel, handlers.ThemeMixin):
         if isinstance(icon, pil.Image):
             # Resize to fit ctrl
             icon = icon.resize(size=self.iconSize)
-            # Supply an alpha channel if there is one
-            if "A" in icon.getbands():
-                alpha = icon.tobytes("raw", "A")
-            else:
-                alpha = None
+            # Supply an alpha channel
+            if "A" not in icon.getbands():
+                # Make sure there's an alpha channel to supply
+                alpha = pil.new("L", icon.size, 255)
+                icon.putalpha(alpha)
+            alpha = icon.tobytes("raw", "A")
             icon = wx.Bitmap.FromBufferAndAlpha(
                 width=icon.size[0],
                 height=icon.size[1],
