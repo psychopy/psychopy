89a3d9edcba2fa934500e5ef85bf6eb80937e470
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-06-20 17:25:50 -0400

    RF: Stubs for gazepoint and pupil labs eyetrackers

--- psychopy/iohub/devices/eyetracker/hw/pupil_labs/pupil_core/constants.py
+++ psychopy/iohub/devices/eyetracker/hw/pupil_labs/pupil_core/constants.py
@@ -3,5 +3,17 @@
 # Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
 
-EYE_ID_RIGHT = 0
-EYE_ID_LEFT = 1
+import psychopy.logging as logging
+
+try:
+    from psychopy_eyetracker_pupil_labs.pupil_labs.pupil_core.constants import (
+        EYE_ID_RIGHT, 
+        EYE_ID_LEFT)
+except (ModuleNotFoundError, ImportError, NameError):
+    logging.error(
+        "Pupil Labs eyetracker support requires the package "
+        "'psychopy-eyetracker-pupil-labs' to be installed. Please install this "
+        "package and restart the session to enable support.")
+
+if __name__ == "__main__":
+    pass
