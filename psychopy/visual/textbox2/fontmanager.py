5ab19c004c7bc19ee03def9754b55f1ad3be7da4
Todd Parsons <todd.e.parsons@googlemail.com>
2023-06-23 08:58:25 +0100

    ENH: Add Open Sans as a final fallback in case none of the default fonts are installed

--- psychopy/visual/textbox2/fontmanager.py
+++ psychopy/visual/textbox2/fontmanager.py
@@ -745,7 +745,7 @@ class FontManager():
 
     def getDefaultSansFont(self):
         """Load and return the FontInfo for the first found default font"""
-        for name in ['Verdana', 'DejaVu Sans', 'Bitstream Vera Sans', 'Tahoma']:
+        for name in ['Verdana', 'DejaVu Sans', 'Bitstream Vera Sans', 'Tahoma', 'Open Sans']:
             these = self.getFontsMatching(name, fallback=False)
             if not these:
                 continue
