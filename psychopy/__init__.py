d96f615736d8d36ab9f442225e85e951724ed83b
Jon Peirce <jon.peirce@gmail.com>
2023-06-26 15:24:23 +0100

    PKG: version 2023.2.0rc1

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-__version__ = '2023.2.0dev6s'
+__version__ = '2023.2.0rc1'
 __license__ = 'GPL v3'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
