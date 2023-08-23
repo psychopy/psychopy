a6c2cf3c3c92c6380e3545099a3b3c078d55b852
Todd Parsons <todd.e.parsons@googlemail.com>
2023-08-23 10:12:27 +0100

    Revert "BF: Evaluate variable names in conditions file safely"
    This reverts commit 3817458e5c35e987875f62f5d3a6001b9564ac14.

--- psychopy/data/utils.py
+++ psychopy/data/utils.py
@@ -18,7 +18,6 @@ from pkg_resources import parse_version
 
 from psychopy import logging, exceptions
 from psychopy.tools.filetools import pathToString
-from psychopy.tools.environmenttools import getFromNames
 from psychopy.localization import _translate
 
 try:
@@ -430,8 +429,6 @@ def importConditions(fileName, returnFieldNames=False, selection=""):
                     val = val.split(",")
                     # strip spaces
                     val = [subval.strip() for subval in val]
-                    # evaluate variable names
-                    val = getFromNames(val, namespace=globals())
 
                 # if it has any line breaks correct them
                 if isinstance(val, str):
