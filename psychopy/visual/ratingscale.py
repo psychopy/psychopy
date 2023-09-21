ff8c3356313ff1f0b342be0229127206419dd1d1
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2023-09-21 11:10:56 +0200

    Full path for inheritance

--- psychopy/visual/ratingscale.py
+++ psychopy/visual/ratingscale.py
@@ -23,6 +23,8 @@ from psychopy.constants import FINISHED, STARTED, NOT_STARTED
 
 class RatingScale(MinimalStim):
     """A class for obtaining ratings, e.g., on a 1-to-7 or categorical scale.
+    This is a lazy-imported class, therefore import using full path 
+    `psychopy.visual.ratingscale.RatingScale` when inheriting from it.
 
     A RatingScale instance is a re-usable visual object having a ``draw()``
     method, with customizable appearance and response options. ``draw()``
