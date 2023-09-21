e2ff49a454869d43f71cb660a74a979b95186769
TEParsons <todd.e.parsons@gmail.com>
2023-09-21 17:29:48 +0100

    RF: Treat each node managed by a serial device as an object

--- psychopy/hardware/bbtk/tpad.py
+++ psychopy/hardware/bbtk/tpad.py
@@ -1,80 +1,123 @@
-from .. import serialdevice as sd
+from .. import serialdevice as sd, photodiode
 import serial
 import re
 
 from ... import layout
 
+# possible values for self.channel
+channelCodes = {
+    'A': "Buttons",
+    'C': "Optos",
+    'M': "Voice key",
+    'T': "TTL in",
+}
+# possible values for self.state
+stateCodes = {
+    'P': "Pressed/On",
+    'R': "Released/Off",
+}
+# possible values for self.button
+buttonCodes = {
+    '1': "Button 1",
+    '2': "Button 2",
+    '3': "Button 2",
+    '4': "Button 2",
+    '5': "Button 2",
+    '6': "Button 2",
+    '7': "Button 2",
+    '8': "Button 2",
+    '9': "Button 2",
+    '0': "Button 2",
+    '[': "Opto 1",
+    ']': "Opto 2",
+}
 
-class TPadResponse:
-    # possible values for self.channel
-    channels = {
-        'A': "Buttons",
-        'C': "Optos",
-        'M': "Voice key",
-        'T': "TTL in",
-    }
-    # possible values for self.state
-    states = {
-        'P': "Pressed/On",
-        'R': "Released/Off",
-    }
-    # possible values for self.button
-    buttons = {
-        '1': "Button 1",
-        '2': "Button 2",
-        '3': "Button 2",
-        '4': "Button 2",
-        '5': "Button 2",
-        '6': "Button 2",
-        '7': "Button 2",
-        '8': "Button 2",
-        '9': "Button 2",
-        '0': "Button 2",
-        '[': "Opto 1",
-        ']': "Opto 2",
-    }
-    # define format which raw string input must fit
-    fmt = (
-        r"[{channels}] [{states}] [{buttons}] \d*"
-    ).format(
-        channels="".join(re.escape(key) for key in channels),
-        states="".join(re.escape(key) for key in states),
-        buttons="".join(re.escape(key) for key in buttons)
-    )
+# define format for messages
+messageFormat = (
+    r"([{channels}]) ([{states}]) ([{buttons}]) (\d*)"
+).format(
+    channels="".join(re.escape(key) for key in channelCodes),
+    states="".join(re.escape(key) for key in stateCodes),
+    buttons="".join(re.escape(key) for key in buttonCodes)
+)
 
-    def __init__(self, raw):
-        self.raw = raw
-        self.channel, self.state, self.button, self.time = raw.split(" ")
 
-    def __repr__(self):
-        # string value for channel
-        channel = self.channel
-        if self.channel in self.channels:
-            channel += f" ({self.channels[self.channel]})"
-        # string value for state
-        state = self.state
-        if self.state in self.states:
-            state += f" ({self.states[self.state]})"
-        # string value for button
-        button = self.button
-        if self.button in self.buttons:
-            button += f" ({self.buttons[self.button]})"
+def splitTPadMessage(message):
+    return re.match(messageFormat, message).groups()
 
-        return (
-            f"<TPadResponse: channel={channel}, state={state}, button={button}>"
+
+class TPadPhotodiode(photodiode.BasePhotodiode):
+    def __init__(self, port, number):
+        # initialise base class
+        photodiode.BasePhotodiode.__init__(self, port)
+        # store number
+        self.number = number
+
+    def setThreshold(self, threshold):
+        self.device.setMode(0)
+        self.device.sendMessage(f"AAO{self.number} {threshold}")
+
+    def getState(self):
+        # dispatch messages from device
+        self.device.dispatchMessages()
+        # return last known state of this diode
+        if len(self.messages):
+            return self.messages[-1].state
+
+    def parseMessage(self, message):
+        # if given a string, split according to regex
+        if isinstance(message, str):
+            message = splitTPadMessage(message)
+        # split into variables
+        # assert isinstance(message, (tuple, list)) and len(message) == 4
+        channel, state, button, time = message
+        # convert state to bool
+        if state == "P":
+            state = True
+        elif state == "R":
+            state = False
+        # # validate
+        # assert channel == "C", (
+        #     "TPadPhotometer {} received non-photometer message: {}"
+        # ).format(self.number, message)
+        # assert button == str(self.number), (
+        #     "TPadPhotometer {} received message intended for photometer {}: {}"
+        # ).format(self.number, button, message)
+        # create PhotodiodeResponse object
+        resp = photodiode.PhotodiodeResponse(
+            int(time), state, threshold=self.getThreshold()
         )
 
-    def isPress(self):
-        return self.state == "P"
+        return resp
 
-    def isRelease(self):
-        return self.state == "R"
+    def findPhotodiode(self, win):
+        # set mode to 3
+        self.device.setMode(3)
+        self.device.pause()
+        # continue as normal
+        photodiode.BasePhotodiode.findPhotodiode(self, win)
+
+
+class TPadButton:
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class TPadVoicekey:
+    def __init__(self, *args, **kwargs):
+        pass
 
 
 class TPad(sd.SerialDevice):
     def __init__(self, port=None):
         # initialise as a SerialDevice
         sd.SerialDevice.__init__(self, port=port, baudrate=115200)
+        # dict of responses by timestamp
+        self.messages = {}
+        # inputs
+        self.photodiodes = {i+1: TPadPhotodiode(port, i+1) for i in range(2)}
+        self.buttons = {i+1: TPadButton(port, i+1) for i in range(10)}
+        self.voicekeys = {i+1: TPadVoicekey(port, i+1) for i in range(1)}
 
     def setMode(self, mode):
         # exit out of whatever mode we're in (effectively set it to 0)
@@ -99,24 +142,33 @@ class TPad(sd.SerialDevice):
 
         return bool(resp)
 
-    @staticmethod
-    def parseLine(line):
-        if re.match(TPadResponse.fmt, line):
-            return TPadResponse(line)
-        else:
-            return line
+    def dispatchMessages(self, timeout=1/30):
+        # get data from box
+        data = sd.SerialDevice.getResponse(self, length=2, timeout=timeout)
+        self.pause()
+        # parse lines
+        for line in data:
+            if re.match(messageFormat, line):
+                # if line fits format, split into attributes
+                parts = channel, state, button, time = splitTPadMessage(line)
+                # integerise time and button
+                button = int(button)
+                time = int(time)
+                # store message
+                self.messages[time] = line
+                # choose object to dispatch to
+                node = None
+                if channel == "A" and button in self.buttons:
+                    node = self.buttons[button]
+                if channel == "C" and button in self.photodiodes:
+                    node = self.photodiodes[button]
+                if channel == "M" and button in self.voicekeys:
+                    node = self.voicekeys[button]
+                # dispatch
+                if node is not None:
+                    message = node.parseMessage(parts)
+                    node.receiveMessage(message)
 
-    def getResponse(self, length=1, timeout=None):
-        # if timeout is None, use 2*min time for device
-        if timeout is None:
-            timeout = 1/60
-        # do usual value getting
-        data = sd.SerialDevice.getResponse(self, length=length, timeout=timeout)
-        # parse response
-        if isinstance(data, str):
-            return self.parseLine(data)
-        else:
-            return [self.parseLine(line) for line in data]
 
     def calibratePhotodiode(self, level=127):
         # set to mode 0
--- psychopy/hardware/photodiode.py
+++ psychopy/hardware/photodiode.py
@@ -1,199 +1,232 @@
+from collections import OrderedDict
 from psychopy import layout
-from .bbtk.tpad import TPad, TPadResponse
-from psychopy.tools.attributetools import attributeSetter
+from psychopy.hardware import serialdevice as sd
 
 
-def findDiode(win, device):
-    """
-    Draws rectangles on the screen and records photodiode responses to recursively find the location of the diode.
+class PhotodiodeResponse:
+    def __init__(self, t, value, threshold=None):
+        self.t = t
+        self.value = value
+        self.threshold = threshold
 
-    Returns
-    -------
-    psychopy.layout.Position
-        Position of the diode on the window. Essentially, the center of the last rectangle which the photodiode
-        was able to detect.
-    psychopy.layout.Size
-        Size of the area of certainty. Essentially, the size of the last (smallest) rectangle which the photodiode
-        was able to detect.
-    """
-    # stash autodraw
-    win.stashAutoDraw()
-    # calibrate photodiode to midgrey
-    device.calibratePhotodiode(127)
-    device.setMode(3)
-    # import visual here - if they're using this function, it's already in the stack
-    from psychopy import visual
-    # black box to cover screen
-    bg = visual.Rect(
-        win,
-        size=(2, 2), pos=(0, 0), units="norm",
-        fillColor="black",
-        autoDraw=False
-    )
-    # add low opacity label
-    label = visual.TextBox2(
-        win,
-        text="Finding photodiode...",
-        fillColor=(0, 0, 0), color=(80, 80, 80), colorSpace="rgb255",
-        pos=(0, 0), size=(2, 2), units="norm",
-        alignment="center",
-        autoDraw=False
-    )
-    # make rect
-    rect = visual.Rect(
-        win,
-        size=(2, 2), pos=(0, 0), anchor="center", units="norm",
-        fillColor="white",
-        autoDraw=False
-    )
+    def __repr__(self):
+        return f"<PhotodiodeResponse: t={self.t}, value={self.value}, threshold={self.threshold}>"
 
-    def scanQuadrants():
+
+class BasePhotodiode:
+    def __init__(self, port):
+        # get serial device from port (if photodiode manages its own device, this needs to be handled by the subclass)
+        self.device = sd.ports[port]
+        # dict in which to store messages by timestamp
+        self.messages = OrderedDict()
+
+    def receiveMessage(self, message):
+        assert isinstance(message, PhotodiodeResponse), (
+            "{ownType}.receiveMessage() can only receive messages of type PhotodiodeResponse, instead received "
+            "{msgType}. Try parsing the message first using {ownType}.parseMessage()"
+        ).format(ownType=type(self).__name__, msgType=type(message).__name__)
+
+        self.messages[message.t] = message
+
+    def findPhotodiode(self, win):
         """
-        Recursively shrink the rectangle around the position of the photodiode until it's too small to detect.
+        Draws rectangles on the screen and records photodiode responses to recursively find the location of the diode.
+
+        Returns
+        -------
+        psychopy.layout.Position
+            Position of the diode on the window. Essentially, the center of the last rectangle which the photodiode
+            was able to detect.
+        psychopy.layout.Size
+            Size of the area of certainty. Essentially, the size of the last (smallest) rectangle which the photodiode
+            was able to detect.
         """
-        # work out width and height of area
-        w, h = rect.size
-        # work out left, right, top and bottom of area
-        r, t = rect.pos + rect.size / 2
-        l, b = rect.pos - rect.size / 2
-
-        # set rect size to half of area size
-        rect.size /= 2
-        # try each corner
-        for x, y in [
-            (l + w / 4, t - h / 4),  # top left
-            (r - w / 4, t - h / 4),  # top right
-            (l + w / 4, b + h / 4),  # bottom left
-            (r - w / 4, b + h / 4),  # bottom right
-        ]:
-            # position rect
-            rect.pos = (x, y)
-            # draw
-            bg.draw()
-            label.draw()
-            rect.draw()
-            win.flip()
-            # poll photodiode
-            device.pause()
-            if device.getDiodeState():
-                # if it detected this rectangle, recur
-                return scanQuadrants()
-        # if none of these have returned, rect is too small to cover the whole photodiode, so return
-        return
-
-    # recursively shrink rect around the photodiode
-    scanQuadrants()
-    # get response again just to clear it
-    device.pause()
-    device.getResponse()
-    # reinstate autodraw
-    win.retrieveAutoDraw()
-
-    return (
-        layout.Size(rect.pos + rect.size / (-2, 2), units="norm", win=win),
-        layout.Size(rect.size * 2, units="norm", win=win)
-    )
-
-
-class PhotodiodeValidator:
-    def __init__(
-            self, win, device: TPad,
-            diodePos=None, diodeSize=None, diodeUnits="norm",
-            autoLog=False):
-        # set autolog
-        self.autoLog = autoLog
-        # store window handle
-        self.win = win
-        # store device handle
-        self.device = device
-        # list to store linked stim handles
-        self.stim = []
-
+        # stash autodraw
+        win.stashAutoDraw()
+        # calibrate photodiode to midgrey
+        self.setThreshold(127)
+        # import visual here - if they're using this function, it's already in the stack
         from psychopy import visual
-        # black rect which is always drawn on win flip
-        self.offRect = visual.Rect(
+        # black box to cover screen
+        bg = visual.Rect(
             win,
+            size=(2, 2), pos=(0, 0), units="norm",
             fillColor="black",
-            depth=1, autoDraw=True,
-            autoLog=False
+            autoDraw=False
         )
-        # white rect which is only drawn when target stim is, and covers black rect
-        self.onRect = visual.Rect(
+        # add low opacity label
+        label = visual.TextBox2(
             win,
+            text="Finding photodiode...",
+            fillColor=(0, 0, 0), color=(80, 80, 80), colorSpace="rgb255",
+            pos=(0, 0), size=(2, 2), units="norm",
+            alignment="center",
+            autoDraw=False
+        )
+        # make rect
+        rect = visual.Rect(
+            win,
+            size=(2, 2), pos=(0, 0), anchor="center", units="norm",
             fillColor="white",
-            depth=0, autoDraw=False,
-            autoLog=False
+            autoDraw=False
         )
 
-        # if no pos or size are given for photodiode, figure it out
-        if diodePos is None or diodeSize is None:
-            _guessPos, _guessSize = findDiode(self.win, self.device)
-            if diodePos is None:
-                diodePos = _guessPos
-            if diodeSize is None:
-                diodeSize = _guessSize
-        # position rects to match photodiode
-        self.diodeUnits = diodeUnits
-        self.diodeSize = diodeSize
-        self.diodePos = diodePos
+        def scanQuadrants():
+            """
+            Recursively shrink the rectangle around the position of the photodiode until it's too small to detect.
+            """
+            # work out width and height of area
+            w, h = rect.size
+            # work out left, right, top and bottom of area
+            r, t = rect.pos + rect.size / 2
+            l, b = rect.pos - rect.size / 2
 
-    def connectStimulus(self, stim):
-        # store mapping of stimulus to self in window
-        self.win.validators[stim] = self
+            # set rect size to half of area size
+            rect.size /= 2
+            # try each corner
+            for x, y in [
+                (l + w / 4, t - h / 4),  # top left
+                (r - w / 4, t - h / 4),  # top right
+                (l + w / 4, b + h / 4),  # bottom left
+                (r - w / 4, b + h / 4),  # bottom right
+            ]:
+                # position rect
+                rect.pos = (x, y)
+                # draw
+                bg.draw()
+                label.draw()
+                rect.draw()
+                win.flip()
+                # poll photodiode
+                if self.getState():
+                    # if it detected this rectangle, recur
+                    return scanQuadrants()
+            # if none of these have returned, rect is too small to cover the whole photodiode, so return
+            return
 
-    def draw(self):
-        self.onRect.draw()
+        # recursively shrink rect around the photodiode
+        scanQuadrants()
+        # reinstate autodraw
+        win.retrieveAutoDraw()
 
-    def validate(self, expectWhite):
-        isWhite = self.getDiodeState()
-        # was rect white/black as expected?
-        valid = isWhite == expectWhite
-        # do methods for valid or not
-        if valid:
-            self.onValid(isWhite)
-        else:
-            self.onInvalid(isWhite)
-        # return whether expected white matches found
-        return valid
+        return (
+            layout.Size(rect.pos + rect.size / (-2, 2), units="norm", win=win),
+            layout.Size(rect.size * 2, units="norm", win=win)
+        )
 
-    def getDiodeState(self):
-        # start off assuming black
-        isWhite = False
-        # get response
-        resp = self.device.getResponse(length=2)
-        # go through response lines
-        for line in resp:
-            # if we have a TPadResponse, look for photodiode on
-            if isinstance(line, TPadResponse) and line.channel == "C" and line.state == "P":
-                isWhite = True
+    def setThreshold(self, threshold):
+        raise NotImplementedError()
 
-        return isWhite
+    def getThreshold(self):
+        if hasattr(self, "_threshold"):
+            return self._threshold
 
-    @attributeSetter
-    def diodeUnits(self, value):
-        self.onRect.units = value
-        self.offRect.units = value
+    def getState(self):
+        raise NotImplementedError()
 
-    @attributeSetter
-    def diodePos(self, value):
-        self.onRect.pos = value
-        self.offRect.pos = value
-
-    @attributeSetter
-    def diodeSize(self, value):
-        self.onRect.size = value
-        self.offRect.size = value
-
-    @staticmethod
-    def onValid(isWhite):
-        pass
-
-    @staticmethod
-    def onInvalid(isWhite):
-        msg = "Stimulus validation failed. "
-        if isWhite:
-            msg += "Stimulus drawn when not expected."
-        else:
-            msg += "Stimulus not drawn when expected."
-
-        raise AssertionError(msg)
+    def parseMessage(self, message):
+        raise NotImplementedError()
+#
+#
+# class PhotodiodeValidator:
+#     def __init__(
+#             self, win, device,
+#             diodePos=None, diodeSize=None, diodeUnits="norm",
+#             autoLog=False):
+#         # set autolog
+#         self.autoLog = autoLog
+#         # store window handle
+#         self.win = win
+#         # store device handle
+#         self.device = device
+#         # list to store linked stim handles
+#         self.stim = []
+#
+#         from psychopy import visual
+#         # black rect which is always drawn on win flip
+#         self.offRect = visual.Rect(
+#             win,
+#             fillColor="black",
+#             depth=1, autoDraw=True,
+#             autoLog=False
+#         )
+#         # white rect which is only drawn when target stim is, and covers black rect
+#         self.onRect = visual.Rect(
+#             win,
+#             fillColor="white",
+#             depth=0, autoDraw=False,
+#             autoLog=False
+#         )
+#
+#         # if no pos or size are given for photodiode, figure it out
+#         if diodePos is None or diodeSize is None:
+#             _guessPos, _guessSize = findDiode(self.win, self.device)
+#             if diodePos is None:
+#                 diodePos = _guessPos
+#             if diodeSize is None:
+#                 diodeSize = _guessSize
+#         # position rects to match photodiode
+#         self.diodeUnits = diodeUnits
+#         self.diodeSize = diodeSize
+#         self.diodePos = diodePos
+#
+#     def connectStimulus(self, stim):
+#         # store mapping of stimulus to self in window
+#         self.win.validators[stim] = self
+#
+#     def draw(self):
+#         self.onRect.draw()
+#
+#     def validate(self, expectWhite):
+#         isWhite = self.getDiodeState()
+#         # was rect white/black as expected?
+#         valid = isWhite == expectWhite
+#         # do methods for valid or not
+#         if valid:
+#             self.onValid(isWhite)
+#         else:
+#             self.onInvalid(isWhite)
+#         # return whether expected white matches found
+#         return valid
+#
+#     def getDiodeState(self):
+#         # start off assuming black
+#         isWhite = False
+#         # get response
+#         resp = self.device.getResponse(length=2)
+#         # go through response lines
+#         for line in resp:
+#             # if we have a TPadResponse, look for photodiode on
+#             if isinstance(line, TPadResponse) and line.channel == "C" and line.state == "P":
+#                 isWhite = True
+#
+#         return isWhite
+#
+#     @attributeSetter
+#     def diodeUnits(self, value):
+#         self.onRect.units = value
+#         self.offRect.units = value
+#
+#     @attributeSetter
+#     def diodePos(self, value):
+#         self.onRect.pos = value
+#         self.offRect.pos = value
+#
+#     @attributeSetter
+#     def diodeSize(self, value):
+#         self.onRect.size = value
+#         self.offRect.size = value
+#
+#     @staticmethod
+#     def onValid(isWhite):
+#         pass
+#
+#     @staticmethod
+#     def onInvalid(isWhite):
+#         msg = "Stimulus validation failed. "
+#         if isWhite:
+#             msg += "Stimulus drawn when not expected."
+#         else:
+#             msg += "Stimulus not drawn when expected."
+#
+#         raise AssertionError(msg)
