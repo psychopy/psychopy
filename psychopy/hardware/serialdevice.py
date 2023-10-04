9e2d0f37513e23e2a57a9a499598fa3c62d5ff7d
TEParsons <todd.e.parsons@gmail.com>
2023-10-04 11:48:08 +0100

    RF: Move _findPossiblePorts out of SerialDevice so it can be called while constructing the global `ports`

--- psychopy/hardware/serialdevice.py
+++ psychopy/hardware/serialdevice.py
@@ -14,12 +14,44 @@ import time
 
 from psychopy import logging
 import serial
-from serial.tools import list_ports
 from psychopy.tools import systemtools as st
 from psychopy.tools.attributetools import AttributeGetSetMixin
 
+def _findPossiblePorts():
+    if sys.platform == 'win32':
+        # get profiles for all serial port devices
+        profiles = st.systemProfilerWindowsOS(classname="Ports")
+        # get COM port for each device
+        final = []
+        for profile in profiles:
+            # find "COM" in profile description
+            desc = profile['Device Description']
+            start = desc.find("COM") + 3
+            end = desc.find(")", start)
+            # skip this profile if there's no reference to a COM port
+            if -1 in (start, end):
+                continue
+            # get COM port number
+            num = desc[start:end]
+            # skip this profile if COM port number doesn't look numeric
+            if not num.isnumeric():
+                continue
+            # store COM port
+            final.append(f"COM{num}")
+    else:
+        # on linux and mac the options are too wide so use serial.tools
+        from serial.tools import list_ports
+        poss = list_ports.comports()
+        # filter out any that report 'n/a' for their hardware
+        final = []
+        for p in poss:
+            if p[2] != 'n/a':
+                final.append(p[0])  # just the port address
+    return final
+
+
 # map out all ports on this device, to be filled as serial devices are initialised
-ports = {port.name: None for port in list_ports.comports()}
+ports = {port: None for port in _findPossiblePorts()}
 
 class SerialDevice(AttributeGetSetMixin):
     """A base class for serial devices, to be sub-classed by specific devices
@@ -122,39 +154,6 @@ class SerialDevice(AttributeGetSetMixin):
             ports[port] = self
         # we aren't in a time-critical period so flush messages
         logging.flush()
-
-    @staticmethod
-    def _findPossiblePorts():
-        if sys.platform == 'win32':
-            # get profiles for all serial port devices
-            profiles = st.systemProfilerWindowsOS(classname="Ports")
-            # get COM port for each device
-            final = []
-            for profile in profiles:
-                # find "COM" in profile description
-                desc = profile['Device Description']
-                start = desc.find("COM") + 3
-                end = desc.find(")", start)
-                # skip this profile if there's no reference to a COM port
-                if -1 in (start, end):
-                    continue
-                # get COM port number
-                num = desc[start:end]
-                # skip this profile if COM port number doesn't look numeric
-                if not num.isnumeric():
-                    continue
-                # store COM port
-                final.append(f"COM{num}")
-        else:
-            # on linux and mac the options are too wide so use serial.tools
-            from serial.tools import list_ports
-            poss = list_ports.comports()
-            # filter out any that report 'n/a' for their hardware
-            final = []
-            for p in poss:
-                if p[2] != 'n/a':
-                    final.append(p[0])  # just the port address
-        return final
 
     def isAwake(self):
         """This should be overridden by the device class
