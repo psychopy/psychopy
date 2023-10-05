7367f4462ca9e139c06b5ab11b1ad7beb7b4ed63
Todd Parsons <todd.e.parsons@googlemail.com>
2023-10-05 18:38:23 +0100

    NF: Python Shelf (needs methods)

---
+++ psychopy/data/shelf.py
@@ -0,0 +1,113 @@
+import json
+import os
+from pathlib import Path
+from psychopy.preferences import prefs
+
+
+class Shelf:
+    scopeAliases = {
+        'designer': ["designer", "d", "des", "user"],
+        'experiment': ["experiment", "e", "exp", "project"],
+        'participant': ["participant", "p", "par", "subject"]
+    }
+
+    def __init__(self, scope="experiment", expPath=None, participant=None):
+        # handle scope aliases
+        scope = self.scopeFromAlias(scope)
+
+        # if given an experiment path, sanitize it
+        if expPath is not None:
+            # convert to Path object
+            expPath = Path(expPath)
+            # if given a file path, use parent dir
+            if not expPath.is_dir():
+                expPath = expPath.parent
+
+        # work out path of scope file from scope and params
+        if scope == "designer":
+            # access shelf from user folder
+            self.path = Path(prefs.paths['userPrefsDir']) / "shelf.json"
+        elif scope in "experiment":
+            # make sure we have the information we need to get scope file
+            assert expPath is not None, (
+                "Cannot access experiment-scope shelf records without reference to experiment's origin path. Please "
+                "supply a value for 'expPath' when creating an experiment-scope Shelf object."
+            )
+            # access shelf from experiment folder
+            self.path = expPath / "shelf.json"
+        elif scope in "participant":
+            # make sure we have the information we need to get scope file
+            assert participant is not None, (
+                "Cannot access participant-scope shelf records without reference to participant ID. Please "
+                "supply a value for 'participant' when creating a participant-scope Shelf object."
+            )
+            # access shelf from a participant shelf file in the user folder
+            self.path = Path(prefs.paths['userPrefsDir']) / "shelf" / f"{participant}.json"
+
+        # open file(s)
+        self.data = ShelfData(self.path)
+
+    @staticmethod
+    def scopeFromAlias(alias):
+        # if alias is present in aliases dict, return corresponding scope
+        for scope in Shelf.scopeAliases:
+            if alias in Shelf.scopeAliases[scope]:
+                return scope
+        # if it isn't aliased, return as is
+        return alias
+
+
+class ShelfData:
+    def __init__(self, path):
+        # make sure path exists
+        if not path.parent.is_dir():
+            os.makedirs(str(path.parent), exist_ok=True)
+        if not path.is_file():
+            path.write_text("{}", encoding="utf-8")
+        # store ref to path
+        self._path = path
+        # make sure file is valid json
+        try:
+            self.read()
+        except json.JSONDecodeError as err:
+            errcls = type(err)
+            raise json.JSONDecodeError((
+                    f"Contents of shelf file '{path}' are not valid JSON syntax. Has the file been edited outside of "
+                    f"PsychoPy? Original error:\n"
+                    f"\t{errcls.__module__}.{errcls.__name__}: {err.msg}"
+                ),
+                doc=err.doc,
+                pos=err.pos
+            )
+
+    def __repr__(self):
+        # get data from file
+        data = self.read()
+
+        return repr(data)
+
+    def read(self):
+        # get data from file
+        with self._path.open("r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        return data
+
+    def __getitem__(self, key):
+        # get data from file
+        data = self.read()
+
+        return data[key]
+
+    def write(self, data):
+        # write data to file
+        with self._path.open("w", encoding="utf-8") as f:
+            json.dump(data, f)
+
+    def __setitem__(self, key, value):
+        # get data from file
+        data = self.read()
+        # set data
+        data[key] = value
+        # write data to file
+        self.write(data)
