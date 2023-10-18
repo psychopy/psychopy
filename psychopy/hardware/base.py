7d891b115ac6c3500a398dd7771b887f13a817b9
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-10-18 13:49:54 -0400

    RF: Strip out `BaseDevice` attrs for now

--- psychopy/hardware/base.py
+++ psychopy/hardware/base.py
@@ -1,32 +1,22 @@
-import inspect
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Base class for hardware device interfaces.
+
+"""
+
+# Part of the PsychoPy library
+# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+__all__ = [
+    'BaseDevice'
+]
 
 
 class BaseDevice:
-    def __new__(cls, *args, **kwargs):
-        from .manager import mgr
-        # put all args in one dict
-        argNames = list(inspect.signature(cls.__init__).parameters)[1:]
-        params = kwargs.copy()
-        for i, arg in args:
-            params[argNames[i]] = arg
-        # check that this device doesn't already exist
-        for device in mgr:
-            # if they're a different class, they're definitely not the same device
-            if not isinstance(device, cls):
-                continue
-            # use own class's comparison method to determine same device
-            if device.isSameDevice(params):
-                # if same device, return it rather than making a new object
-                return device
+    """Base class for device interfaces."""
+    pass
 
-        # if we got this far, it's a new device, continue as normal
-        obj = object.__new__(cls)
-        mgr.registerDevice(obj)
-        return obj
 
-    def isSameDevice(self, params):
-        raise NotImplementedError()
-
-    def close(self):
-        raise NotImplementedError()
-
+if __name__ == "__main__":
+    pass
