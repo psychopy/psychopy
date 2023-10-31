e1f9e6f1dfaee1313fef3606f6019a05738411b2
TEParsons <todd.e.parsons@gmail.com>
2023-10-31 10:43:20 +0000

    TEST: Update DeviceManager tests to match new structure

--- psychopy/tests/test_hardware/test_device_manager.py
+++ psychopy/tests/test_hardware/test_device_manager.py
@@ -6,14 +6,6 @@ class TestDeviceManager:
     def setup_class(cls):
         cls.mgr = manager.getDeviceManager()
 
-    def test_methods_present(self):
-        # check that all device classes have the required methods
-        for deviceType in manager._deviceMethods:
-            for action in ("add", "remove", "get", "getall", "available"):
-                assert action in manager._deviceMethods[deviceType], (
-                    f"Could not find method for action '{action}' for device type '{deviceType}' in DeviceManager"
-                )
-
     @skip_under_vm
     def test_all_devices(self):
         devices = (
@@ -39,6 +31,6 @@ class TestDeviceManager:
         device = self.mgr.getDevice(name)
         assert device == _device
         # check it's in list of registered devices
-        devices = self.mgr.getDevices(deviceType)
+        devices = self.mgr.getInitialisedDevices(deviceType)
         assert name in devices
         assert devices[name] == device
