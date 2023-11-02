6f432cae5887d5ec1f10c6bd628fb9cd51b25f6f
TEParsons <todd.e.parsons@gmail.com>
2023-11-02 15:12:22 +0000

    ENH: Standardise format between ButtonGroup and PhotodiodeGroup

--- psychopy/hardware/button.py
+++ psychopy/hardware/button.py
@@ -1,13 +1,15 @@
 import json
+from psychopy.hardware import base
 
 
 class ButtonResponse:
-    def __init__(self, t, value):
+    def __init__(self, t, channel, value):
         self.t = t
+        self.channel = channel
         self.value = value
 
     def __repr__(self):
-        return f"<ButtonResponse: t={self.t}, value={self.value}>"
+        return f"<ButtonResponse: t={self.t}, channel={self.channel}, value={self.value}>"
 
     def getJSON(self):
         message = {
@@ -15,6 +17,7 @@ class ButtonResponse:
             'class': "PhotodiodeResponse",
             'data': {
                 't': self.t,
+                'channel': self.channel,
                 'value': self.value,
             }
         }
@@ -22,12 +25,14 @@ class ButtonResponse:
         return json.dumps(message)
 
 
-class BaseButton:
-    def __init__(self, parent):
+class BaseButtonGroup(base.BaseDevice):
+    def __init__(self, parent, channels=1):
         # store reference to parent device (usually a button box)
         self.parent = parent
+        # store number of channels
+        self.channels = channels
         # attribute in which to store current state
-        self.state = False
+        self.state = [None] * channels
         # list in which to store messages in chronological order
         self.responses = []
         # list of listener objects
@@ -48,7 +53,7 @@ class BaseButton:
         """
         self.listeners.append(listener)
 
-    def getResponses(self, state=None, clear=True):
+    def getResponses(self, state=None, channel=None, clear=True):
         """
         Get responses which match a given on/off state.
 
@@ -56,6 +61,8 @@ class BaseButton:
         ----------
         state : bool or None
             True to get button "on" responses, False to get button "off" responses, None to get all responses.
+        channel : int
+            Which button to get responses from?
         clear : bool
             Whether or not to remove responses matching `state` after retrieval.
 
@@ -72,12 +79,13 @@ class BaseButton:
         for resp in self.responses.copy():
             # does this message meet the criterion?
             if state is None or resp.value == state:
-                # if clear, remove the response
-                if clear:
-                    i = self.responses.index(resp)
-                    resp = self.responses.pop(i)
-                # append the response to responses array
-                matches.append(resp)
+                if channel is None or resp.channel == channel:
+                    # if clear, remove the response
+                    if clear:
+                        i = self.responses.index(resp)
+                        resp = self.responses.pop(i)
+                    # append the response to responses array
+                    matches.append(resp)
 
         return matches
 
@@ -87,18 +95,18 @@ class BaseButton:
             "{msgType}. Try parsing the message first using {ownType}.parseMessage()"
         ).format(ownType=type(self).__name__, msgType=type(message).__name__)
         # update current state
-        self.state = message.value
+        self.state[message.channel] = message.value
         # add message to responses
         self.responses.append(message)
         # relay message to listener
         for listener in self.listeners:
             listener.receiveMessage(message)
 
-    def getState(self):
+    def getState(self, channel):
         # dispatch messages from device
         self.parent.dispatchMessages()
         # return state after update
-        return self.state
+        return self.state[channel]
 
     def parseMessage(self, message):
         raise NotImplementedError()
