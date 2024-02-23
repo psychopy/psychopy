204e79d76eeca29b58496c67ebf2c513770cb5f4
Jon Peirce <jon.peirce@gmail.com>
2024-02-23 14:53:35 +0000

    PKG: version bump 2024.1.0rc9

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-__version__ = '2024.1.0rc7'
+__version__ = '2024.1.0rc9'
 __license__ = 'GPL v3'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
