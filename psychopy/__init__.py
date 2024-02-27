c794584997a9464751696ec638aedc2b0cbd1204
Jon Peirce <jon.peirce@gmail.com>
2024-02-27 12:32:32 +0000

    PKG: version bump 2024.1.0rc11

--- psychopy/__init__.py
+++ psychopy/__init__.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-__version__ = '2024.1.0rc10'
+__version__ = '2024.1.0rc11'
 __license__ = 'GPL v3'
 __author__ = 'Open Science Tools Ltd'
 __author_email__ = 'support@opensciencetools.org'
@@ -63,3 +63,4 @@ try:
     import readline
 except ImportError:
     pass  # all that will happen is the stderr/stdout might get redirected
+
