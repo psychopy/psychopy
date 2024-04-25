636edf612d158cdcec115ac03490fc5230ff1eac
jonathan <jonathan.hirdes@eyelogic.de>
2024-04-25 12:19:04 +0200

    NF: adds support for EyeLogic eyetrackers

---
+++ psychopy/iohub/devices/eyetracker/hw/eyelogic/eyetracker.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of the PsychoPy library
+# Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+import psychopy.logging as logging
+
+try:
+    from psychopy_eyetracker_eyelogic.eyelogic.eyetracker import EyeTracker
+except (ModuleNotFoundError, ImportError, NameError):
+    logging.error(
+        "The EyeLogic eyetracker requires package 'psychopy-eyetracker-eyelogic' to "
+        "be installed. Please install this package and restart the session to "
+        "enable support.")
+
+if __name__ == "__main__":
+    pass
