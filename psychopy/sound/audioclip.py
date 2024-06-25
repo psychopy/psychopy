6fe2932d8af8ac465eafab47b99122856f29ac0d
Matthew D. Cutone <mcutone@opensciencetools.org>
2024-06-25 11:40:23 -0400

    RF: Better handling of input args

--- psychopy/sound/audioclip.py
+++ psychopy/sound/audioclip.py
@@ -450,6 +450,27 @@ class AudioClip:
         AudioClip
             Audio clip containing the synthesized speech.
 
+        Examples
+        --------
+        Synthesize speech using the default gTTS engine::
+
+            import psychopy.sound as sound
+            voiceClip = sound.AudioClip.synthesizeSpeech(
+                'How are you doing today?')
+
+        Save the synthesized speech to a file for later use::
+
+            voiceClip = sound.AudioClip.synthesizeSpeech(
+                'How are you doing today?', outFile='/path/to/speech.mp3')
+
+        Synthesize speech using the gTTS engine with a specific language, 
+        timeout, and top-level domain::
+
+            voiceClip = sound.AudioClip.synthesizeSpeech(
+                'How are you doing today?', 
+                engine='gtts', 
+                synthConfig={'lang': 'en', 'timeout': 10, 'tld': 'us'})
+
         """
         if engine not in ['gtts']:
             raise ValueError('Unsupported TTS engine specified.')
@@ -465,30 +486,30 @@ class AudioClip:
                 raise ImportError(
                     'The gTTS package is required for speech synthesis.')
 
-            # inform the user that the timeout is infinite
-            timeout = synthConfig.get('timeout', None)
-            if timeout is None:
+            # set defaults for parameters if not specified
+            if 'timeout' not in synthConfig:
+                synthConfig['timeout'] = None
                 logging.warning(
                     'The gTTS speech-to-text engine has been configured with '
                     'an infinite timeout. The application may stall if the '
                     'server is unresponsive. To set a timeout, specify the '
                     '`timeout` key in `synthConfig`.')
-            if 'timeout' in synthConfig: # remove the key so we don't pass it
-                del synthConfig['timeout']
             
-            # check if the language is supported
-            language = synthConfig.get('lang', 'en')
-            if language not in gtts.lang.tts_langs():
-                raise ValueError('Unsupported language code specified.')
-            if 'lang' in synthConfig:
-                del synthConfig['lang']
+            if 'lang' not in synthConfig:
+                synthConfig['lang'] = 'en'
+            else:
+                # check if the value is a valid language code
+                if synthConfig['lang'] not in gtts.lang.tts_langs():
+                    raise ValueError('Unsupported language code specified.')
+
+            if 'tld' not in synthConfig:
+                synthConfig['tld'] = 'us'
+                logging.info(
+                    'Using the US top-level domain (TLD) for the gTTS engine.')
 
             try:
                 handle = gtts.gTTS(
                     text=text, 
-                    tld=synthConfig.get('tld', 'us'), 
-                    lang=language, 
-                    timeout=timeout,
                     **synthConfig)
             except gtts.gTTSError as e:
                 raise AudioSynthesisError(
