6d74f3f2a4b60b5ddf2ee63a8724e9f814b38982
Jonathan Peirce <jon.peirce@gmail.com>
2024-07-09 18:01:15 +0100

    PKG: utils branch formatter no longer needed

--- utils.py
+++
@@ -1,9 +0,0 @@
-
-def _branchFormatter(name):
-    """Receives name of this git branch (from setuptools-git-versioning) and
-    returns the relevant release tag"""
-    # see https://setuptools-git-versioning.readthedocs.io/en/stable/options/branch_formatter.html
-    if name == "release":
-        return "post"  # so we get 2024.2.1post4+4ea4af for post-release
-    else:
-        return "dev"  # so we get 2024.2.2dev4+4ea4af for pre-release
