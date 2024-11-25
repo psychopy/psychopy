10d084e66f1075d858f735bfca7836467d737cd7
Matthew D. Cutone <mcutone@opensciencetools.org>
2024-11-25 15:57:36 -0500

    RF: Changed CLAMP to GL_CLAMP_TO_BORDER

--- psychopy/tools/fontmanager.py
+++ psychopy/tools/fontmanager.py
@@ -298,9 +298,9 @@ class _TextureAtlas:
                       .format(self.name))
         gl.glBindTexture(gl.GL_TEXTURE_2D, self.textureID)
         gl.glTexParameteri(gl.GL_TEXTURE_2D,
-                           gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
+                           gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_BORDER)
         gl.glTexParameteri(gl.GL_TEXTURE_2D,
-                           gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
+                           gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_BORDER)
         gl.glTexParameteri(gl.GL_TEXTURE_2D,
                            gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
         gl.glTexParameteri(gl.GL_TEXTURE_2D,
