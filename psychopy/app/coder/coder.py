b539a584528791387df7c2def2a9eac982664eb4
Matthew D. Cutone <mcutone@opensciencetools.org>
2025-06-18 14:48:04 -0400

    NF: Show message that index is being updated

--- psychopy/app/coder/coder.py
+++ psychopy/app/coder/coder.py
@@ -2856,6 +2856,15 @@ class CoderFrame(BaseAuiFrame, handlers.ThemeMixin):
         # UnitTestFrame.Show()
 
     def openPluginManager(self, evt=None):
+        import psychopy.app.plugin_manager.packageIndex as packageIndex
+        if packageIndex.isIndexing():
+            wx.MessageBox(
+                _translate("The package index is currently being updated. Try again later."),
+                _translate("Package indexing in progress"),
+                style=wx.OK | wx.ICON_INFORMATION
+            )
+            return
+        
         dlg = psychopy.app.plugin_manager.dialog.EnvironmentManagerDlg(self)
         dlg.Show()
         # Do post-close checks
