6e341339be30e77c396abf61f092d58684357edf
Todd OST <todd.e.parsons@gmail.com>
2025-06-23 13:33:46 +0100

    ENH: Allow device params to have categories so e.g. calibration can start off hidden

--- psychopy/app/deviceManager/panel.py
+++ psychopy/app/deviceManager/panel.py
@@ -1,3 +1,4 @@
+from psychopy.app import utils
 from psychopy.app.builder.dialogs.paramCtrls import EVT_PARAM_CHANGED, ParamCtrl
 from psychopy.app.builder.validators import WarningManager
 from psychopy.app.themes import fonts
@@ -25,65 +26,95 @@ class DevicePanel(wx.Panel):
         self.border.Add(
             self.sizer, proportion=1, border=0, flag=wx.EXPAND | wx.ALL
         )
-        # sort params by order
+        # sort params by categ then order
         sortedParams = {}
         for key in device.order:
             if key in device.params:
-                sortedParams[key] = device.params[key]
+                # make sure there's a page for this category
+                categ = device.params[key].categ
+                if categ not in sortedParams:
+                    sortedParams[categ] = {}
+                # add param to categ
+                sortedParams[categ][key] = device.params[key]
         for key in device.params:
-            if key not in sortedParams:
-                sortedParams[key] = device.params[key]
+            # make sure there's a page for this category
+            categ = device.params[key].categ
+            if categ not in sortedParams:
+                sortedParams[categ] = {}
+            # add param to categ
+            if key not in sortedParams[categ]:
+                sortedParams[categ][key] = device.params[key]
         # param ctrls
         self.paramCtrls = {}
-        for name, param in sortedParams.items():
-            # make param ctrl
-            self.paramCtrls[name] = ctrl = ParamCtrl(
-                self,
-                field=name,
-                param=param,
-                element=None,
-                warnings=self.warnings
-            )
-            ctrl.Bind(EVT_PARAM_CHANGED, self.onParamEdit)
-            # make label
-            lbl = wx.StaticText(
-                self, label=param.label
-            )
-            # make sizer for this ctrl
-            if type(ctrl).__name__ in ("BoolCtrl"):
-                sizer = wx.BoxSizer(wx.HORIZONTAL)
-                # add crl
-                sizer.Add(
-                    ctrl, border=6, flag=wx.EXPAND | wx.RIGHT
-                )
-                # add label
-                sizer.Add(
-                    lbl, flag=wx.ALIGN_CENTER
-                )
+        for categ in sortedParams:
+            # if categ if Basic, add to root sizer
+            toggle = None
+            if categ == "Basic":
+                categSizer = self.sizer
             else:
-                sizer = wx.BoxSizer(wx.VERTICAL)
-                # add label
-                sizer.Add(
-                    lbl, border=3, flag=wx.EXPAND | wx.BOTTOM
+                # otherwise, make a show/hider
+                categSizer = wx.BoxSizer(wx.VERTICAL)
+                toggle = utils.ShowHideBtn(
+                    self, target=categSizer, label=categ
                 )
-                sizer.Add(
-                    ctrl, flag=wx.EXPAND
+                self.sizer.Add(
+                    toggle, border=6, flag=wx.EXPAND | wx.ALL
                 )
-                # add ctrl
-            # add sizer to panel
-            self.sizer.Add(
-                sizer, border=6, flag=wx.EXPAND | wx.ALL
-            )
-            # store name param ctrl
-            if name == "deviceLabel":
-                self.nameCtrl = self.paramCtrls[name]
-                # bump up the font size
-                self.nameCtrl.ctrl.SetFont(fonts.AppFont(
-                    pointSize=int(fonts.AppFont.pointSize*1.5),
-                    bold=True
-                ).obj)
-                # hide label
-                lbl.Hide()
+                self.sizer.Add(
+                    categSizer, border=0, flag=wx.EXPAND | wx.ALL
+                )
+            for name, param in sortedParams[categ].items():
+                # make param ctrl
+                self.paramCtrls[name] = ctrl = ParamCtrl(
+                    self,
+                    field=name,
+                    param=param,
+                    element=None,
+                    warnings=self.warnings
+                )
+                ctrl.Bind(EVT_PARAM_CHANGED, self.onParamEdit)
+                # make label
+                lbl = wx.StaticText(
+                    self, label=param.label
+                )
+                # make sizer for this ctrl
+                if type(ctrl).__name__ in ("BoolCtrl"):
+                    sizer = wx.BoxSizer(wx.HORIZONTAL)
+                    # add crl
+                    sizer.Add(
+                        ctrl, border=6, flag=wx.EXPAND | wx.RIGHT
+                    )
+                    # add label
+                    sizer.Add(
+                        lbl, flag=wx.ALIGN_CENTER
+                    )
+                else:
+                    sizer = wx.BoxSizer(wx.VERTICAL)
+                    # add label
+                    sizer.Add(
+                        lbl, border=3, flag=wx.EXPAND | wx.BOTTOM
+                    )
+                    sizer.Add(
+                        ctrl, flag=wx.EXPAND
+                    )
+                    # add ctrl
+                # add sizer to panel
+                categSizer.Add(
+                    sizer, border=6, flag=wx.EXPAND | wx.ALL
+                )
+                # store name param ctrl
+                if name == "deviceLabel":
+                    self.nameCtrl = self.paramCtrls[name]
+                    # bump up the font size
+                    self.nameCtrl.ctrl.SetFont(fonts.AppFont(
+                        pointSize=int(fonts.AppFont.pointSize*1.5),
+                        bold=True
+                    ).obj)
+                    # hide label
+                    lbl.Hide()
+            # start other categories off hidden
+            if toggle:
+                toggle.setValue(False)
         # profile label
         self.profileLbl = wx.StaticText(self, label="Device information")
         self.sizer.Add(
