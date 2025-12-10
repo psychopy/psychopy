a8162bc2a7c04d8531319a46a57a38c0a6cd21b0
Alex Wade <wade@wadelab.net>
2025-12-10 11:17:30 +0000

    Fix AttributeError: KeyboardDevice missing _buffers attribute for non-PTB backends

--- psychopy/hardware/keyboard.py
+++ psychopy/hardware/keyboard.py
@@ -386,6 +386,12 @@ class KeyboardDevice(BaseResponseDevice, aliases=["keyboard"]):
 
         logging.info('keyboard.Keyboard is using %s backend.' % KeyboardDevice._backend)
 
+        # Initialize _buffers and _devs for all backends to prevent AttributeError
+        if not hasattr(self, '_buffers'):
+            self._buffers = {}
+        if not hasattr(self, '_devs'):
+            self._devs = {}
+
         # array in which to store ongoing presses
         self._keysStillDown = deque()
         # set whether or not to mute any keypresses which happen outside of PsychoPy
