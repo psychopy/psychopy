f1b4aa556b793ed1aa20296385ab97a1845c4645
Jon Peirce <jon.peirce@gmail.com>
2026-02-23 10:09:14 +0000

    RF: removed app-building code

--- building/plat_custom_installs.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-import sys
-
-if sys.platform == 'win32':
-    subprocess.call(
-        'pip install --extra-index-url https://www.lfd.uci.edu/~gohlke/pythonlibs pyWinhook'
-        )
