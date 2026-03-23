5bedcac1fa0c8a943f941b8881743b3f7da18013
Todd Parsons <todd.e.parsons@gmail.com>
2026-03-23 16:17:19 +0000

    FF: Default to system locale

--- psychopy/localization/translation.py
+++ psychopy/localization/translation.py
@@ -1,5 +1,7 @@
 from i18next import config, trans
 from pathlib import Path
+import locale
+
 
 __all__ = [
     "getLocale",
@@ -10,27 +12,37 @@ __all__ = [
 # setup i18next config
 config.fallback_lang = "en-US"
 config.locale_path = Path(__file__).parent / "locales"
-
-
-# settable locale
-locale = "ja-JP"
-
-# wrapper to translate to this locale
-def _translate(value):
-    return trans(value, lang=locale)
-
-
-def setLocale(value):
-    """
-    Set the current locale
-    """
-    global locale
-    locale = value
+# global for settable locale
+currentLocale = None
 
 
 def getLocale():
     """
     Get the current locale
     """
-    return locale
+    return currentLocale
 
+
+def setLocale(value):
+    """
+    Set the current locale
+    """
+    global currentLocale
+    # if requested system, get system locale
+    if value in (None, "system locale", "system"):
+        value = locale.getdefaultlocale()[0]
+    # sanitize
+    value = value.replace("_", "-")
+    # set
+    currentLocale = value
+
+
+def _translate(value):
+    """
+    Wrapper around i18next.trans which translates to the current locale
+    """
+    return trans(value, lang=currentLocale)
+
+
+# default to system locale
+setLocale(None)
