53b3853617982b1bf75577c649cf673a29b6f392
TEParsons <todd.e.parsons@gmail.com>
2026-05-15 10:35:03 +0100

    DOC: Update hello_world, autoDraw/log, exp handler, log files and runtime info demos

--- psychopy/demos/coder/experiment control/experimentHandler.py
+++ psychopy/demos/coder/experiment control/experimentHandler.py
@@ -1,57 +1,90 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
-Demo of class data.ExperimentHandler
+Shows how to use an ExperimentHandler object to manage running an experiment.
+
+The contents of this file are in the public domain.
 """
 
 from psychopy import data, logging
+from psychopy.demos.utils import outputFolder
 from numpy import random
 logging.console.setLevel(logging.DEBUG)
 
-exp = data.ExperimentHandler(name='testExp',
-                version='0.1',
-                extraInfo={'participant':'jwp', 'ori':45},
-                runtimeInfo=None,
-                originPath=None,
-                savePickle=True,
-                saveWideText=True,
-                dataFileName='testExp')
+# create an object which manages the whole experiment
+exp = data.ExperimentHandler(
+    name="testExp",
+    version="0.1",
+    extraInfo={
+        'participant': "jwp", 
+        'ori': 45
+    },
+    runtimeInfo=None,
+    originPath=None,
+    savePickle=True,
+    saveWideText=True,
+    dataFileName=outputFolder / "testExp"
+)
 
-# a first loop (like training?)
-conds = data.createFactorialTrialList(
-    {'faceExpression':['happy', 'sad'], 'presTime':[0.2, 0.3]})
-training = data.TrialHandler(trialList=conds, nReps=3, name='train',
-                 method='random',
-                 seed=100)  # this will set the global seed - for the whole exp
+# this is an easy way to make conditions with every combination of multiple factors
+# if you prefer, you could also create a spreadsheet file and load it with data.importConditions
+conds = data.createFactorialTrialList({
+    'faceExpression': ['happy', 'sad'], 
+    'presTime': [0.2, 0.3]
+})
+
+# create our first loop - a training step
+training = data.TrialHandler(
+    trialList=conds, 
+    nReps=3, 
+    name='train',
+    method='random',
+    # beware: this sets the global seed for all random functions
+    seed=100
+)
 exp.addLoop(training)
-# run those trials
+
+# run those trials...
 for trial in training:
+    # let's add some data (and pretend it came from a participant...)
     training.addData('training.rt', random.random() * 0.5 + 0.5)
     if random.random() > 0.5:
         training.addData('training.key', 'left')
     else:
         training.addData('training.key', 'right')
+    # this tells the ExperimentHandler to move on to the next row in your output data
     exp.nextEntry()
 
-# then run 3 repeats of a staircase
-outerLoop = data.TrialHandler(trialList=[], nReps=3, name='stairBlock',
-                 method='random')
+# this loop handles repeating the blocks inside it
+outerLoop = data.TrialHandler(
+    trialList=[], 
+    nReps=3, 
+    name='stairBlock',
+    method='random'
+)
 exp.addLoop(outerLoop)
-for thisRep in outerLoop:  # the outer loop doesn't save any data
-    staircase = data.StairHandler(startVal=10, name='staircase', nTrials=5)
+# run the outer loop...
+for thisRep in outerLoop:
+    # each repetition, create a staircase
+    staircase = data.StairHandler(
+        startVal=10, 
+        name='staircase', 
+        nTrials=5
+    )
     exp.addLoop(staircase)
+    # run the staircase...
     for thisTrial in staircase:
-        id=random.random()
+        # create a random id for this trial
+        id = random.random()
+        exp.addData('id', id)
+        # more "participant data"
         if random.random() > 0.5:
             staircase.addData(1)
         else:
             staircase.addData(0)
-        exp.addData('id', id)
+        # next row
         exp.nextEntry()
+
+# now that the experiment is finished, show the results
 for e in exp.entries:
     print(e)
 print("Done. 'exp' experimentHandler will now (end of script) save data to testExp.csv")
 print(" and also to testExp.psydat, which is a pickled version of `exp`")
-
-# The contents of this file are in the public domain.
