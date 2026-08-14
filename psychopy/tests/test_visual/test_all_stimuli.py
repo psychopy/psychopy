4016ac8c6d077f35473c5b18b78e8b2539083513
Todd Parsons <todd.e.parsons@gmail.com>
2026-08-14 10:34:39 +0100

    TEST: Update artefacts for TextStim tests

--- psychopy/tests/test_visual/test_all_stimuli.py
+++ psychopy/tests/test_visual/test_all_stimuli.py
@@ -229,13 +229,22 @@ class _baseVisualTest():
         #set font
         fontFile = str(utils.TESTS_FONT)
         #using init
-        stim = visual.TextStim(win,text=u'\u03A8a', color=[0.5, 1.0, 1.0], ori=15,
-            height=0.8*self.scaleFactor, pos=[0,0], font='DejaVu Serif',
-            fontFiles=[fontFile])
+        stim = visual.TextStim(
+            win,
+            text=u'\u03A8a', 
+            color=[0.5, 1.0, 1.0], 
+            ori=15,
+            height=0.8*self.scaleFactor,
+            wrapWidth=1.6*self.scaleFactor,
+            pos=[0,0], 
+            font='DejaVu Serif',
+            fontFiles=[fontFile]
+        )
         stim.draw()
         if self.win.winType != 'pygame':
             #compare with a LIBERAL criterion (fonts do differ)
-            utils.compareScreenshot('text1_%s.png' %(self.contextName), win, crit=20)
+            # win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / ('text1_%s.png' % self.contextName))
+            utils.compareScreenshot('text1_%s.png' % self.contextName, win, crit=20)
         win.flip()  # AFTER compare screenshot
         #using set
         stim.text = 'y'
@@ -253,6 +262,7 @@ class _baseVisualTest():
         "{}".format(stim) #check that str(xxx) is working
         if self.win.winType != 'pygame':
             #compare with a LIBERAL criterion (fonts do differ)
+            # win.getMovieFrame(buffer='back').save(Path(utils.TESTS_DATA_PATH) / ('text2_%s.png' % self.contextName))
             utils.compareScreenshot('text2_%s.png' %self.contextName,
                                     win, crit=20)
 
