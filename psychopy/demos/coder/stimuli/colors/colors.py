4aad0628d7ebfe6c4a7fe77311f550e9cef30c1f
Todd Parsons <todd.e.parsons@gmail.com>
2026-08-28 17:32:54 +0100

    DOC: Update colors demo

--- psychopy/demos/coder/stimuli/colors/colors.py
+++ psychopy/demos/coder/stimuli/colors/colors.py
@@ -1,70 +1,107 @@
 """
-This demo shows off the Color class (`psychopy.colors.Color`), allowing object 
-colors to be set and accessed in a variety of different color spaces! Just choose
-a color space from the slider at the top and type a value into the textbox
+This demo shows how colors are defined in PsychoPy, and shows how to use the Color class (`psychopy.colors.Color`) to easily convert between them. Just choose a color space from the slider at the top, then type a value into the textbox to see that value as a color.
+
+The contents of this file are in the public domain.
 """
-from psychopy import visual, colors
 
-# Create window
-win = visual.Window(size=(1080, 720), units='height')
+from psychopy import visual, colors, event
 
-# Button to end script
-endBtn = visual.ButtonStim(win, "End",
-    pos=(0, -0.45), anchor='bottom-center', 
-    size=(0.2, 0.1), letterHeight=0.05)
-# Color space chooser
-spaces = ['named', 'hex', 'rgb', 'rgb1', 'rgb255', 'hsv', 'lms']
-spaceCtrl = visual.Slider(win, style="radio",
-    ticks=list(range(len(spaces))), granularity=1,
+# create window
+win = visual.Window(
+    size=(1080, 720), 
+    units='height'
+)
+
+# add some instructions
+instr = visual.TextBox2(
+    win, 
+    text=(
+        "Choose a color space and type a value into the textbox to see that color in the box to the left.\n"
+        "\n"
+        "For more info on the different spaces, check out the documentation: \n"
+        "psychopy.org/general/colours.html\n"
+        "\n"
+        "Press ESCAPE to quit."
+    ), 
+    pos=(0.3, 0), 
+    size=(0.5, 0.5)
+)
+# add a slider to choose color space
+spaces = [
+    # named colors, these are the same as available in HTML:
+    # https://www.w3schools.com/tags/ref_colornames.asp
+    "named", 
+    # hex values, e.g. #ffffff for white
+    'hex', 
+    # rgb (red, green and blue) values, ranging from -1 to 1
+    'rgb', 
+    # rgb values ranging from 0 to 1
+    'rgb1', 
+    # rgb values ranging from 0 to 255
+    'rgb255', 
+    # hsv (hue. saturation, value/luminance) values
+    'hsv', 
+    # lms (long, medium, short) cone values
+    'lms'
+]
+spaceCtrl = visual.Slider(
+    win,
+    style="radio",
+    ticks=None,
     labels=spaces,
-    pos=(0, 0.4), size=(1.2, 0.05), labelHeight=0.02)
-spaceCtrl.value = spaces.index('rgb')
-# TextBox to type in values to try out
-valueCtrl = visual.TextBox2(win, text="#ffffff", font="Courier Prime",
-    pos=(-0.3, 0.1), size=(0.5, 0.2), letterHeight=0.05,
-    color='white', fillColor='black',
-    editable=True)
+    pos=(0, 0.4), 
+    size=(1.2, 0.05)
+)
+spaceCtrl.markerPos = spaceCtrl.labels.index("rgb")
+# add an editable textbox for color values
+valueCtrl = visual.TextBox2(
+    win,
+    text="#ffffff",
+    font="JetBrains Mono",
+    pos=(-0.3, 0.1), 
+    size=(0.5, 0.2),
+    color='white', 
+    fillColor='black',
+    editable=True
+)
+# start off with the ctrl selected
 win.currentEditable = valueCtrl
-# Rect to show current colour
-colorBox = visual.TextBox2(win, text="", font="Open Sans",
-    pos=(-0.3, -0.2), size=(0.5, 0.2), padding=0.05, letterHeight=0.05,
-    color='white', borderColor='white', fillColor=None)
-# Instructions
-instr = (
-    "This demo shows how to define colors in PsychoPy - type a value into the "
-    "textbox to the left, choose a color space and the box below will appear in "
-    "the color and space you have chosen. For more info on the different spaces, "
-    "check out the documentation: \n"
-    "psychopy.org/general/colours.html"
-    )
-instrBox = visual.TextBox2(win, text=instr, font="Open Sans",
-    pos=(0.3, 0), size=(0.5, 0.5), padding=0.05, letterHeight=0.03,
-    color='white', borderColor=None, fillColor=None)
+# add a box to show color
+colorBox = visual.TextBox2(
+    win, 
+    text="", 
+    pos=(-0.3, -0.2), 
+    size=(0.5, 0.2),
+    color='white', 
+    borderColor='white', 
+    fillColor=None
+)
 
-
-while not endBtn.isClicked:
-    # Try to make a fillColor, make it False if failed
+# start a frame loop...
+while not event.getKeys(["escape"]):
+    # process value from value ctrl
+    val = valueCtrl.text
+    if val.isnumeric():
+        val = float(val)
+    if "," in val:
+        val = eval(val)
+    # try to make a fill color from the given values
     try:
-        val = valueCtrl.text
-        # If input looks like a number, convert it
-        if val.isnumeric():
-            val = float(val)
-        # If input looks like an array, un-stringify it
-        if "," in val:
-            val = eval(val)
-        # Get color space from radio slider
-        space = spaces[int(spaceCtrl.markerPos)]
-        col = colors.Color(val, space)
-    except Exception:
-        col = False
-    # Set the color box's fill color, show text "invalid" if color is invalid
-    if col:
+        col = colors.Color(
+            val,
+            space=spaceCtrl.labels[int(spaceCtrl.markerPos)]
+        )
+        # show the color
         colorBox.text = ""
         colorBox.fillColor = col
-    else:
+    except:
+        # if color is invalid, say as much
         colorBox.text = "invalid color"
         colorBox.fillColor = None
-    # Draw stim and flip
-    for stim in (valueCtrl, endBtn, colorBox, spaceCtrl, instrBox):
-        stim.draw()
+    # draw everything
+    instr.draw()
+    spaceCtrl.draw()
+    valueCtrl.draw()
+    colorBox.draw()
+    # flip the window
     win.flip()
