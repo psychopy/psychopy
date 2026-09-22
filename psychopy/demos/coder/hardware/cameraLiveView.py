2e105838511639494243bee23229bdb38d3c9947
Matthew D. Cutone <mcutone@opensciencetools.org>
2026-09-22 16:53:56 -0400

    RF: Add MacOS 10.15+ camera enumeration method and rework stopping logic for recordings (#7795)
    * RF: Add MacOS 10.15+ camera enumeration method
    
    * DOCS: Updated demo and changed file name `camera.py` -> `cameraLiveView.py`
    
    * TEST: Added condition `needs-camera` for a system with camera attached
    
    * TEST: Add camera tests when on real hardware
    
    * RF/NF: Added `isStopping` and changes to setting stop time
    
    * TEST: Added tests for camera (needs real hardware)
    
    * TEST: Fixed test missing assert

--- psychopy/demos/coder/hardware/cameraLiveView.py
+++ psychopy/demos/coder/hardware/cameraLiveView.py
@@ -11,7 +11,7 @@ win = visual.Window(size=(800, 600), units='pix', color='black',
                     fullscr=False, allowGUI=True, title='Camera feed')
 
 # open a camera
-cam = Camera(device=0,  # change this value to change the mode
+cam = Camera(device=0,  # change this value to change the camera
              win=win, 
              usageMode='cv')  # use 'cv' mode if not recording to disk
 cam.open()
@@ -20,10 +20,14 @@ print("camera open: %s @ %s fps" % (cam.frameSize, cam.frameRate))
 # create an ImageStim to display the camera feed
 camView = visual.ImageStim(win, image=cam, size=cam.frameSize, units='pix')
 
+# instruction text
+instr = visual.TextStim(win, text="Press escape to exit.", pos=(0, -280), color='white')
+
 # enter the main loop to display the camera feed
 shots = []
 while event.getKeys(keyList=['escape']) == []:
     camView.draw()
+    instr.draw()
     win.flip()
 
 # clean up and close the camera and window
